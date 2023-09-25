ee2d014fe34246efe2cc889944dafdacbad2d9b9
geforseN <geforsenjs@gmail.com>
2023-09-25 21:21:30 +0700

    add header with theme change

---
+++ components/v-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="navbar p-0 h-8 justify-between border-b border-b-base-300">
+    <span
+      class="bg-secondary p-2 font-bold text-5xl h-full border-2 border-neutral"
+    >
+      pastbl
+    </span>
+    <button
+      class="btn btn-secondary justify-self-end border-4 border-neutral mr-2 text-base"
+      data-toggle-theme="emerald,emerald-dark"
+      data-act-class="ACTIVECLASS"
+    >
+      toggle theme
+    </button>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { themeChange } from "theme-change";
+// FIXME: when theme is 'emerald-dark' on page refresh there is FOUC (from 'emerald' to 'emerald-dark')
+// NOTE: to fix it should add data-theme="emerald-dark" to index.html <html>
+// TODO: use cookie instead of session storage
+// NOTE: theme-change does not provide cookie save, only session
+onMounted(() => themeChange(false));
+</script>
