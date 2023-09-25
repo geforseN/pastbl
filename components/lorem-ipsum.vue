b6a45c758b7b0d8bd7644584618837201b7d16df
geforseN <semen.neb@gmail.com>
2023-09-25 21:23:44 +0700

    add lorem ipsum component

---
+++ components/lorem-ipsum.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-for="number of count">
+    {{ number + ")" }} Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    Debitis quisquam, repellendus asperiores explicabo, modi quibusdam
+    distinctio qui quaerat reiciendis temporibus, consequuntur omnis quod rerum
+    repudiandae beatae inventore nemo facere fuga?
+  </div>
+</template>
+
+<script lang="ts" setup>
+// NOTE: propsDestructure in vue config is used, experimental feature
+const { count = 10 } = defineProps<{ count?: number }>();
+</script>
