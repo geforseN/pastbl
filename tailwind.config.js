984b3003634e967426d01701f37628aabd82f8fd
geforseN <geforsenjs@gmail.com>
2023-09-25 21:08:11 +0700

    update daisyui themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
-  daisyui: {themes: ["emerald"], logs: false},
+  daisyui: {
+    themes: [
+      "emerald",
+      {
+        "emerald-dark": {
+          primary: "#0d7ea1",
+          secondary: "#1da6ad",
+          accent: "#523004",
+          neutral: "#241e2b",
+          "base-100": "#22323c",
+          info: "#132167",
+          success: "#155d2f",
+          warning: "#9d560b",
+          error: "#a52011",
+        },
+      },
+    ],
+    logs: false,
+  },
 };
-
