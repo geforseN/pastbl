61a180250635ed22717b0402179c8d9931bea9a3
geforseN <geforsenjs@gmail.com>
2023-09-25 16:18:00 +0700

    update configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    // "@unocss/nuxt",
+    // "@onu-ui/nuxt",
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    // "@tailvue/nuxt",
+  ],
+  // css: ["onu-ui/dist/style.css"],
+});
