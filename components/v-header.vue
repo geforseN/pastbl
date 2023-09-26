17ccfe84d6a9a0634b00d199f2bfdaf5e8a4aa8a
geforseN <semen.neb@gmail.com>
2023-09-26 12:22:05 +0700

    add nickname input in header

--- components/v-header.vue
+++ components/v-header.vue
@@ -5,6 +5,7 @@
     >
       pastbl
     </span>
+    <input placeholder="Your Nickname" class="placeholder:text-lg placeholder:text-neutral-50 input bg-secondary" v-model="userStore.user.nickname" />
     <button
       class="btn btn-secondary justify-self-end border-4 border-neutral mr-2 text-base"
       data-toggle-theme="emerald,emerald-dark"
@@ -17,9 +18,11 @@
 
 <script setup lang="ts">
 import { themeChange } from "theme-change";
+import { useUserStore } from "~/store/user.store";
 // FIXME: when theme is 'emerald-dark' on page refresh there is FOUC (from 'emerald' to 'emerald-dark')
 // NOTE: to fix it should add data-theme="emerald-dark" to index.html <html>
 // TODO: use cookie instead of session storage
 // NOTE: theme-change does not provide cookie save, only session
 onMounted(() => themeChange(false));
+const userStore = useUserStore()
 </script>
