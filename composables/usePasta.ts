24d6063af59c79ded34949e91f228c5c015e4166
geforseN <semen.neb@gmail.com>
2023-09-27 22:21:00 +0700

    usePasta: change return

--- composables/usePasta.ts
+++ composables/usePasta.ts
@@ -5,37 +5,37 @@ export default function usePasta() {
   const tags = ref<Pasta["tags"]>([]);
   const text = ref<Pasta["text"]>("");
 
-  async function addTagWithClear(newTag: PastaTag) {
-    if (newTag.length === 0) {
-      throw new ExtendedError("Can not add the empty tag");
-    }
-    if (tags.value.includes(newTag)) {
-      throw new ExtendedError("The tag were already added to pasta");
-    }
-    tags.value.push(newTag);
-    tag.value = "";
-  }
-
-  function removeTag(tagValue: string) {
-    const tagIndex = tags.value.indexOf(tagValue);
-    if (tagIndex === -1) {
-      throw new ExtendedError("Can not remove the tag which is not in tags");
-    }
-    tags.value.splice(tagIndex, 1);
-  }
-
-  function clear() {
-    text.value = "";
-    tags.value = [];
-    tag.value = "";
-  }
-
   return {
     tag,
     tags,
     text,
-    clear,
-    removeTag,
-    addTagWithClear,
+    clear: () => {
+      text.value = "";
+      tags.value = [];
+      tag.value = "";
+    },
+    removeTag: (tagValue: string) => {
+      const tagIndex = tags.value.indexOf(tagValue);
+      if (tagIndex === -1) {
+        throw new ExtendedError("Can not remove the tag which is not in tags");
+      }
+      tags.value.splice(tagIndex, 1);
+    },
+    removeAllTags: () => {
+      tags.value = [];
+    },
+    addTag: async (newTag: PastaTag) => {
+      if (newTag.length === 0) {
+        throw new ExtendedError("Can not add the empty tag", {
+          title: "Pasta tag was not added",
+        });
+      }
+      if (tags.value.includes(newTag)) {
+        throw new ExtendedError("The tag were already added to pasta", {
+          title: "Pasta tag was not added",
+        });
+      }
+      tags.value.push(newTag);
+    },
   };
 }
