8d0d0bfad8f7fd3c2e6f589d306013d9a3170209
geforseN <geforsenjs@gmail.com>
2023-09-27 22:11:07 +0700

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+        },
+        ".btn-twitch~~todo": {
+          "background-color": "#1EA1F1",
+          "border-color": "#1EA1F1",
+        },
+        ".btn-twitch:hover~~todo": {
+          "background-color": "#1C96E1",
+          "border-color": "#1C96E1",
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          "base-content": "#ffffff",
+        },
+      },
+    ],
   },
 };
