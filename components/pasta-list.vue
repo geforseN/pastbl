63390daac8e41da4260edfad1e7905e4142d0983
geforseN <semen.neb@gmail.com>
2023-09-27 22:27:13 +0700

    add-pasta and pasta-list: big change

--- components/pasta-list.vue
+++ components/pasta-list.vue
@@ -1,17 +1,56 @@
 <template>
-  <div v-if="pastasStore.isLoaded_" class="bg-red-900">LOADING</div>
+  <div v-if="!pastasStore.isLoaded_" class="bg-red-900">LOADING</div>
   <template v-else>
     <div v-if="pastasStore.pastas.length === 0" class="justify-self-center mt-4">
-      NO PASTAS
+      No pastas were added yet!
     </div>
-    <div v-else class="w-[340px]" >
-      <v-pasta :pasta="pasta" v-for="pasta of pastasStore.pastas">
-        <slot />
-      </v-pasta>
+    <div v-else class="">
+      <div v-if="!clipboard.isSupported">Your browser does not support Clipboard API</div>
+      <div v-for="pasta of pastasStore.pastas" class="flex">
+        <chat-pasta :pasta="pasta" class="w-[340px]">
+          <slot />
+        </chat-pasta>
+        <button v-if="clipboard.isSupported" class="btn btn-md btn-square rounded-none border-accent border-2 text-xs"
+          @click="handleCopypastaCopy(pasta)">
+          copy pasta
+        </button>
+      </div>
     </div>
   </template>
 </template>
 
-<script setup lang="ts" >
+<script setup lang="ts">
+// TODO add useSound composable
+// TODO add user.preference.shouldShowToastOnCopypastaSuccess
+
 const pastasStore = usePastasStore()
+
+const toast = useToast()
+const clipboard = useClipboard({})
+if (!clipboard.isSupported) {
+  toast.add({ description: 'Your browser does not support Clipboard API', title: 'Copypasta problem', timeout: 10_000, color: 'red' })
+}
+
+async function handleCopypastaCopy(pasta: Pasta) {
+  try {
+    await clipboard.copy(pasta.text);
+    if (!clipboard.copied) {
+      throw new Error('Copypasta was not copied')
+    }
+    toast.add({ description: 'Copypasta copied successfully', title: 'Copypasta 🤙🤙🤙', timeout: 1_700 })
+  } catch (error: Error | unknown) {
+    const description =
+      typeof error === 'object'
+      && error !== null
+      && 'message' in error
+      && error.message
+    toast.add({
+      description: description || 'Copypasta was not copied',
+      title: 'Copypasta problem',
+      timeout: 7_000,
+      color: 'red'
+    })
+  }
+}
+
 </script>
