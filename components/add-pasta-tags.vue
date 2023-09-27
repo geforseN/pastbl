a4bcfdf1414afcdb7a1acacd40b0d4122957d09c
geforseN <semen.neb@gmail.com>
2023-09-27 22:20:32 +0700

    add-pasta-tags: make black-box logic

--- components/add-pasta-tags.vue
+++ components/add-pasta-tags.vue
@@ -1,60 +1,19 @@
 <template>
-  <div class="join">
-    <input
-      type="text"
-      id="add-tag"
-      v-model="pastaStore.tag"
-      placeholder="Type pasta tag"
-      class="input input-bordered join-item w-96"
-      minlength="1"
-    />
+  <div class="join mt-2 w-full">
+    <input type="text" id="add-tag" @keyup.enter="emit('addTag')" v-model="modelValue" placeholder="Type pasta tag"
+      class="input input-bordered w-full border-base-content join-item bg-opposite placeholder:text-base-content" />
     <button
-      class="join-item btn text-3xl"
-      @click="
-        () => pastaStore.addTagWithClear(pastaStore.tag).catch((err) => console.log(err))
-      "
-    >
+      class="px-0 join-item btn w-[170px] text-3xl border border-base-content hover:border hover:border-base-content hover:bg-secondary/5"
+      @click="emit('addTag')">
       <span class="inline-flex gap-x-1 items-center">
         +
-        <span class="text-sm">add tag</span>
+        <span class="text-base">add tag</span>
       </span>
     </button>
   </div>
 </template>
 <script setup lang="ts">
-const pastaStore =  usePastaStore()
+const modelValue = defineModel<string>();
+const emit = defineEmits<{ addTag: [tag?: string] }>();
+// , changeTag: []
 </script>
-
-<!-- <div class="form-control w-full max-w-xs">
-      <label class="label" for="add-tag">
-        <span class="label-text">Tag for pasta</span>
-        <span class="label-text-alt relative right-12 bg-slate-400 p-1 rounded">
-          Add tag
-        </span>
-      </label>
-      <div class="flex relative">
-        <input
-          type="text"
-          id="add-tag"
-          v-model="pasta.tag"
-          placeholder="Type pasta tag"
-          class="input input-bordered w-96 p-3 pr-30"
-          minlength="1"
-        />
-        <button
-          class="btn text-3xl absolute right-0"
-          @click="
-            () =>
-              pasta.addTagWithClear(pasta.tag).catch((err) => console.log(err))
-          "
-        >
-          <span class="w-full inline-flex gap-x-1 items-center">
-            +
-            <span class="text-sm">add tag</span>
-          </span>
-        </button>
-      </div>
-      <label class="label">
-        <span class="label-text-alt">Tag for pasta</span>
-      </label>
-    </div> -->
