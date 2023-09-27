8aa0c83e0c041c93f6eaf35835ef38b28a58fdac
geforseN <geforsenjs@gmail.com>
2023-09-27 22:22:39 +0700

    css changes

--- components/added-tags.vue
+++ components/added-tags.vue
@@ -1,10 +1,11 @@
 <template>
-  <div v-if="pastaStore.tags.length === 0">NO TAGS WERE ADDED</div>
-  <ul v-else class="flex w-20 flex-wrap">
-    <li class="badge badge-lg bg-secondary flex gap-x-1" v-for="tag of pastaStore.tags">
+  <div class="w-full flex justify-center" v-if="pastaStore.tags.length === 0">
+    <span class="bg-warning rounded text-warning-content px-2 py-1 border">No tags were added</span>
+  </div>
+  <ul v-else class="w-full grid">
+    <li class="badge badge-lg bg-secondary flex gap-x-1 w-full" v-for="tag of pastaStore.tags">
       <span>{{ tag }}</span>
-      <button class="btn btn-circle block btn-xs text-xs font-normal h-1"
-        @click="pastaStore.removeTag(tag)">
+      <button class="btn btn-circle block btn-xs text-xs font-normal h-1" @click="pastaStore.removeTag(tag)">
         <span>x</span>
       </button>
     </li>
