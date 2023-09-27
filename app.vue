d0a8a64209390fcfa1dc36c1bd2756c1af2ed9a9
geforseN <semen.neb@gmail.com>
2023-09-27 22:26:40 +0700

    app: change template & move handler to script

--- app.vue
+++ app.vue
@@ -13,30 +13,40 @@
     <!-- NOTE: client only is used here because pastaStore and pastasStore persist data in localStorage, which is client only -->
     <!-- NOTE: for persist pinia-plugin-persistedstate is used, 
       for nuxt3 by default it uses cookie persist, but 
-      for some reason, pasta with length over ~400 char is not saved in cookie,
-      so because of that localStorage is used, which does not behave like that with long pasta text
+      for some reason, pasta with length over ~400 char is not saved in cookie, so 
+      localStorage is used for persist, which does not behave like that with long pasta text
     -->
     <client-only>
-      <div class="grid grid-flow-row grid-cols-2 justify-items-center">
+      <div class="flex justify-center gap-x-12 w-full mt-2">
         <pasta-list>
           <user-nickname :user="userStore.user" />
         </pasta-list>
-        <add-pasta @createPastaClick="(event: MouseEvent) => {
-          pastasStore.createPasta({ tags: pastaStore.tags, text: pastaStore.text })
-            .then(() => {
-              pastaStore.clear();
-            })
-        }" />
-        <user-settings class="col-start-2 col-span-1 row-start-2"></user-settings>
+        <div class="flex flex-col gap-y-4">
+          <add-pasta @createPastaClick="handlePastaCreation" @createPastaEnterPressed="handlePastaCreation" />
+          <user-settings />
+        </div>
       </div>
     </client-only>
   </div>
 </template>
 
 <script setup lang="tsx" >
-
+const toast = useToast()
 
 const pastasStore = usePastasStore();
 const pastaStore = usePastaStore();
 const userStore = useUserStore();
+
+function handlePastaCreation<E extends KeyboardEvent | MouseEvent>(_event: E) {
+  pastasStore.createPasta({ tags: pastaStore.tags, text: pastaStore.text })
+    .then(() => {
+      pastaStore.clear();
+      toast.add({ description: 'Pasta added successfully', title: 'Pasta 🤙🤙🤙' })
+    }).catch((error) => {
+      if (!(error instanceof ExtendedError)) {
+        throw error
+      }
+      toast.add({ description: error.message, title: 'Pasta creation went wrong', color: 'red', timeout: 10_000 })
+    })
+}
 </script>
