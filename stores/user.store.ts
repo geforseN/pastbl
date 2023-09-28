1d73490f10a240cd6e454d920e8f8bb8fd64c988
geforseN <geforsenjs@gmail.com>
2023-09-28 11:58:29 +0700

    user.store: add user.statistic and refactor

--- stores/user.store.ts
+++ stores/user.store.ts
@@ -3,12 +3,68 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore(
   "user",
   () => {
-    const preferences = reactive<{ nickname: { color: string } }>({
-      nickname: { color: "red" },
+    const user = ref<{
+      nickname: string;
+      preferences: {
+        nickname: { color: string };
+      };
+      statistic: { lastCopyPasta?: MegaPasta };
+      badges: { count: number };
+    }>({
+      nickname: "",
+      preferences: {
+        nickname: { color: "red" },
+      },
+      badges: { count: 1 },
+      statistic: {
+        lastCopyPasta: undefined,
+      },
     });
-    const user = ref({ nickname: "", preferences, badges: { count: 1 } });
+    
+    const pastasStore = usePastasStore();
 
-    return { preferences, user };
+    const preferences = computed(() => user.value.preferences);
+    const statistic = computed(() => user.value.statistic);
+
+    watch(
+      () => pastasStore.latestPasta,
+      (latestPasta) => {
+        if (!latestPasta) {
+          return console.log("In watch: No latestPasta was found");
+        }
+        if (
+          statistic.value.lastCopyPasta?.createdAt === latestPasta.createdAt
+        ) {
+          return console.log("In watch: User already persisted latest pasta");
+        }
+        console.log("In watch: New lastCopyPasta", latestPasta);
+        user.value.statistic.lastCopyPasta = latestPasta;
+      }
+    );
+
+    return { preferences, user, statistic };
   },
-  { persist: true }
+  {
+    persist: {
+      storage: persistedState.localStorage,
+      afterRestore(ctx) {
+        // NOTE: queueMicrotask is used because without queueMicrotask store has not defined yet
+        queueMicrotask(() => {
+          const pastasStore = usePastasStore();
+          const lastPasta = pastasStore.latestPasta;
+          if (!lastPasta) {
+            return console.log("User has not created pasta");
+          }
+          if (
+            lastPasta.createdAt ===
+            ctx.store.statistic?.lastCopyPasta?.createdAt
+          ) {
+            return console.log("User already persisted latest pasta");
+          }
+          console.log("New lastCopyPasta", lastPasta);
+          ctx.store.user.statistic.lastCopyPasta = lastPasta;
+        });
+      },
+    },
+  }
 );
