08872a3d837e28a785717ec1474d9de1d1475b45
geforseN <geforsenjs@gmail.com>
2023-09-28 12:00:00 +0700

    added-tags: change template

--- components/added-tags.vue
+++ components/added-tags.vue
@@ -2,16 +2,20 @@
   <div class="w-full flex justify-center" v-if="props.tags.length === 0">
     <span class="bg-warning rounded text-warning-content px-2 py-1 border">No tags were added</span>
   </div>
-  <ul v-else class="w-full grid">
-    <li class="badge badge-lg bg-secondary flex gap-x-1 w-full" v-for="tag of props.tags">
-      <span>{{ tag }}</span>
-      <button class="btn btn-circle block btn-xs text-xs font-normal h-1" @click="emit('removeTag', tag)">
+  <div v-else class="grid gap-2 overflow-y-auto max-h-[380px]">
+    <div v-for="tag of props.tags" :title="tag" class="bg-secondary flex justify-between px-1 py-0.5 gap-x-1 rounded-lg">
+      <span class="line-clamp-4 break-all">{{ tag }}</span>
+      <button class="btn block btn-xs h-full text-xs font-normal" :title="`remove tag: ${tag}`"
+        @click="emit('removeTag', tag)">
         <span>x</span>
       </button>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 const props = defineProps<{ tags: string[] }>();
 const emit = defineEmits<{ removeTag: [tag: string] }>();
+// TODO: can add on tag mouseover logic with hint: 
+// if user mouseovered then click on any place of tag will remove it,
+// should show transparent large text 'REMOVE' over that tag
 </script>
