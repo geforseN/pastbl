0a19fbff064bffe00256394b75544dd46843345f
geforseN <semen.neb@gmail.com>
2023-09-28 17:13:11 +0700

    change placeholder

--- components/add-pasta-tags.vue
+++ components/add-pasta-tags.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="join mt-2 w-full">
-    <input type="text" id="add-tag" @keyup.enter="emit('addTag')" v-model="modelValue" placeholder="Type pasta tag"
+    <input type="text" id="add-tag" @keyup.enter="emit('addTag')" v-model="modelValue" placeholder="Enter a pasta tag"
       class="input input-bordered w-full border-base-content join-item bg-opposite placeholder:text-base-content" />
     <button
       class="px-0 join-item btn w-[170px] text-3xl border border-base-content hover:border hover:border-base-content hover:bg-secondary/5"
