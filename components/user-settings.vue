cfee2f34a37354af02983cfd0f07b37697b0500d
geforseN <geforsenjs@gmail.com>
2023-09-28 23:56:02 +0700

    update user-settings component

--- components/user-settings.vue
+++ components/user-settings.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="p-2 border-2 border-base-content rounded">
-    <h2 class="text-3xl font-bold mb-2">User settings</h2>
-    <div>
-      <div class="w-full max-w-xs flex items-center border-2 border-secondary rounded-md">
-        <label class="label py-0">
-          <span class="oneWordPerLine w-min">Badges count</span>
+  <section class="p-2 border-2 border-base-content rounded">
+    <h2 class="text-3xl font-bold mb-2 border-b p-2">User settings</h2>
+    <div class="w-full flex flex-col gap-y-4">
+      <div class="w-full grid grid-cols-[2fr,4fr] rounded-md pt-6 pb-2 px-2 border-y-2">
+        <label class="label" for="badges-count">
+          <span class="text-xl font-medium">Badges count</span>
         </label>
-        <input v-model="userStore.user.badges.count" min="0" max="10" type="number" name="" id="badges-count"
-          class="input border-l-black w-24">
+        <user-settings-badges-count v-model:badges-count.number="userStore.user.badges.count" />
       </div>
-      <div class="w-full max-w-xs flex items-center border-2 border-secondary rounded-md">
-        <label class="label">
-          <span class="label-text">Nickname</span>
+      <div class="w-full grid grid-cols-[2fr,4fr] rounded-md pt-6 pb-1 px-2 border-y-2">
+        <label class="label" for="nickname">
+          <span class="text-xl font-medium">Nickname</span>
         </label>
-        <input name="" id="" placeholder="Your Nickname"
-          class="placeholder:text-lg placeholder:text-neutral-50 input input-bordered bg-secondary"
+        <input name="nickname" id="nickname" placeholder="Your Nickname"
+          class="placeholder:text-lg placeholder:text-base-content input input-bordered input-secondary"
           v-model="userStore.user.nickname" />
       </div>
     </div>
-  </div>
+  </section>
 </template>
 <script setup lang="ts">
 const userStore = useUserStore();
