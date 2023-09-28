526b618ae1f5f842ed9793c3e1f7f0ac36edf0fc
geforseN <semen.neb@gmail.com>
2023-09-28 12:03:49 +0700

    add-pasta: change

--- components/add-pasta.vue
+++ components/add-pasta.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="border-2 rounded border-base-content p-2 h-max">
-    <div class="flex gap-x-2">
+    <div class="flex gap-x-2 max-h-[75vh]">
       <div class="flex flex-col w-max">
         <textarea v-model="pastaStore.text" placeholder="Enter a pasta"
           @keyup.enter="emit('createPastaEnterPressed', $event)"
-          class="w-80 h-[60vh] max-h-[75vh] text box-content textarea leading-[19.5px] text-[13px] font-normal placeholder:text-lg placeholder:text-base-content caret-[#a970ff] leading-3 rounded focus-within:outline-[#a970ff] focus-within:outline-offset-0 focus-within:outline-4 border-base-content border-2 px-[16px] py-[8px] hover:px-[15px] hover:py-[7px] hover:border-[3px]" />
+          class="w-80 h-[60vh] max-h-[75vh] text box-content textarea leading-[19.5px] text-[13px] font-normal placeholder:text-lg placeholder:text-base-content caret-[#a970ff rounded focus-within:outline-[#a970ff] focus-within:outline-offset-0 focus-within:outline-4 border-base-content border-2 px-[16px] py-[8px] hover:px-[15px] hover:py-[7px] hover:border-[3px]" />
       </div>
       <div class="flex flex-col justify-between w-40">
         <div class="flex flex-col items-center">
@@ -13,16 +13,17 @@
             create pasta
           </button>
           <div class="">
-            <span>Symbols count: </span>
+            <span>Pasta length: </span>
             <span :class="`text-${pastaLengthColor}`">
-              {{ pastaStore.text.length }}</span>
-
+              {{ pastaStore.text.length }}
+            </span>
           </div>
         </div>
         <div class="flex flex-col gap-y-2">
           <added-tags @remove-tag="tag => pastaStore.removeTag(tag)" :tags="pastaStore.tags" />
           <button v-if="pastaStore.tags.length !== 0" class="btn btn-sm btn-error" @click="pastaStore.removeAllTags">
-            remove all tags</button>
+            remove all tags
+          </button>
         </div>
       </div>
     </div>
@@ -36,9 +37,8 @@ const toast = useToast();
 
 const emit = defineEmits<{
   createPastaClick: [event: MouseEvent];
-  createPastaEnterPressed: [event: KeyboardEvent]
+  createPastaEnterPressed: [event: KeyboardEvent];
 }>();
-
 
 async function handleTagAddToPasta(tag: string) {
   try {
@@ -56,14 +56,18 @@ async function handleTagAddToPasta(tag: string) {
   }
 }
 
+// FIXME: had strange bug, dynamic classes in template,
+// which used computed below, did not wanted to get required style
+// probably tailwind did not added classes in bundle
+// so {{ pastaStore.text.length }} did not have required color, text had base text color 
 const pastaLengthColor = computed(() => {
   if (pastaStore.text.length === 0 || pastaStore.text.length > 1000) {
-    return 'error'
+    return 'error';
   }
   if (pastaStore.text.length > 500) {
-    return 'warning'
+    return 'warning';
   }
-  return 'success'
+  return 'success';
 })
 </script>
 
