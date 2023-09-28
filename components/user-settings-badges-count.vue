a66865544f7af468842951411f739b622a0e3ca6
geforseN <semen.neb@gmail.com>
2023-09-28 23:56:02 +0700

    update user-settings component

---
+++ components/user-settings-badges-count.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="join relative">
+    <div class="join-item flex-1">
+      <label class="label absolute bottom-10 block w-96">
+        <span class="label-text text-error font-bold">{{ errorMessage }}</span>
+      </label>
+      <input :value="props.badgesCount" @input="handleInputChange($event)" min="0" max="10" type="number"
+        name="badges-count" id="badges-count" class="input rounded-r-none input-secondary flex-1 w-full">
+    </div>
+    <button
+      class="join-item text-2xl font-medium btn btn-square border border-secondary focus-within:bg-secondary focus-within:outline-secondary hover:bg-secondary"
+      @click="() => {
+        if (props.badgesCount === 10) {
+          return errorMessage = 'The badges count can not be more than 10';
+        }
+        if (errorMessage) {
+          errorMessage = ''
+        }
+        emit('update:badgesCount', props.badgesCount + 1)
+      }">+</button>
+    <button
+      class="pb-1 join-item text-2xl font-medium btn btn-square border border-secondary focus-within:bg-secondary focus-within:outline-secondary hover:bg-secondary"
+      @click="() => {
+        if (props.badgesCount === 0) {
+          return errorMessage = 'The badges count can not be negative number';
+        }
+        if (errorMessage) {
+          errorMessage = ''
+        }
+        emit('update:badgesCount', props.badgesCount - 1)
+      }">-</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps<{ badgesCount: number }>();
+const emit = defineEmits<{ 'update:badgesCount': [value: number] }>();
+const errorMessage = ref('');
+
+async function handleInputChange(event: unknown) {
+  if (typeof event !== 'object' || event === null) {
+    throw new TypeError();
+  }
+  if (!('target' in event) || typeof event.target !== 'object' || event.target === null) {
+    throw new TypeError();
+  }
+  if (!('value' in event.target)) {
+    throw new TypeError();
+  }
+  const value = Number(event.target.value);
+  if (!Number.isInteger(value)) {
+    throw new TypeError();
+  }
+  if (value > 10) {
+    return errorMessage.value = ('The badges count can not be more than 10');
+  }
+  if (value < 0) {
+    return errorMessage.value = ('The badges count can not be negative number');
+  }
+  if (errorMessage.value) {
+    errorMessage.value = ''
+  }
+  emit('update:badgesCount', value);
+}
+</script>
