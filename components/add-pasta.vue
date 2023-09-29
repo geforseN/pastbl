3fab08d35dd9e6f059fc7d3616b46bbba8a7a2ba
geforseN <semen.neb@gmail.com>
2023-09-29 21:40:26 +0700

    add-pasta: add image in h2

--- components/add-pasta.vue
+++ components/add-pasta.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border-2 rounded border-base-content p-2 h-max">
+  <section class="border-2 rounded border-base-content p-2 h-max">
     <!-- FIXME: had strange bug, dynamic classes in template, -->
     <!-- which used computed below, did not wanted to get required style -->
     <!-- probably tailwind did not added classes in bundle -->
@@ -12,6 +12,16 @@
     <div hidden class="focus-within:outline-error focus-within:outline-warning focus-within:outline-success" />
     <div hidden class="text-error text-warning text-success " />
     <!-- UPD: above two hidden div elements with proper classes are added to fix classes can not came into bundle  -->
+    <h2 class="text-3xl font-bold mb-2 border-b p-2 relative">
+      Create pasta
+      <!-- TODO onhover change img to basedge, xdd, aRolf ... -->
+      <div class="relative inline-block">
+        <img class="inline ml-1" src="https://cdn.7tv.app/emote/6306876cbe8c19d70f9d6b22/1x.webp" alt="Jokerge emote">
+        <img class="absolute right-0 bottom-0 scale-150 -translate-y-1 -translate-x-[2.5px] motion-reduce:hidden"
+          src="https://cdn.7tv.app/emote/6216d2f73808dfe5c465bc4a/1x.webp" alt="Alert emote"
+          :hidden="!shouldShowDisgustingAlert">
+      </div>
+    </h2>
     <div class="flex gap-x-2 max-h-[75vh]">
       <twitch-chat ref="twitchChatRef" v-model="pastaStore.text"
         @enter-pressed="emit('createPastaEnterPressed', $event)"></twitch-chat>
@@ -36,7 +46,7 @@
       </div>
     </div>
     <add-pasta-tags class="mb-2" v-model="pastaStore.tag" @add-tag="(tag = pastaStore.tag) => handleTagAddToPasta(tag)" />
-  </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
@@ -76,5 +86,13 @@ const pastaLengthColor = computed(() => {
     return 'warning';
   }
   return 'success';
+})
+
+const shouldShowDisgustingAlert = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    shouldShowDisgustingAlert.value = false;
+  }, 3_000)
 })
 </script>
