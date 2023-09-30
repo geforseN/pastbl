4a545369319280cdf425658fc107e02baa11c732
geforseN <geforsenjs@gmail.com>
2023-09-30 16:30:34 +0700

    simplify stores code

--- stores/pasta.store.ts
+++ stores/pasta.store.ts
@@ -3,14 +3,8 @@ import { defineStore } from "pinia";
 export const usePastaStore = defineStore(
   "pasta",
   () => {
-    const pasta = usePasta();
     return {
-      ...pasta,
-      pastaValue: computed(() => ({
-        tag: pasta.tag,
-        tags: pasta.tags,
-        text: pasta.text,
-      })),
+      ...usePasta(),
     };
   },
   {
