5d691de5c58e4db960b67830d011b8081ba483f5
geforseN <geforsenjs@gmail.com>
2023-10-01 23:41:00 +0700

    user-settings-badges-count: lint and fix label

--- components/user-settings-badges-count.vue
+++ components/user-settings-badges-count.vue
@@ -1,53 +1,74 @@
 <template>
   <div class="join relative">
     <div class="join-item grow">
-      <label class="label absolute bottom-10 block w-96">
+      <label for="badges-count" class="label absolute bottom-10 block w-96">
         <span class="label-text text-error font-bold">{{ errorMessage }}</span>
       </label>
-      <input :value="props.badgesCount" @input="handleInputChange($event)" min="0" max="10" type="number"
-        name="badges-count" id="badges-count" class="input text-lg rounded-r-none input-secondary w-full">
+      <input
+        :value="props.badgesCount"
+        @input="handleInputChange($event)"
+        min="0"
+        max="10"
+        type="number"
+        name="badges-count"
+        id="badges-count"
+        class="input text-lg rounded-r-none input-secondary w-full"
+      />
     </div>
     <button
       class="pb-1 join-item text-2xl font-medium btn btn-square border border-secondary focus-within:bg-secondary focus-within:outline-secondary hover:bg-secondary"
-      @click="() => {
-        if (props.badgesCount === 0) {
-          errorMessage = 'The badges count can not be negative number';
-          return;
+      @click="
+        () => {
+          if (props.badgesCount === 0) {
+            errorMessage = 'The badges count can not be negative number';
+            return;
+          }
+          if (errorMessage) {
+            errorMessage = '';
+          }
+          emit('update:badgesCount', props.badgesCount - 1);
         }
-        if (errorMessage) {
-          errorMessage = ''
-        }
-        emit('update:badgesCount', props.badgesCount - 1)
-      }">-</button>
+      "
+    >
+      -
+    </button>
     <button
       class="join-item text-2xl font-medium btn btn-square border border-secondary focus-within:bg-secondary focus-within:outline-secondary hover:bg-secondary"
-      @click="() => {
-        if (props.badgesCount === 10) {
-          errorMessage = 'The badges count can not be more than 10';
-          return;
+      @click="
+        () => {
+          if (props.badgesCount === 10) {
+            errorMessage = 'The badges count can not be more than 10';
+            return;
+          }
+          if (errorMessage) {
+            errorMessage = '';
+          }
+          emit('update:badgesCount', props.badgesCount + 1);
         }
-        if (errorMessage) {
-          errorMessage = ''
-        }
-        emit('update:badgesCount', props.badgesCount + 1)
-      }">+</button>
+      "
+    >
+      +
+    </button>
   </div>
 </template>
 
 <script lang="ts" setup>
-
 const props = defineProps<{ badgesCount: number }>();
-const emit = defineEmits<{ 'update:badgesCount': [value: number] }>();
-const errorMessage = ref('');
+const emit = defineEmits<{ "update:badgesCount": [value: number] }>();
+const errorMessage = ref("");
 
 async function handleInputChange(event: unknown) {
-  if (typeof event !== 'object' || event === null) {
+  if (typeof event !== "object" || event === null) {
     throw new TypeError();
   }
-  if (!('target' in event) || typeof event.target !== 'object' || event.target === null) {
+  if (
+    !("target" in event) ||
+    typeof event.target !== "object" ||
+    event.target === null
+  ) {
     throw new TypeError();
   }
-  if (!('value' in event.target)) {
+  if (!("value" in event.target)) {
     throw new TypeError();
   }
   const value = Number(event.target.value);
@@ -55,14 +76,14 @@ async function handleInputChange(event: unknown) {
     throw new TypeError();
   }
   if (value > 10) {
-    return errorMessage.value = ('The badges count can not be more than 10');
+    return (errorMessage.value = "The badges count can not be more than 10");
   }
   if (value < 0) {
-    return errorMessage.value = ('The badges count can not be negative number');
+    return (errorMessage.value = "The badges count can not be negative number");
   }
   if (errorMessage.value) {
-    errorMessage.value = ''
+    errorMessage.value = "";
   }
-  emit('update:badgesCount', value);
+  emit("update:badgesCount", value);
 }
 </script>
