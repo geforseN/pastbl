f7e08560900857305d3ef0fc71fd25cc4b7af07c
geforseN <semen.neb@gmail.com>
2023-10-02 14:06:21 +0700

    twitch-chat: make px width instead of rem

--- components/twitch-chat.vue
+++ components/twitch-chat.vue
@@ -4,7 +4,7 @@
     v-model="modelValue"
     placeholder="Enter a pasta"
     @keyup.enter="emit('enterPressed', $event)"
-    class="w-80 h-[60vh] max-h-[75vh] text box-content textarea leading-[19.5px] text-[13px] font-normal placeholder:text-lg placeholder:text-base-content caret-[#a970ff rounded focus-within:outline-[#a970ff] focus-within:outline-offset-0 focus-within:outline-4 border-base-content border-2 px-[16px] py-[8px] hover:px-[15px] hover:py-[7px] hover:border-[3px]"
+    class="w-[320px] h-[60vh] max-h-[75vh] text box-content textarea leading-[19.5px] text-[13px] font-normal placeholder:text-lg placeholder:text-base-content caret-[#a970ff rounded focus-within:outline-[#a970ff] focus-within:outline-offset-0 focus-within:outline-4 border-base-content border-2 px-[16px] py-[8px] hover:px-[15px] hover:py-[7px] hover:border-[3px]"
   />
 </template>
 <script setup lang="ts">
