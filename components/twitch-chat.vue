092547e2731f19eff73ccd2be06a6a6c38bee0a2
geforseN <semen.neb@gmail.com>
2023-10-03 18:56:42 +0700

    twitch-chat: change styles and add id

--- components/twitch-chat.vue
+++ components/twitch-chat.vue
@@ -1,10 +1,12 @@
 <template>
+  <!-- TODO make id as prop, use useAttrs or $attrs -->
   <textarea
+    id="twitch-chat"
     ref="textarea"
     v-model="input"
     placeholder="Enter a pasta"
     @keyup.enter.exact="(event) => emit('enterPressed', event)"
-    class="min-w-[320px] max-w-[320px] min-h-[7rem] max-h-[50vh] resize-none text box-content leading-[19.5px] text-[13px] font-normal placeholder:text-lg placeholder:text-base-content caret-[#a970ff] rounded focus-within:outline-[#a970ff] focus-within:outline-offset-0 focus-within:outline-4 border-base-content border-2 px-[16px] py-[8px] hover:px-[15px] hover:py-[7px] hover:border-[3px]"
+    class="min-w-[320px] max-w-[320px] min-h-[7rem] max-h-[50vh] resize-none px-4 py-2 text box-content leading-[19.5px] text-[13px] font-normal placeholder:text-lg placeholder:relative placeholder:bottom-1 placeholder:text-base-content caret-[#a970ff] rounded outline outline-2 outline-base-content hover:outline hover:outline-4 hover:outline-base-content focus-within:outline focus-within:outline-4 focus-within:outline-[#a970ff] hover:focus-within:outline hover:focus-within:outline-[5px] hover:focus-within:outline-[#a970ff]"
   />
 </template>
 <script setup lang="ts">
@@ -12,7 +14,6 @@ const modelValue = defineModel<string>({ required: true });
 const emit = defineEmits<{
   enterPressed: [event: KeyboardEvent];
 }>();
-
 const { textarea, input } = useTextareaAutosize({
   input: modelValue,
 });
