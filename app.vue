0c335575a5d60854a0b77cdc92eecd23ebad5f46
geforseN <geforsenjs@gmail.com>
2023-10-03 18:57:23 +0700

    fix ref button

--- app.vue
+++ app.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="relative">
     <v-header />
-    <!-- NOTE: client only is used here because pastaStore and pastasStore persist data in localStorage, which is client only -->
-    <!-- NOTE: for persist pinia-plugin-persistedstate is used, 
-      for nuxt3 by default it uses cookie persist, but 
-      for some reason, pasta with length over ~400 char is not saved in cookie, so 
-      localStorage is used for persist, which does not behave like that with long pasta text
-    -->
     <main
       class="flex flex-col gap-y-4 items-center min-[920px]:items-start min-[920px]:flex-row justify-center gap-x-12 w-full mt-2"
     >
+      <!-- NOTE: client only is used here because pastaStore and pastasStore persist data in localStorage, which is client only -->
+      <!-- NOTE: for persist pinia-plugin-persistedstate is used, 
+        for nuxt3 by default it uses cookie persist, but 
+        for some reason, pasta with length over ~400 char is not saved in cookie, so 
+        localStorage is used for persist, which does not behave like that with long pasta text
+      -->
       <client-only>
         <div class="flex flex-col-reverse min-[920px]:flex-col">
           <pasta-list>
@@ -17,11 +17,18 @@
               <user-nickname :user="userStore.user" />
             </template>
           </pasta-list>
-          <button class="my-2 btn btn-primary w-full text-xl" @click="">
+          <button
+            class="my-2 btn btn-primary w-full text-xl"
+            @click="
+              () => {
+                console.log((pastaFromResponsiveRef as any).twitchChatRef.textareaRef.focus());
+              }
+            "
+          >
             go create pasta
           </button>
         </div>
-        <pasta-form-responsive />
+        <pasta-form-responsive ref="pastaFromResponsiveRef" />
       </client-only>
     </main>
     <u-notifications>
@@ -36,6 +43,7 @@
 </template>
 
 <script setup lang="tsx">
+const pastaFromResponsiveRef = ref()
 const userStore = useUserStore();
 </script>
 <style>
