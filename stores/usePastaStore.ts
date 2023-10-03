8a5a12e2891c0c409d0ba2a503212af6c8f81017
geforseN <geforsenjs@gmail.com>
2023-10-03 18:54:39 +0700

    pasta-form-responsive: change

--- stores/usePastaStore.ts
+++ stores/usePastaStore.ts
@@ -3,8 +3,27 @@ import { defineStore } from "pinia";
 export const usePastaStore = defineStore(
   "pasta",
   () => {
+    const pasta = usePasta();
+    const toast = useToast();
+
+    async function handleTagAddToPasta() {
+      try {
+        await pasta.addTag(pasta.tag.value);
+      } catch (error) {
+        if (!(error instanceof ExtendedError)) {
+          throw error;
+        }
+        toast.add({
+          description: error.description,
+          title: error.title,
+          color: error.color,
+        });
+      }
+    }
+
     return {
-      ...usePasta(),
+      ...pasta,
+      handleTagAddToPasta,
     };
   },
   {
