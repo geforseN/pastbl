afd45b6a6a2626cdf70659be65de25b2e580d987
geforseN <semen.neb@gmail.com>
2023-10-04 18:14:45 +0700

    v-header: change
    add nav > ul > li and use display: contents
    change text and border colors
    remove useColorMode (has done nothing)

--- components/v-header.vue
+++ components/v-header.vue
@@ -1,17 +1,23 @@
 <template>
   <header class="navbar h-8 justify-between border-b border-b-base-300 p-0">
-    <span
-      class="h-full border-2 border-neutral bg-secondary p-2 text-5xl font-bold"
-    >
-      pastbl
-    </span>
-    <button
-      class="btn btn-secondary mr-2 justify-self-end border-4 border-neutral text-base"
-      data-toggle-theme="light,dark"
-      data-act-class="ACTIVECLASS"
-    >
-      toggle theme
-    </button>
+    <nav class="contents">
+      <ul class="contents">
+        <li
+          class="h-full border-2 border-neutral bg-secondary p-2 text-5xl font-bold text-base-content"
+        >
+          pastbl
+        </li>
+        <li>
+          <button
+            class="btn btn-secondary mr-2 justify-self-end border-2 border-base-content text-base-content"
+            data-toggle-theme="light,dark"
+            data-act-class="ACTIVECLASS"
+          >
+            toggle theme
+          </button>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -22,10 +28,6 @@ import { themeChange } from "theme-change";
 // TODO: use cookie instead of session storage
 // NOTE: theme-change does not provide cookie save, only session
 onMounted(() => themeChange(false));
-
-const colorMode = useColorMode();
-
-console.log(colorMode);
 
 onMounted(() => {
   document.documentElement.classList.remove("dark", "light");
