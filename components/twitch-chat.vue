131d8b6279acacccb5717406a11fdb8cf9c969ba
geforseN <semen.neb@gmail.com>
2023-10-04 15:35:17 +0700

    twitch-chat: add box-content to fix size

--- components/twitch-chat.vue
+++ components/twitch-chat.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- TODO make id as prop, use useAttrs or $attrs -->
   <textarea
-    class="twitch-text max-h-[50vh] min-h-[7rem] min-w-[320px] max-w-[320px] resize-none rounded px-4 py-2 text-[13px] font-normal leading-[19.5px] caret-twitch outline outline-2 outline-base-content placeholder:relative placeholder:bottom-1 placeholder:text-lg placeholder:text-base-content focus-within:outline focus-within:outline-4 focus-within:outline-twitch hover:outline hover:outline-4 hover:outline-base-content hover:focus-within:outline hover:focus-within:outline-[5px] hover:focus-within:outline-twitch"
+    class="twitch-text box-content max-h-[50vh] min-h-[7rem] min-w-[320px] max-w-[320px] resize-none rounded px-4 py-2 text-[13px] font-normal leading-[19.5px] caret-twitch outline outline-2 outline-base-content placeholder:relative placeholder:bottom-1 placeholder:text-lg placeholder:text-base-content focus-within:outline focus-within:outline-4 focus-within:outline-twitch hover:outline hover:outline-4 hover:outline-base-content hover:focus-within:outline hover:focus-within:outline-[5px] hover:focus-within:outline-twitch"
     id="twitch-chat"
     v-model="input"
     ref="textarea"
