aff257596cc906dbf6a6c1c9bafed554d37b0dee
geforseN <geforsenjs@gmail.com>
2023-10-05 17:27:51 +0700

    add-pasta-tags: add datalist for input

--- components/add-pasta-tags.vue
+++ components/add-pasta-tags.vue
@@ -7,7 +7,24 @@
       type="text"
       @keyup.enter.prevent="emitTag"
       placeholder="Enter a pasta tag (optional)"
+      list="add-tag-suggestions"
     />
+    <datalist id="add-tag-suggestions">
+      <!-- NOTE: 
+        TRIED to use slot as 'was used {{ count }} {{ count === 1 ? "time" : "times" }}' and :value="tag"
+        but it did failed because firefox showed slot value only, which is not what wanted
+        chrome however works great, showing value attribute with smaller slot text below 
+        SO implemented <option /> uses value, label and no slot, which works ok:
+        firefox shows label only, value used onclick
+        chrome shows value and label below, value used onclick
+      -->
+      <option
+        v-for="[tag, count] of pastasStore.allTagsMapSortedByMostPopular"
+        :key="tag"
+        :value="tag"
+        :label="`${tag}, was used ${count} ${count === 1 ? 'time' : 'times'}`"
+      />
+    </datalist>
     <button
       class="btn join-item box-content border border-base-content px-2 text-3xl hover:border hover:border-base-content hover:bg-secondary/5 xl:min-w-[160px] xl:px-0"
       @click.prevent="emitTag"
@@ -31,4 +48,6 @@ function emitTag() {
     modelValue.value = "";
   }
 }
+// TODO: ? remove usePastasStore usage, add prop for it ?
+const pastasStore = usePastasStore();
 </script>
