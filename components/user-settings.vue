38acd54d088d1de2f3061f60fbdab0be0b9f9d54
geforseN <geforsenjs@gmail.com>
2023-10-08 17:17:40 +0700

    user-settings: update styles

--- components/user-settings.vue
+++ components/user-settings.vue
@@ -1,10 +1,8 @@
 <template>
-  <section class="rounded border-2 border-base-content p-2">
-    <h2 class="mb-2 border-b p-2 text-3xl font-bold">User settings</h2>
-    <div class="flex w-full flex-col gap-y-4">
-      <div
-        class="grid w-full grid-cols-[2fr,4fr] rounded-md border-y-2 px-2 pb-2 pt-6"
-      >
+  <section class="divide-y-2 rounded border-2 border-base-content p-2">
+    <h2 class="p-2 text-3xl font-bold">User settings</h2>
+    <div class="grid grid-cols-1 divide-y divide-base-content">
+      <div class="grid w-full grid-cols-1 p-2">
         <label class="label" for="badges-count">
           <span class="text-xl font-medium">Badges count</span>
         </label>
@@ -12,9 +10,7 @@
           v-model:badges-count.number="userStore.user.badges.count"
         />
       </div>
-      <div
-        class="grid w-full grid-cols-[2fr,4fr] rounded-md border-y-2 px-2 pb-1 pt-6"
-      >
+      <div class="grid w-full grid-cols-1 p-2">
         <label class="label" for="nickname">
           <span class="text-xl font-medium">Nickname</span>
         </label>
