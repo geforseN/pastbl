bd8e2e2c1ee80c526334782d91ba43d82db9cffa
geforseN <geforsenjs@gmail.com>
2023-10-08 15:59:44 +0700

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative">
+    <v-header />
+    <slot />
+    <u-notifications>
+      <template #title="{ title }">
+        <span class="text-xl">{{ title }}</span>
+      </template>
+      <template #description="{ description }">
+        <span class="font-bold">{{ description }}</span>
+      </template>
+    </u-notifications>
+  </div>
+</template>
