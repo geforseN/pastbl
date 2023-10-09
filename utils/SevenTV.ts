6057800a601af82ef398b3026a238dc3ed1ba7a8
geforseN <geforsenjs@gmail.com>
2023-10-09 18:45:06 +0700

    SevenTV: add SevenTVEmoteString

--- utils/SevenTV.ts
+++ utils/SevenTV.ts
@@ -1,3 +1,5 @@
+import type { SevenTvEmote } from "#imports";
+
 export class SevenTV {
   shouldLog = false;
 
@@ -13,4 +15,17 @@ export class SevenTV {
         return new SevenTVUser(streamerProfile);
       });
   }
+}
+
+export function SevenTVEmoteString(emote: SevenTvEmote) {
+  return String.raw`
+    <span
+      class="inline-block"
+      title="${emote.chatName} emote from SevenTV ${
+        !emote.originalName ? "" : `(aka ${emote.originalName})`
+      }"
+    >
+      <img src="https:${emote.url}/1x.webp">
+    </span>
+  `;
 }
