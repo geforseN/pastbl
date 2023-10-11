816d6c9c78db8cd08ed16c2aa9eed7df572e6f55
geforseN <semen.neb@gmail.com>
2023-10-11 18:44:02 +0700

    useEmotes: add composable

---
+++ composables/useEmotes.ts
@@ -0,0 +1,35 @@
+import { EmoteMap } from "~/integrations";
+
+export default function useEmotes(
+  emoteCollectionsGetters: Promise<EmoteMap>[],
+) {
+  const emoteCollections = ref<EmoteMap[]>([]);
+  const failedEmoteCollectionsReasons = ref<EmoteMap[]>([]);
+  const isLoaded = ref(false);
+
+  Promise.allSettled<Promise<EmoteMap>>(emoteCollectionsGetters).then(
+    (settledEmotesCollections) => {
+      emoteCollections.value = settledEmotesCollections
+        .filter(
+          (collection): collection is PromiseFulfilledResult<EmoteMap> =>
+            collection.status === "fulfilled",
+        )
+        .flatMap((result) => result.value);
+
+      failedEmoteCollectionsReasons.value = settledEmotesCollections
+        .filter(
+          (collection): collection is PromiseRejectedResult =>
+            collection.status === "rejected",
+        )
+        .map((result) => result.reason);
+      import.meta.env.DEV &&
+        console.log(failedEmoteCollectionsReasons, emoteCollections);
+    },
+  );
+
+  return {
+    isLoaded,
+    emoteCollections,
+    failedEmoteCollectionsReasons,
+  };
+}
