1869b10762f7b250f644df334ea37bd2d0d2b17f
geforseN <semen.neb@gmail.com>
2023-10-11 16:04:36 +0700

    integrations: add index file

---
+++ integrations/index.ts
@@ -0,0 +1,16 @@
+import { BetterTTVEmoteString } from "./BetterTTV";
+import { sevenTV } from "./SevenTV";
+import type { BetterTTVEmote } from "./BetterTTV/BetterTTV";
+import type { SevenTvEmote } from "./SevenTV/SevenTV.types";
+
+export const templateStrings = {
+  BetterTTV: BetterTTVEmoteString,
+  SevenTV: sevenTV.getEmoteTemplateString,
+  default: (emote: Emote) =>
+    `<span class="inline-block" title="${emote.chatName} emote"><img src="https:${emote.url}/1x.webp"></span>`,
+};
+
+export type Emote = SevenTvEmote | BetterTTVEmote;
+
+type EmoteName = string;
+export type EmoteMap = Map<EmoteName, Emote>;
