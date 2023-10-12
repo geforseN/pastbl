3fdb184d6e8efeb1479e972e94b1ea5ef1c3c012
geforseN <geforsenjs@gmail.com>
2023-10-12 20:00:06 +0700

    integrations index file: change
    add interfaces
    change imports
    change Emote type

--- integrations/index.ts
+++ integrations/index.ts
@@ -1,16 +1,17 @@
+import type { BetterTTVEmote } from "./BetterTTV";
+// import type { SevenTvEmote } from "./SevenTV";
+
 import { BetterTTVEmoteString } from "./BetterTTV";
 import { sevenTV } from "./SevenTV";
-import type { BetterTTVEmote } from "./BetterTTV/BetterTTV.client";
-import type { SevenTvEmote } from "./SevenTV/SevenTV.types";
 
 export const templateStrings = {
   BetterTTV: BetterTTVEmoteString,
   SevenTV: sevenTV.getEmoteTemplateString,
   default: (emote: Emote) =>
-    `<span class="inline-block" title="${emote.chatName} emote"><img src="https:${emote.url}/1x.webp"></span>`,
+    `<span class="inline-block" title="${emote.token} emote"><img src="https:${emote.url}/1x.webp"></span>`,
 };
 
-export type Emote = SevenTvEmote | BetterTTVEmote;
+export type Emote = BetterTTVEmote; // TODO uncomment // | SevenTvEmote;
 
 type EmoteName = string;
 export type EmoteMap = Map<EmoteName, Emote>;
@@ -19,3 +20,20 @@ export {
   getBttvEmoteCollectionByUserId,
   getBttvGlobalEmoteCollection,
 } from "./BetterTTV";
+
+export interface EmoteCollection {
+  emotes: BaseEmote[];
+  source: "BetterTTV" | "SevenTV" | "FrankerFaceZ" | "Twitch";
+  updatedAt: ReturnType<(typeof Date)["now"]>;
+  // TODO add property => version: number
+}
+
+export interface BaseEmote {
+  id: string;
+  url: string;
+  token: string;
+  isAnimated: boolean;
+  isModifier: boolean;
+  isListed: boolean;
+  source: string;
+}
