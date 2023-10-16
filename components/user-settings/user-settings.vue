2d098eecf07e72de7385302dd3aba3100056ed5a
geforseN <geforsenjs@gmail.com>
2023-10-16 23:17:08 +0700

    user-settings: update heading text

--- components/user-settings/user-settings.vue
+++ components/user-settings/user-settings.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="divide-y-2 rounded border-2 border-base-content p-2">
-    <h2 class="p-2 text-3xl font-bold">User settings</h2>
+    <h2 class="p-2 text-3xl font-bold">Change user settings</h2>
     <div class="grid grid-cols-1 divide-y divide-base-content">
       <div class="grid w-full grid-cols-1 p-2">
         <label class="label" for="badges-count">
