9c23d2f150eccc302883744135c0354a0ad381a0
geforseN <geforsenjs@gmail.com>
2023-10-16 19:56:25 +0700

    emote-collection-user-loaded-data: add

---
+++ components/emote-collection/emote-collection-user-loaded-data.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="flex w-full grow bg-slate-600/20 2xl:max-w-sm"
+    v-if="collections.ffz.state.value"
+  >
+    <img
+      width="64"
+      height="64"
+      :src="collections.ffz.state.value.user.avatar"
+      :alt="collections.ffz.state.value.user.display_name + ' avatar'"
+    />
+    <div class="ml-2">
+      <template v-if="collections.ffz.state.value.user.badges">
+        <img
+          class="inline-block h-twitch-badge w-twitch-badge"
+          v-for="badge of Object.values(collections.ffz.state.value.badges)"
+          :style="{ backgroundColor: badge.color }"
+          :src="badge.image"
+          :alt="badge.title + ' badge'"
+          :title="badge.title"
+        />
+      </template>
+      <span class="ml-1">
+        {{ collections.ffz.state.value.user.display_name }}
+      </span>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+defineProps<{ collections: ReturnType<typeof useAsyncEmoteSets> }>();
+</script>
