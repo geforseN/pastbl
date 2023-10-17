5ef12167b029d3453b20fc8527c17cd7fdda9d43
geforseN <semen.neb@gmail.com>
2023-10-17 17:49:22 +0700

    emote-collection-collapsed-set: change
     lint
     change generic name
     remove set prop for slots

--- components/emote-collection/emote-collection-collapsed-set.vue
+++ components/emote-collection/emote-collection-collapsed-set.vue
@@ -6,23 +6,22 @@
       @keypress.enter.exact="isOpen = !isOpen"
     />
     <header class="collapse-title">
-      <slot name="title" :set="props.set" />
+      <slot name="title" />
     </header>
     <main class="collapse-content -mb-4 p-0">
       <!-- NOTE: div has v-if for lazy loading images, so user will start fetch images only when collapse component open    -->
       <!-- NOTE: v-show wont help with lazy loading, images will be loaded even if collapse is closed    -->
       <div v-if="isOpen">
-        <slot name="emoteList" :set="props.set" :is-open="isOpen" />
+        <slot name="emoteList" :is-open="isOpen" />
       </div>
     </main>
   </div>
 </template>
-
-<script lang="ts" setup generic="Set">
+<script lang="ts" setup generic="EmoteSet">
 const isOpen = ref(false);
-const props = defineProps<{ set: Set }>();
-const slots = defineSlots<{
-  title: (titleProps: { set: Set }) => any;
-  emoteList: (listProps: { set: Set; isOpen: boolean }) => any;
+defineProps<{ set: EmoteSet }>();
+defineSlots<{
+  title: () => any;
+  emoteList: (listProps: { isOpen: boolean }) => any;
 }>();
 </script>
