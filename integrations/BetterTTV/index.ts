25e0f630bbad2803f0da3300f997b3c0c2bddf6b
geforseN <semen.neb@gmail.com>
2023-10-22 21:31:21 +0700

    add ffz emote string, update imports and types

--- integrations/BetterTTV/index.ts
+++ integrations/BetterTTV/index.ts
@@ -3,3 +3,5 @@ import type { BetterTTVEmote } from "./entity/BetterTTVEmote";
 export function BTTVEmoteString(emote: BetterTTVEmote) {
   return `<span class="inline-block" title="${emote.token} emote from BetterTTV"><img src="https:${emote.url}/1x.webp"></span>`;
 }
+
+export { createBTTVGlobalCollection } from "./entity/createBTTVGlobalCollection";
