eaf11f2ba81a294071f81734f25bb6e062178e93
geforseN <geforsenjs@gmail.com>
2023-10-29 16:54:40 +0700

    update indexedDB files

--- client-only/IndexedDB.ts
+++ client-only/IndexedDB.ts
@@ -1,21 +1,6 @@
 import { openDB, type DBSchema, type IDBPDatabase } from "idb";
 import type { Emote, EmoteCollection, EmoteSet } from "~/integrations";
 
-interface LocalPastasDB extends DBSchema {
-  pastas: {
-    key: number;
-    value: any;
-  };
-  archive: {
-    key: number;
-    value: any;
-  };
-  redacted: {
-    key: number;
-    value: any;
-  };
-}
-
 type Profile = {
   twitch: {
     nickname: string;
@@ -35,7 +20,7 @@ type Profile = {
 
 interface UsersEmoteCollectionsDB extends DBSchema {
   profiles: {
-    key: string /* twitch nickname */;
+    key: string;
     value: Profile;
   };
   emotes: {
@@ -45,14 +30,15 @@ interface UsersEmoteCollectionsDB extends DBSchema {
       byId: Emote["id"];
       bySource: "BetterTTV" | "SevenTV" | "FrankerFaceZ" | "Twitch";
       byToken: Emote["token"];
+      byTags: string[];
     };
   };
 }
 
-export async function openUserEmoteCollectionsDB() {
+export function openUserEmoteCollectionsDB() {
   return openDB<UsersEmoteCollectionsDB>("user-emote-collections", 1, {
     upgrade(database) {
-      const usersStore = database.createObjectStore("profiles", {
+      database.createObjectStore("profiles", {
         keyPath: "twitch.nickname",
       });
       const emotesStore = database.createObjectStore("emotes", {
@@ -61,52 +47,32 @@ export async function openUserEmoteCollectionsDB() {
       emotesStore.createIndex("bySource", "source", { unique: false });
       emotesStore.createIndex("byId", "id", { unique: false });
       emotesStore.createIndex("byToken", "token", { unique: false });
+      // NOTE: tags only exist in 7TV emotes
+      emotesStore.createIndex("byTags", "tags", {
+        unique: false,
+        multiEntry: true,
+      });
     },
   });
 }
 
-// TODO: maybe add here updatedAt instead of entity
-function mapSet(emoteSet: EmoteSet) {
-  if (!emoteSet.emotes) {
-    throw new Error(
-      "Emote set does not have emotes, can not add id to IndexedDB collection",
-    );
-  }
-  const { emotes, ...setToSave } = {
-    ...emoteSet,
-    emoteIds: emoteSet.emotes.map((emote) => emote.id),
-  } satisfies EmoteSet & { emoteIds: Emote["id"][] };
-
-  return { ...setToSave };
-}
-
-function mapEmote(emote: Emote) {
-  return {
-    ...emote,
-    updatedAt: Date.now(),
-  };
-}
-
-export function getMappedEmotesFromIdb(
-  profile: Profile,
+export function addUserEmotesToDB(
+  userEmotes: Emote[],
   db: IDBPDatabase<UsersEmoteCollectionsDB>,
 ) {
-  return Object.entries(profile.collections).reduce(
-    async (record, [key, collection]) => {
-      const emotes = await db.getAllFromIndex(
-        "emotes",
-        "bySource",
-        key as "BetterTTV" | "SevenTV" | "FrankerFaceZ" | "Twitch",
-      );
-      record[key as "BetterTTV" | "SevenTV" | "FrankerFaceZ" | "Twitch"] =
-        emotes.filter((emote) =>
-          collection.sets.some((set) => set.emoteIds.includes(emote.id)),
-        );
-      return record;
-    },
-    {} as Record<
-      "BetterTTV" | "SevenTV" | "FrankerFaceZ" | "Twitch",
-      (Emote & { updatedAt: number })[]
-    >,
+  const emoteStore = db
+    .transaction("emotes", "readwrite")
+    .objectStore("emotes");
+  return Promise.all(
+    userEmotes.map((emote) =>
+      emoteStore.put({ ...emote, updatedAt: Date.now() }),
+    ),
   );
+}
+
+export function addUserProfileToDB(db: IDBPDatabase<UsersEmoteCollectionsDB>) {
+  const profileStore = db
+    .transaction("profiles", "readwrite")
+    .objectStore("profiles");
+  console.log({ profileStore, TODO: "go make me work" });
 }
