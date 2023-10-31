9d75a8f479e865956f302e4b979d9afb7675425b
geforseN <geforsenjs@gmail.com>
2023-10-31 21:55:34 +0700

    change default layout
    add bottom nav
    change header to top-nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="relative">
-    <v-header />
+  <div class="relative grid grid-rows-layout">
+    <top-nav />
     <slot />
+    <nav
+      class="btm-nav flex-row gap-x-2 border-t-2 border-t-primary px-2 md:hidden"
+    >
+      <ol class="contents">
+        <li class="w-full">
+          <nuxt-link
+            class="btn btn-secondary w-full bg-secondary/10 text-secondary hover:bg-secondary/20"
+            to="/collections"
+          >
+            load emotes
+          </nuxt-link>
+        </li>
+        <li class="w-full">
+          <button
+            class="btn btn-info w-full bg-info/10 text-info hover:bg-info/20"
+            for="search-pasta"
+            @click="openFindPastaModal"
+          >
+            Find Pasta
+          </button>
+        </li>
+      </ol>
+    </nav>
     <u-notifications>
       <template #title="{ title }">
         <span class="text-xl">{{ title }}</span>
@@ -12,3 +35,9 @@
     </u-notifications>
   </div>
 </template>
+<script setup lang="ts">
+// TODO: add logic fro openFindPastaModal
+function openFindPastaModal() {
+  return "TODO";
+}
+</script>
