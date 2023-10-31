399eb3c47ac57e2d194f0768a93db69091e832d0
geforseN <semen.neb@gmail.com>
2023-10-31 12:50:09 +0700

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,9 +11,9 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:vue/vue3-essential",
     "@nuxtjs/eslint-config-typescript",
+    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
   overrides: [
