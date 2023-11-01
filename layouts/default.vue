a96951fb53137946895b9396db72f7a8692ca87a
geforseN <geforsenjs@gmail.com>
2023-11-01 14:13:43 +0700

    default layout: update styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,21 +2,20 @@
   <div class="relative grid grid-rows-layout">
     <top-nav />
     <slot />
-    <nav
-      class="btm-nav flex-row gap-x-2 border-t-2 border-t-primary px-2 md:hidden"
-    >
+    <nav class="btm-nav btm-nav-md flex-row gap-0 md:hidden">
       <ol class="contents">
-        <li class="w-full">
+        <li class="w-full grow">
           <nuxt-link
-            class="btn btn-secondary w-full bg-secondary/10 text-secondary hover:bg-secondary/20"
+            class="btn btn-secondary h-16 w-full rounded-none border-0 bg-secondary/10 text-secondary hover:bg-secondary/20 hover:underline focus:underline"
             to="/collections"
+            active-class=" border-t-2 border-t-secondary"
           >
             load emotes
           </nuxt-link>
         </li>
         <li class="w-full">
           <button
-            class="btn btn-info w-full bg-info/10 text-info hover:bg-info/20"
+            class="btn btn-info h-16 w-full rounded-none border-0 bg-info/10 text-info hover:bg-info/20"
             for="search-pasta"
             @click="openFindPastaModal"
           >
