05182443a0c420e6c5e43a88d539bf0a868e7838
geforseN <geforsenjs@gmail.com>
2023-11-02 23:23:23 +0700

    change UserNotFoundError

--- integrations/UserNotFoundError.ts
+++ integrations/UserNotFoundError.ts
@@ -1,1 +1,15 @@
-export class UserNotFoundError extends Error {}
+export class UserNotFoundError extends Error {
+  source: "BetterTTV" | "SevenTV" | "FrankerFaceZ";
+
+  constructor(
+    source: "BetterTTV" | "SevenTV" | "FrankerFaceZ",
+    username?: string,
+  ) {
+    super(
+      `⚠️ ${source} does not have ${
+        username ? `user with username ${username}` : "such user"
+      }`,
+    );
+    this.source = source;
+  }
+}
