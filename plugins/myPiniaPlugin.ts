12bc6e25ac75c45a29fc06ab0e7c9ff8216a11b7
geforseN <geforsenjs@gmail.com>
2023-11-05 16:32:20 +0700

    add pinia log plugin

---
+++ plugins/myPiniaPlugin.ts
@@ -0,0 +1,14 @@
+import type { PiniaPluginContext } from "pinia";
+
+function MyPiniaPlugin({ store }: PiniaPluginContext) {
+  store.$subscribe((mutation) => {
+    // eslint-disable-next-line no-console
+    console.log(`[🍍 ${mutation.storeId}]: ${mutation.type}`);
+  });
+  return { creationTime: new Date() };
+}
+
+export default defineNuxtPlugin(({ $pinia }) => {
+  // @ts-expect-error do not know how to TypeScript this
+  $pinia.use(MyPiniaPlugin);
+});
