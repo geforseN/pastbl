586fa2a2e0d24c5bbde3fae40836a6aa5e0aa24f
geforseN <geforsenjs@gmail.com>
2023-11-06 16:52:16 +0700

    update emote strings

--- integrations/BetterTTV/index.ts
+++ integrations/BetterTTV/index.ts
@@ -1,7 +1,7 @@
 import type { BetterTTVEmote } from "./entity/BetterTTVEmote";
 
 export function BTTVEmoteString(emote: BetterTTVEmote) {
-  return `<span class="inline-block" title="${emote.token} emote from BetterTTV"><img src="https:${emote.url}/1x.webp"></span>`;
+  return `<span class="inline-block" title="${emote.token} emote from BetterTTV"><img src="https:${emote.url}/1x.webp" loading="lazy"></span>`;
 }
 
 export { createBTTVGlobalCollection } from "./entity/createBTTVGlobalCollection";
