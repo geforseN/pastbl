ca82e1a1c2d1ac3eda75629390df60df91ce6a5a
geforseN <semen.neb@gmail.com>
2023-11-06 16:50:26 +0700

    create7TVGlobalCollection: change

--- integrations/SevenTV/entity/create7TVGlobalCollection.ts
+++ integrations/SevenTV/entity/create7TVGlobalCollection.ts
@@ -7,12 +7,10 @@ import { SevenTVEmote } from "./SevenTVEmote";
 import { SevenTVSet } from "./SevenTVSet";
 
 export function create7TVGlobalCollection(
-  sets: Awaited<ReturnType<typeof get7TVGlobalEmotesSet>>[],
+  set: Awaited<ReturnType<typeof get7TVGlobalEmotesSet>>,
 ): I7TVGlobalCollection {
-  const sevenTVSets = sets
-    .filter((set) => set.emotes?.length)
-    .map((set) => {
-      return new SevenTVSet(set, (emote) => new SevenTVEmote(emote, "global"));
-    });
+  const sevenTVSets = [
+    new SevenTVSet(set, (emote) => new SevenTVEmote(emote, "global")),
+  ];
   return new SevenTVGlobalCollection(sevenTVSets);
 }
