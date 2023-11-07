f177f5a378dc40b9cd587516fa0012ecaa84ee6f
geforseN <geforsenjs@gmail.com>
2023-11-07 19:21:07 +0700

    index page: update
    add emotes support
    change template

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,10 @@
   <main
     class="mt-2 flex w-full flex-col items-center justify-center gap-x-12 gap-y-4 go-brr:flex-row go-brr:items-start"
   >
-    <!-- NOTE: client only is used here because pastaStore and pastasStore persist data in localStorage, which is client only -->
-    <!-- NOTE: for persist pinia-plugin-persistedstate is used, 
-        for nuxt3 by default it uses cookie persist, but 
-        for some reason, pasta with length over ~400 char is not saved in cookie, so 
-        localStorage is used for persist, which does not behave like that with long pasta text
-      -->
-    <!-- <div class="loading loading-spinner animate-pulse" v-if="!isMounted"></div> -->
-
     <div class="flex flex-col-reverse go-brr:flex-col">
       <client-only>
         <chat-pasta-list>
-          <template #user-nickname>
+          <template #userNickname>
             <chat-pasta-nickname :user="userStore.user" />
           </template>
         </chat-pasta-list>
@@ -27,27 +19,53 @@
         >
           go create pasta
         </button>
-        <template #fallback>LOADING LIST AND BUTTON</template>
+        <template #fallback>
+          <div
+            class="flex h-[75vh] w-[414px] animate-pulse items-center justify-center bg-base-300"
+          >
+            <span class="loading" />
+          </div>
+        </template>
       </client-only>
     </div>
     <div class="flex w-min flex-col gap-y-4">
-      <client-only>
-        <pasta-form-responsive ref="pastaFormRef" />
-        <load-emote-collection-form />
-        <user-settings />
-        <template #fallback>LOADING FORMS</template>
-      </client-only>
+      <pasta-form-responsive ref="pastaFormRef" />
+      <load-emote-collection-form />
+      <user-settings />
     </div>
   </main>
 </template>
 
 <script setup lang="tsx">
-useHead({ title: "pastbl" });
+import { availableEmoteSources, globalEmotesGetters } from "~/integrations";
+
+useHead({ title: "pastbl", htmlAttrs: { lang: "en" } });
 
 const pastaFormRef = ref();
 const userStore = useUserStore();
 
-onMounted(async () => {});
+onMounted(() => {
+  tryAddGlobalEmotes();
+});
+
+async function tryAddGlobalEmotes() {
+  const dbs = await import("~/client-only/IndexedDB").then(({ openDBs }) =>
+    openDBs(),
+  );
+  const addedGlobalCollectionNames = await dbs.collectionsDB
+    .transaction("global")
+    .store.getAllKeys();
+  const sourcesToLoad = availableEmoteSources.filter(
+    (source) => !addedGlobalCollectionNames.includes(source),
+  );
+  for (const source of sourcesToLoad) {
+    const globalCollectionGetter = globalEmotesGetters[source];
+    const globalCollection = await globalCollectionGetter();
+    dbs.collectionsDB
+      .transaction("global", "readwrite")
+      .store.add(globalCollection);
+  }
+}
 </script>
 <style>
 html,
