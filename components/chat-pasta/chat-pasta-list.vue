ee4f131d12c0c5a14e7f5963328bc3dd5bf160c4
geforseN <geforsenjs@gmail.com>
2023-11-07 19:16:58 +0700

    chat-pasta-list: use composable

--- components/chat-pasta/chat-pasta-list.vue
+++ components/chat-pasta/chat-pasta-list.vue
@@ -30,7 +30,7 @@
           <button
             class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
             :disabled="!clipboard.isSupported.value"
-            @click="handleCopypastaCopy(pasta)"
+            @click="handlePastaCopy(pasta)"
           >
             Copy pasta
           </button>
@@ -45,51 +45,13 @@
     </chat-pasta>
   </div>
 </template>
-
 <script setup lang="ts">
 defineSlots<{
   userNickname: () => VNode;
 }>();
 
-const pastasStore = usePastasStore();
-const userStore = useUserStore();
-
 const clipboard = useClipboard();
-const toast = useToast();
+const pastasStore = usePastasStore();
 
-if (!clipboard.isSupported) {
-  toast.add({
-    description: "Your browser does not support Clipboard API",
-    title: "Copypasta problem",
-    timeout: 10_000,
-    color: "red",
-  });
-}
-
-async function handleCopypastaCopy(pasta: Pasta) {
-  try {
-    await clipboard.copy(pasta.text);
-    if (!clipboard.copied) {
-      throw new Error("Copypasta was not copied");
-    }
-    if (userStore.preferences.alerts.copypastaCopy.mustShowOnSuccess) {
-      toast.add({
-        description: "Copypasta copied successfully",
-        title: "Copypasta 🤙🤙🤙",
-        timeout: 1_700,
-      });
-    }
-    if (userStore.preferences.sounds.copypastaCopy.mustSoundOnSuccess) {
-      // TODO add useSound
-    }
-  } catch (error: Error | unknown) {
-    toast.add({
-      description:
-        error instanceof Error ? error.message : "Something went wrong",
-      title: "Copypasta problem",
-      timeout: 7_000,
-      color: "red",
-    });
-  }
-}
+const { handlePastaCopy } = usePastaCopy({ clipboard, pastasStore });
 </script>
