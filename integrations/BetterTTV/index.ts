7b61024adf77d0f0c3d2c632aafebfab3ade8106
geforseN <semen.neb@gmail.com>
2023-11-10 16:12:58 +0700

    integrations: add global collections type imports

--- integrations/BetterTTV/index.ts
+++ integrations/BetterTTV/index.ts
@@ -4,5 +4,6 @@ export function BTTVEmoteString(emote: BetterTTVEmote) {
   return `<span class="inline-block" title="${emote.token} emote from BetterTTV"><img src="https:${emote.url}/1x.webp" loading="lazy"></span>`;
 }
 
+export type { BetterTTVSet } from "./entity/BetterTTVSet";
 export { createBTTVGlobalCollection } from "./entity/createBTTVGlobalCollection";
 export { createBTTVUserCollection } from "./entity/createBTTVUserCollection";
--- integrations/FrankerFaceZ/index.ts
+++ integrations/FrankerFaceZ/index.ts
@@ -4,6 +4,7 @@ export function FFZEmoteString(emote: FrankerFaceZEmote) {
   return `<span class="inline-block" title="${emote.token} emote from FrankerFaceZ"><img src="https:${emote.url}/1" loading="lazy"></span>`;
 }
 
+export type { FrankerFaceZSet } from "./entity/FrankerFaceZSet";
 export { createFFZGlobalCollection } from "./entity/createFFZGlobalCollection";
 export { createFFZUserCollection } from "./entity/createFFZUserCollection";
 export { createFFZUserSets } from "./entity/createFFZUserSets";
