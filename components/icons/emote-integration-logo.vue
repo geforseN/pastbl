9d6d9ced3ecfa436f32c91eff7ae78ab9faa9290
geforseN <geforsenjs@gmail.com>
2023-11-10 16:12:06 +0700

    emote-integration-logo: add

---
+++ components/icons/emote-integration-logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <icons-ffz-logo v-if="props.source === 'FrankerFaceZ'" />
+  <icons-bttv-logo v-else-if="props.source === 'BetterTTV'" />
+  <icons-seventv-logo v-else-if="props.source === 'SevenTV'" />
+</template>
+
+<script lang="ts" setup>
+import type { AvailableEmoteSources } from "~/integrations";
+
+const props = defineProps<{ source: AvailableEmoteSources }>();
+</script>
