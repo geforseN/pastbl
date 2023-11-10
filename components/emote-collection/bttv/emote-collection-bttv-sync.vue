91d86fb630a91fb8f37bdd91588afbabdcd93f11
geforseN <geforsenjs@gmail.com>
2023-11-10 16:07:29 +0700

    emote-collection-list-sync: decompose

---
+++ components/emote-collection/bttv/emote-collection-bttv-sync.vue
@@ -0,0 +1,20 @@
+<template>
+  <li
+    class="flex flex-col divide-y-2 divide-[#63b3ed] border-2 border-[#63b3ed] bg-[#1a202c] p-2 text-white"
+  >
+    <emote-collection-header
+      v-bind="{ isError: false, isLoading: false, isReady: false }"
+    >
+      <h3>BetterTTV</h3>
+      <template #collection-logo>
+        <icons-bttv-logo class="max-h-[32px]" height="32" />
+      </template>
+    </emote-collection-header>
+    <emote-collection-bttv-sets class="pt-1" :sets="props.sets" />
+  </li>
+</template>
+<script lang="ts" setup>
+import type { BetterTTVSet } from "~/integrations/BetterTTV";
+
+const props = defineProps<{ sets: BetterTTVSet[] }>();
+</script>
---
+++ components/emote-collection/ffz/emote-collection-ffz-sync.vue
@@ -0,0 +1,28 @@
+<template>
+  <li
+    class="flex flex-col divide-y-2 divide-ffz border-2 border-ffz bg-[#222222] p-2 text-white"
+  >
+    <emote-collection-header
+      v-bind="{ isError: false, isLoading: false, isReady: false }"
+    >
+      <h3>FrankerFaceZ</h3>
+      <template #collection-logo>
+        <icons-ffz-logo class="max-h-[32px]" height="32" />
+      </template>
+    </emote-collection-header>
+    <emote-collection-ffz-sets
+      class="flex flex-col gap-1 pt-1"
+      :capacity="props.capacity"
+      :sets="props.sets"
+    />
+  </li>
+</template>
+
+<script lang="ts" setup>
+import type { FrankerFaceZSet } from "~/integrations/FrankerFaceZ/";
+
+const props = defineProps<{
+  sets: FrankerFaceZSet[];
+  capacity?: number;
+}>();
+</script>
