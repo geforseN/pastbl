0516b9f1caeef21f40738452c30b3ee523625fb9
geforseN <geforsenjs@gmail.com>
2023-11-18 14:05:22 +0700

    update composables
    remove not implemented composable
    use assert, add brackets

--- composables/TODO.useAsyncStateWithWarningRef.ts
+++
@@ -1,3 +0,0 @@
-export const useAsyncStateWithWarningRef = () => {
-  return ref();
-};
--- composables/usePasta.ts
+++ composables/usePasta.ts
@@ -14,28 +14,31 @@ export const usePasta = () => {
       tags.value = [];
       text.value = "";
     },
-    removeTag: (tagValue: string) => {
-      const tagIndex = tags.value.indexOf(tagValue);
-      if (tagIndex === -1) {
-        throw new ExtendedError("Can not remove the tag which is not in tags");
-      }
+    removeTag: (tagToRemove: string) => {
+      const tagIndex = tags.value.indexOf(tagToRemove);
+      assert.ok(
+        tagIndex >= 0,
+        new ExtendedError("Can not remove the tag which is not in tags"),
+      );
       tags.value.splice(tagIndex, 1);
     },
     removeAllTags: () => {
       tags.value = [];
     },
-    addTag: (newTag: PastaTag) => {
-      if (newTag.length === 0) {
-        throw new ExtendedError("Can not add the empty tag", {
+    addTag: (tagToAdd: PastaTag) => {
+      assert.ok(
+        tagToAdd.length !== 0,
+        new ExtendedError("Can not add the empty tag", {
           title: "Pasta tag was not added",
-        });
-      }
-      if (tags.value.includes(newTag)) {
-        throw new ExtendedError("The tag were already added to pasta", {
+        }),
+      );
+      assert.ok(
+        !tags.value.includes(tagToAdd),
+        new ExtendedError("The tag were already added to pasta", {
           title: "Pasta tag was not added",
-        });
-      }
-      tags.value.push(newTag);
+        }),
+      );
+      tags.value.push(tagToAdd);
     },
   };
 };
