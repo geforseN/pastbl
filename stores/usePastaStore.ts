b6edfae7055776547ee0be18a930d568b5a40632
geforseN <semen.neb@gmail.com>
2023-11-18 13:50:07 +0700

    remove pasta & pastas store persist

--- stores/usePastaStore.ts
+++ stores/usePastaStore.ts
@@ -1,34 +1,26 @@
 import { defineStore } from "pinia";
 
-export const usePastaStore = defineStore(
-  "pasta",
-  () => {
-    const pasta = usePasta();
-    const toast = useToast();
+export const usePastaStore = defineStore("pasta", () => {
+  const pasta = usePasta();
+  const toast = useToast();
 
-    function handleTagAddToPasta() {
-      try {
-        pasta.addTag(pasta.tag.value);
-      } catch (error) {
-        if (!(error instanceof ExtendedError)) {
-          throw error;
-        }
-        toast.add({
-          description: error.description,
-          title: error.title,
-          color: error.color,
-        });
+  function handleTagAddToPasta() {
+    try {
+      pasta.addTag(pasta.tag.value);
+    } catch (error) {
+      if (!(error instanceof ExtendedError)) {
+        throw error;
       }
+      toast.add({
+        description: error.description,
+        title: error.title,
+        color: error.color,
+      });
     }
+  }
 
-    return {
-      ...pasta,
-      handleTagAddToPasta,
-    };
-  },
-  {
-    persist: false && {
-      storage: persistedState.localStorage,
-    },
-  },
-);
+  return {
+    ...pasta,
+    handleTagAddToPasta,
+  };
+});
