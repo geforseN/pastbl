443e43994c10af3c5dbfe3f72d66f1032b73fe35
geforseN <semen.neb@gmail.com>
2023-11-29 20:55:00 +0700

    add nitroApp log plugin

---
+++ server/plugins/log.ts
@@ -0,0 +1,22 @@
+export default defineNitroPlugin((nitroApp) => {
+  nitroApp.hooks.hook("error", (error, { event }) => {
+    // eslint-disable-next-line no-console
+    console.error(`${event?.path} Application error:`, error);
+  });
+  nitroApp.hooks.hook("request", (event) => {
+    // eslint-disable-next-line no-console
+    console.log("on request", event.path);
+  });
+  nitroApp.hooks.hook("beforeResponse", (event /*, { body } */) => {
+    // eslint-disable-next-line no-console
+    console.log("on response", event.path /*, { body } */);
+  });
+  nitroApp.hooks.hook("afterResponse", (event /*, { body } = {} */) => {
+    // eslint-disable-next-line no-console
+    console.log("on after response", event.path /*, { body } */);
+  });
+  nitroApp.hooks.hookOnce("close", () => {
+    // eslint-disable-next-line no-console
+    console.log("on close");
+  });
+});
