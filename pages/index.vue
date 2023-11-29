c67647772b261af251111d8be572811c84e0d4c6
geforseN <geforsenjs@gmail.com>
2023-11-29 21:15:27 +0700

    index page: change column template (ugly)

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,11 @@
 <template>
-  <main
-    class="mt-2 flex w-full flex-col items-center justify-center gap-x-12 gap-y-4 go-brr:flex-row go-brr:items-start"
-  >
-    <div class="flex flex-col-reverse go-brr:flex-col">
-      <client-only>
-        <chat-pasta-list>
-          <template #userNickname>
-            <chat-pasta-nickname :user="userStore.user" />
-          </template>
-        </chat-pasta-list>
-        <button
-          class="btn btn-primary sticky top-2 my-2 w-full text-xl go-brr:bottom-2"
-          @click="
-              () => {
-                (pastaFormRef as any).twitchChatRef.textareaRef.focus();
-              }
-            "
-        >
-          go create pasta
-        </button>
-        <template #fallback>
-          <div
-            class="flex h-[75vh] w-[414px] animate-pulse items-center justify-center bg-base-300"
-          >
-            <span class="loading" />
-          </div>
-        </template>
-      </client-only>
-    </div>
+  <main>
+    <div>I'm from index.vue</div>
     <div class="flex w-min flex-col gap-y-4">
-      <pasta-form-responsive ref="pastaFormRef" />
-      <load-emote-collection-form />
-      <change-emote-collection />
-      <user-settings />
+      <nuxt-link to="/pastas/create">Create pasta</nuxt-link>
+      <nuxt-link to="/pastas/find">Find pasta</nuxt-link>
+      <nuxt-link to="/emotes">Emotes emotes</nuxt-link>
+      <nuxt-link to="/settings">Change settings</nuxt-link>
     </div>
   </main>
 </template>
@@ -41,9 +14,6 @@
 import { availableEmoteSources, globalEmotesGetters } from "~/integrations";
 
 useHead({ title: "pastbl", htmlAttrs: { lang: "en" } });
-
-const pastaFormRef = ref();
-const userStore = useUserStore();
 
 onMounted(() => {
   addMissingGlobalEmotesCollections();
