87953ab3865beede2a02af3e9b17db5f8dc1c78c
geforseN <semen.neb@gmail.com>
2023-12-01 15:46:58 +0700

    change layout and app.vue

--- app.vue
+++ app.vue
@@ -1,8 +1,32 @@
 <template>
   <div>
-    <nuxt-layout>
-      <!-- LINK: watch ./pages/index.vue -->
-      <nuxt-page />
-    </nuxt-layout>
+    <Head>
+      <Title>pastbl</Title>
+    </Head>
+    <Body lang="en">
+      <div class="relative grid grid-rows-layout">
+        <top-nav @find-pasta-button-clicked="navigateTo('/find-my-pasta')" />
+        <nuxt-layout>
+          <nuxt-page />
+        </nuxt-layout>
+        <bottom-nav @find-pasta-button-clicked="navigateTo('/find-my-pasta')" />
+        <u-notifications>
+          <template #title="{ title }">
+            <span class="text-xl">{{ title }}</span>
+          </template>
+          <template #description="{ description }">
+            <span class="font-bold">{{ description }}</span>
+          </template>
+        </u-notifications>
+      </div>
+    </Body>
   </div>
 </template>
+<script setup lang="ts">
+import { themeChange } from "theme-change";
+
+onMounted(() => {
+  themeChange(false);
+  document.documentElement.classList.remove("dark", "light");
+});
+</script>
