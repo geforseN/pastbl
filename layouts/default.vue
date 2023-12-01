c20b7522fe77c5fad5a783d2f2d9a90b56801a64
geforseN <geforsenjs@gmail.com>
2023-12-01 15:46:58 +0700

    change layout and app.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,19 @@
 <template>
-  <div class="relative grid grid-rows-layout">
-    <top-nav @find-pasta-button-clicked="navigateTo('/find-my-pasta')" />
-    <div
-      class="mt-2 flex flex-col items-center justify-center gap-x-12 gap-y-4 go-brr:flex-row go-brr:items-start"
-    >
-      <chat-pasta-list>
-        <template #creatorData>
-          <chat-pasta-creator-data
-            :badges-count="userStore.user.badges.count"
-            :nickname="userStore.user.nickname"
-            :nickname-color="userStore.user.preferences.nickname.color"
-          />
-        </template>
-      </chat-pasta-list>
-      <slot />
-    </div>
-    <bottom-nav @find-pasta-button-clicked="navigateTo('/find-my-pasta')" />
-    <u-notifications>
-      <template #title="{ title }">
-        <span class="text-xl">{{ title }}</span>
+  <div
+    class="mt-2 flex flex-col items-center justify-center gap-x-12 gap-y-4 go-brr:flex-row go-brr:items-start"
+  >
+    <chat-pasta-list>
+      <template #creatorData>
+        <chat-pasta-creator-data
+          :badges-count="userStore.user.badges.count"
+          :nickname="userStore.user.nickname"
+          :nickname-color="userStore.user.preferences.nickname.color"
+        />
       </template>
-      <template #description="{ description }">
-        <span class="font-bold">{{ description }}</span>
-      </template>
-    </u-notifications>
+    </chat-pasta-list>
+    <slot />
   </div>
 </template>
 <script setup lang="ts">
-import { themeChange } from "theme-change";
 const userStore = useUserStore();
-onMounted(() => {
-  themeChange(false);
-  document.documentElement.classList.remove("dark", "light");
-});
 </script>
