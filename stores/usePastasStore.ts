5f378c328e80614e460dc92ca32d1a7c8c45de5c
geforseN <semen.neb@gmail.com>
2023-12-02 21:35:08 +0700

    usePastasStore: change
    remove unused
     move lines
     lint

--- stores/usePastasStore.ts
+++ stores/usePastasStore.ts
@@ -6,6 +6,7 @@ import {
 } from "~/integrations";
 
 export type BasePasta = { text: string; tags: string[] };
+
 export type MegaPasta = BasePasta & {
   length: number;
   createdAt: number;
@@ -33,51 +34,41 @@ export function createMegaPasta(
   };
 }
 
+async function handleEmotePopulateForPastas(addedPastas: MegaPasta[]) {
+  const foundEmotes = await veryCoolAlgorithm(addedPastas);
+  for (const pasta of addedPastas) {
+    pasta.populatedText = pasta.populatedText || pasta.text;
+    for (const token of pasta.validTokens) {
+      const emote = foundEmotes.find((emote) => emote.token === token);
+      if (!emote) {
+        continue;
+      }
+      const emoteTemplate: (emote: IEmote) => string = templateStrings[
+        emote.source as AvailableEmoteSource
+      ] as (emote: IEmote) => string;
+      const emoteAsString = emoteTemplate(emote);
+      pasta.populatedText = pasta.populatedText.replaceAll(
+        token,
+        emoteAsString,
+      );
+    }
+  }
+}
+
 export const usePastasStore = defineStore("pastas", () => {
   const pastas = ref<IDBMegaPasta[]>([]);
-
-  const toast = useNuxtToast();
-
-  const allTags = computed(() => {
-    return [...new Set(pastas.value.flatMap((pasta) => pasta.tags))];
-  });
-
-  const mostPopularTagsMap = computed(() => {
-    return [
-      ...pastas.value
-        .flatMap((pasta) => pasta.tags)
-        .reduce((acc, tag) => {
-          const tagCount = acc.get(tag) || 0;
-          acc.set(tag, tagCount + 1);
-          return acc;
-        }, new Map<string, number>())
-        .entries(),
-    ].sort(([, aCount], [, bCount]) => bCount - aCount) satisfies [
-      tagValue: string,
-      tagCount: number,
-    ][];
-  });
-
-  watchArray(pastas, async (_, __, addedPastas) => {
-    const foundEmotes = await veryCoolAlgorithm(addedPastas);
-    for (const pasta of addedPastas) {
-      pasta.populatedText = pasta.populatedText || pasta.text;
-      for (const token of pasta.validTokens) {
-        const emote = foundEmotes.find((emote) => emote.token === token);
-        if (!emote) {
-          continue;
+  if (typeof window !== "undefined") {
+    import("~/client-only/IndexedDB/index")
+      .then(({ idb }) => idb.pastas)
+      .then((pastasIdb) => pastasIdb.getAllPastas())
+      .then((addedPastas) => {
+        if (process.dev) {
+          // eslint-disable-next-line no-console
+          console.log({ addedPastas });
         }
-        const emoteTemplate: (emote: IEmote) => string = templateStrings[
-          emote.source as AvailableEmoteSource
-        ] as (emote: IEmote) => string;
-        const emoteAsString = emoteTemplate(emote);
-        pasta.populatedText = pasta.populatedText.replaceAll(
-          token,
-          emoteAsString,
-        );
-      }
-    }
-  });
+        pastas.value = addedPastas;
+      });
+  }
 
   const pastas2 = useAsyncState(async () => {
     if (typeof window === "undefined") {
@@ -94,18 +85,11 @@ export const usePastasStore = defineStore("pastas", () => {
     return idbPastas;
   }, []);
 
-  if (typeof window !== "undefined") {
-    import("~/client-only/IndexedDB/index")
-      .then(({ idb }) => idb.pastas)
-      .then((pastasIdb) => pastasIdb.getAllPastas())
-      .then((addedPastas) => {
-        if (process.dev) {
-          // eslint-disable-next-line no-console
-          console.log({ addedPastas });
-        }
-        pastas.value = addedPastas;
-      });
-  }
+  const toast = useNuxtToast();
+
+  watchArray(pastas, (_, __, addedPastas) => {
+    handleEmotePopulateForPastas(addedPastas);
+  });
 
   function getPastaIndexById(id: IDBMegaPasta["id"]) {
     const index = pastas.value.findIndex((pasta) => pasta.id === id);
@@ -125,33 +109,50 @@ export const usePastasStore = defineStore("pastas", () => {
     })),
   );
 
+  const allTags = computed(() => {
+    return [...new Set(pastas.value.flatMap((pasta) => pasta.tags))];
+  });
+
   return {
-    allTags,
-    allTagsSortedByLength: computed(() =>
-      [...allTags.value].sort((a, b) => a.length - b.length),
+    pastas,
+    pastas2,
+    shallowRawPastas,
+    shallowRawNewestPastas: computed(() =>
+      [...shallowRawPastas.value].sort((a, b) => b.createdAt - a.createdAt),
     ),
+    allTags,
     allTagsSorted: computed(() =>
       [...allTags.value].sort((a, b) =>
         a.toLowerCase() > b.toLowerCase() ? 1 : -1,
       ),
     ),
-    shallowRawPastas,
-    shallowRawNewestPastas: computed(() =>
-      [...shallowRawPastas.value].sort((a, b) => b.createdAt - a.createdAt),
+    allTagsSortedByLength: computed(() =>
+      [...allTags.value].sort((a, b) => a.length - b.length),
     ),
+    mostPopularTagsEntries: computed(() => {
+      return [
+        ...pastas.value
+          .flatMap((pasta) => pasta.tags)
+          .reduce((acc, tag) => {
+            const tagCount = acc.get(tag) || 0;
+            acc.set(tag, tagCount + 1);
+            return acc;
+          }, new Map<string, number>())
+          .entries(),
+      ].sort(([, aCount], [, bCount]) => bCount - aCount) satisfies [
+        tagValue: string,
+        tagCount: number,
+      ][];
+    }),
     minPastaTextLengthInPastas: computed(() =>
       pastas.value.reduce((min, pasta) => Math.min(min, pasta.text.length), 0),
     ),
     maxPastaTextLengthInPastas: computed(() =>
       pastas.value.reduce((max, pasta) => Math.max(max, pasta.text.length), 0),
     ),
-    mostPopularTagsMap,
-    pastas,
-    pastas2,
     pastasSortedByNewest: computed(() =>
       [...pastas.value].sort((a, b) => b.createdAt - a.createdAt),
     ),
-    latestPasta: computed(() => pastas.value.at(-1)),
     async createPasta(basePasta: BasePasta) {
       if (basePasta.text.trim().length === 0) {
         throw new ExtendedError("Can not create pasta with empty text", {
