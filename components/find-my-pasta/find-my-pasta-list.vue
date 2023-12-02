559d5f7255c5b5fe5f526be5ee37150db2b47c32
geforseN <geforsenjs@gmail.com>
2023-12-02 23:59:24 +0700

    find-my-pasta-list: refactor
    remove full render list
    add one pasta render and one for left and right

--- components/find-my-pasta/find-my-pasta-list.vue
+++ components/find-my-pasta/find-my-pasta-list.vue
@@ -1,26 +1,98 @@
 <template>
-  <div
-    v-auto-animate
-    class="flex max-h-[78.3dvh] flex-col gap-y-2 overflow-y-auto"
-  >
-    <chat-pasta v-for="pasta of props.pastas" :key="pasta.id" :pasta="pasta">
-      <template #creatorData>
-        <chat-pasta-creator-data
-          :badges-count="userStore.user.badges.count"
-          :nickname="userStore.user.nickname"
-          :nickname-color="userStore.user.preferences.nickname.color"
-        />
-      </template>
-      <template #sidebar>
-        <button
-          class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
-          :disabled="!clipboard.isSupported.value"
-          @click="copyPasta(pasta)"
-        >
-          copy pasta
-        </button>
-      </template>
-    </chat-pasta>
+  <div>
+    <div class="join">
+      <button
+        class="btn btn-secondary join-item"
+        @click="
+          () => {
+            if (selectedPastaNumber > 1) {
+              selectedPastaNumber--;
+            } else {
+              selectedPastaNumber = props.pastas.length;
+            }
+          }
+        "
+      >
+        &lt&lt
+      </button>
+      <input
+        id="current-pasta-number"
+        :value="`${selectedPastaNumber} / ${props.pastas.length}`"
+        class="input join-item input-secondary"
+        readonly
+        name="current-pasta-number"
+      />
+      <button
+        class="btn btn-secondary join-item"
+        @click="
+          () => {
+            if (selectedPastaNumber < props.pastas.length) {
+              selectedPastaNumber++;
+            } else {
+              selectedPastaNumber = 1;
+            }
+          }
+        "
+      >
+        &gt&gt
+      </button>
+    </div>
+    <div class="relative">
+      <chat-pasta v-if="prevPasta" class="prev-pasta" :pasta="prevPasta">
+        <template #creatorData>
+          <chat-pasta-creator-data
+            :badges-count="userStore.user.badges.count"
+            :nickname="userStore.user.nickname"
+            :nickname-color="userStore.user.preferences.nickname.color"
+          />
+        </template>
+        <template #sidebar>
+          <button
+            class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
+            :disabled="!clipboard.isSupported.value"
+            @click="copyPasta(prevPasta)"
+          >
+            copy pasta
+          </button>
+        </template>
+      </chat-pasta>
+      <chat-pasta v-if="selectedPasta" :pasta="selectedPasta">
+        <template #creatorData>
+          <chat-pasta-creator-data
+            :badges-count="userStore.user.badges.count"
+            :nickname="userStore.user.nickname"
+            :nickname-color="userStore.user.preferences.nickname.color"
+          />
+        </template>
+        <template #sidebar>
+          <button
+            class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
+            :disabled="!clipboard.isSupported.value"
+            @click="copyPasta(selectedPasta)"
+          >
+            copy pasta
+          </button>
+        </template>
+      </chat-pasta>
+      <chat-pasta v-if="nextPasta" class="next-pasta" :pasta="nextPasta">
+        <template #creatorData>
+          <chat-pasta-creator-data
+            :badges-count="userStore.user.badges.count"
+            :nickname="userStore.user.nickname"
+            :nickname-color="userStore.user.preferences.nickname.color"
+          />
+        </template>
+        <template #sidebar>
+          <button
+            class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
+            :disabled="!clipboard.isSupported.value"
+            @click="copyPasta(nextPasta)"
+          >
+            copy pasta
+          </button>
+        </template>
+      </chat-pasta>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
@@ -28,9 +100,53 @@ const props = defineProps<{
   pastas: IDBMegaPasta[];
 }>();
 
+const selectedPastaNumber = ref(1);
+const selectedPastaIndex = computed(() => selectedPastaNumber.value - 1);
+const selectedPasta = computed(() => props.pastas[selectedPastaIndex.value]);
+
+const prevPasta = computed(() => {
+  if (props.pastas.length === 1) {
+    return;
+  }
+  return props.pastas.at(selectedPastaIndex.value - 1);
+});
+
+const nextPasta = computed(() => {
+  if (props.pastas.length === 1) {
+    return;
+  }
+  return props.pastas.at((selectedPastaIndex.value + 1) % props.pastas.length);
+});
+
+watch(
+  () => props.pastas.length,
+  () => {
+    selectedPastaNumber.value = 1;
+  },
+);
+
 const userStore = useUserStore();
 
 const clipboard = useClipboard();
 
 const { copyPasta } = usePastaCopy({ userStore, clipboard });
 </script>
+<style scoped>
+.prev-pasta,
+.next-pasta {
+  --pasta-width: 414px;
+  will-change: transform;
+  position: absolute;
+  top: 0;
+}
+
+.prev-pasta {
+  transform: rotateY(-55deg) translateZ(-100px);
+  right: var(--pasta-width);
+}
+
+.next-pasta {
+  transform: rotateY(55deg) translateZ(-100px);
+  left: var(--pasta-width);
+}
+</style>
