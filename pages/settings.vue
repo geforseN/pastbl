a7d9aaad6ca0da8e65113631d85bc0d12284c91f
geforseN <semen.neb@gmail.com>
2023-12-03 17:59:49 +0700

    change settings page

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <nuxt-link to="/">Go back</nuxt-link>
+  <div class="flex flex-col gap-2">
+    <div class="rounded-2xl border-2 border-base-content px-4 py-2">
+      <nuxt-link
+        to="/"
+        class="flex items-center justify-between gap-2 text-3xl font-bold"
+      >
+        <span>Go to main page</span>
+        <icon name="carbon:link" />
+      </nuxt-link>
+    </div>
     <user-settings />
   </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
