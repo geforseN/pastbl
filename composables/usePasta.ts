52c1854d60db97366047005d20ec9e947a381949
geforseN <geforsenjs@gmail.com>
2023-12-03 18:03:59 +0700

    move pasta types and utils

--- composables/usePasta.ts
+++ composables/usePasta.ts
@@ -1,9 +1,56 @@
-type PastaTag = string;
+export type BasePasta = {
+  text: string;
+  tags: string[];
+};
+
+export type MegaPasta = BasePasta & {
+  length: number;
+  createdAt: number;
+  populatedText?: string;
+  lastCopiedAt?: number;
+  validTokens: string[];
+};
+
+export type IDBMegaPasta = MegaPasta & {
+  id: number;
+};
+
+// LINK: http://facweb.cs.depaul.edu/sjost/it212/documents/ascii-pr.htm
+// NOTE: SPACE and DELETE is not included (SPACE === 32, DELETE === 127)
+// PROBABLY can drop more charCodes, but no idea which characters are valid for emote name
+function isValidASCIIChar(char: string) {
+  const charCode = char.charCodeAt(0);
+  return charCode > 32 && charCode < 127;
+}
+
+function isValidToken(word: string) {
+  for (const char of word) {
+    if (!isValidASCIIChar(char)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function createMegaPasta(
+  trimmedText: BasePasta["text"],
+  tags: BasePasta["tags"],
+): MegaPasta {
+  return {
+    tags: toRaw(tags),
+    text: trimmedText,
+    length: trimmedText.length,
+    createdAt: Date.now(),
+    validTokens: [...new Set(trimmedText.split(" "))].filter(isValidToken),
+    lastCopiedAt: undefined,
+    populatedText: undefined,
+  };
+}
 
 export const usePasta = () => {
-  const tag = ref<PastaTag>("");
-  const tags = ref<Pasta["tags"]>([]);
-  const text = ref<Pasta["text"]>("");
+  const tag = ref("");
+  const tags = ref<string[]>([]);
+  const text = ref("");
 
   return {
     tag,
@@ -25,7 +72,7 @@ export const usePasta = () => {
     removeAllTags: () => {
       tags.value = [];
     },
-    addTag: (tagToAdd: PastaTag) => {
+    addTag: (tagToAdd: string) => {
       assert.ok(
         tagToAdd.length !== 0,
         new ExtendedError("Can not add the empty tag", {
