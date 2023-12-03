ac611dc58f50a62d7108a13062cabc25a78598cb
geforseN <semen.neb@gmail.com>
2023-12-03 18:04:52 +0700

    update promise util

--- utils/promise.ts
+++ utils/promise.ts
@@ -1,19 +1,17 @@
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-constraint, @typescript-eslint/no-explicit-any
-export function tupleSettledPromises<T, E extends any = unknown>(
-  settledValues: PromiseSettledResult<T>[],
-): [T[], E[]] {
-  return [
-    settledValues
-      .filter(
-        (result): result is PromiseFulfilledResult<T> =>
-          result.status === "fulfilled",
-      )
-      .map((result) => result.value),
-    settledValues
-      .filter(
-        (result): result is PromiseRejectedResult =>
-          result.status === "rejected",
-      )
-      .map((result) => result.reason),
-  ];
+export async function tupleSettledPromises<T>(
+  values: ReadonlyArray<MaybePromise<T>>,
+): Promise<[Awaited<T>[], unknown[]]> {
+  const settledValues = await Promise.allSettled(values);
+  const fulfilled = settledValues
+    .filter(
+      (result): result is PromiseFulfilledResult<Awaited<T>> =>
+        result.status === "fulfilled",
+    )
+    .map((result) => result.value);
+  const rejected = settledValues
+    .filter(
+      (result): result is PromiseRejectedResult => result.status === "rejected",
+    )
+    .map((result) => result.reason);
+  return [fulfilled, rejected];
 }
