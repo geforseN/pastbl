1f3be6414d45f810a7253c11a17f7c9acb238852
geforseN <geforsenjs@gmail.com>
2023-12-03 22:49:09 +0700

    change pasta token utils

--- composables/usePasta.ts
+++ composables/usePasta.ts
@@ -18,14 +18,13 @@ export type IDBMegaPasta = MegaPasta & {
 // LINK: http://facweb.cs.depaul.edu/sjost/it212/documents/ascii-pr.htm
 // NOTE: SPACE and DELETE is not included (SPACE === 32, DELETE === 127)
 // PROBABLY can drop more charCodes, but no idea which characters are valid for emote name
-function isValidASCIIChar(char: string) {
-  const charCode = char.charCodeAt(0);
+function isValidASCIICharCode(charCode: number) {
   return charCode > 32 && charCode < 127;
 }
 
 function isValidToken(word: string) {
   for (const char of word) {
-    if (!isValidASCIIChar(char)) {
+    if (!isValidASCIICharCode(char.charCodeAt(0))) {
       return false;
     }
   }
