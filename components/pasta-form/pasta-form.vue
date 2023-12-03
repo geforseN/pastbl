31cc99082dccd52d0faf2b3307a2ad244ec19fb3
geforseN <geforsenjs@gmail.com>
2023-12-03 18:57:25 +0700

    update pasta form

--- components/pasta-form/pasta-form.vue
+++ components/pasta-form/pasta-form.vue
@@ -1,25 +1,32 @@
 <template>
-  <section
-    class="flex flex-col gap-y-2 rounded border-2 border-base-content p-2"
-  >
-    <slot name="header" />
-    <div
-      class="flex w-min flex-col gap-2 xl:w-full xl:flex-row xl:justify-between"
-    >
-      <slot name="textarea" />
+  <div class="flex flex-col gap-2">
+    <div class="flex flex-col gap-2 xl:w-full xl:flex-row xl:justify-between">
+      <pasta-form-textarea
+        id="twitch-chat-textarea"
+        ref="twitchChatRef"
+        v-model="pastaTextModel"
+        class="mx-0.5"
+        @enter-pressed="emit('createPasta')"
+      />
       <div
         class="flex flex-row-reverse items-center justify-between gap-1 xl:w-full xl:flex-col"
       >
-        <slot name="button" :dynamic-class="createPastaButtonClass" />
+        <button
+          ref="createPastaButton"
+          class="btn btn-primary h-max text-lg uppercase focus:outline-double focus:outline-4 focus:outline-offset-1 xl:w-full"
+          @click="emit('createPasta')"
+        >
+          create pasta
+        </button>
         <div class="flex h-full flex-col justify-between">
           <pasta-form-pasta-length
             :class="pastaLengthClass[pastaStatus]"
-            :pasta-text="pastaText.trim()"
+            :pasta-text="pastaTextModel.trim()"
           />
           <button
             v-if="props.pastaTags.length !== 0"
             class="btn btn-error btn-sm"
-            @click="() => emit('removeAllTags')"
+            @click="emit('removeAllTags')"
           >
             remove all tags
           </button>
@@ -32,7 +39,7 @@
       @remove-tag="(tag) => emit('removeTagFromPasta', tag)"
     />
     <pasta-form-tags-input
-      v-model="tagToAdd"
+      v-model="tagToAddModel"
       :must-become-empty-on-add="props.mustTagModelBecomeEmptyOnAdd"
       @add-tag="(tagToAdd) => emit('addTagToPasta', tagToAdd)"
     >
@@ -44,36 +51,33 @@
         firefox shows label only, value used onclick
         chrome shows value and label below, value used onclick -->
         <option
-          v-for="[tag, count] of pastasStore.mostPopularTagsMap"
+          v-for="[tag, count] of props.hintedTagsMap"
           :key="tag"
           :value="tag"
           :label="`${tag}, was used ${count} ${count === 1 ? 'time' : 'times'}`"
         />
       </template>
     </pasta-form-tags-input>
-  </section>
+  </div>
 </template>
 <script lang="ts" setup>
-const tagToAdd = defineModel<string>("tag", { default: "", local: true });
-const pastaText = defineModel<string>("text", { required: true, local: false });
-
-const pastasStore = usePastasStore();
-
-defineSlots<{
-  header: () => unknown;
-  button: (props: { dynamicClass: string }) => unknown;
-  textarea: () => unknown;
-}>();
+const tagToAddModel = defineModel<string>("tag", { default: "", local: true });
+const pastaTextModel = defineModel<string>("text", {
+  required: true,
+  local: false,
+});
 
 const props = defineProps<{
-  pastaTags: Pasta["tags"];
+  pastaTags: BasePasta["tags"];
   mustTagModelBecomeEmptyOnAdd?: boolean;
+  hintedTagsMap: [tagValue: string, tagCount: number][];
 }>();
 
 const emit = defineEmits<{
   addTagToPasta: [tag: string];
   removeTagFromPasta: [tag: string];
   removeAllTags: [];
+  createPasta: [];
 }>();
 
 const twitchChatRef = ref<HTMLInputElement>();
@@ -85,7 +89,7 @@ defineExpose({
 type PastaStatus = "error" | "warning" | "success";
 
 const pastaStatus = computed(() => {
-  const pastaTextLength = pastaText.value.trim().length;
+  const pastaTextLength = pastaTextModel.value.trim().length;
 
   if (pastaTextLength === 0 || pastaTextLength > 1000) {
     return "error" satisfies PastaStatus;
@@ -101,15 +105,4 @@ const pastaLengthClass = {
   warning: "text-warning",
   success: "text-success",
 };
-
-const createPastaButtonClass = computed(() => {
-  switch (pastaStatus.value) {
-    case "error":
-      return "bg-error focus:outline-error hover:bg-error/80";
-    case "warning":
-      return "bg-warning focus:outline-warning hover:bg-warning/80";
-    case "success":
-      return "bg-success focus:outline-success hover:bg-success/80";
-  }
-});
 </script>
