fced6784a2dfddc3d4511fc0ea3347676eacab04
geforseN <semen.neb@gmail.com>
2023-12-07 17:33:12 +0700

    add favicon :)

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
+    <Head>
+      <Link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <Link rel="manifest" href="/site.webmanifest" />
+    </Head>
     <Body lang="en">
       <div class="relative grid grid-rows-layout">
         <top-nav />
@@ -29,3 +49,21 @@ onMounted(() => {
   document.documentElement.classList.remove("dark", "light");
 });
 </script>
+
+<!-- 
+  pages:
+   - index: показывает все возможные действия, а  что именно смотри ниже  
+     - pastas/create
+     - pastas/edit/:pastaId
+     - pastas/find?...MUST_BE_MANY_PARAMS_HERE
+     - settings 
+
+  слева будет постоянно список паст
+  справа будет:
+   - форма для создания пасты
+   - форма для фильтрации паст 
+   - настройки пользователя
+   - все загруженные коллекции emotes (также там должен быть input для быстрой загрузки коллекции) 
+   - по нажатию на кнопку 'Change pasta' n-ой пасты
+     - форма для редактирования пасты 
+ -->
