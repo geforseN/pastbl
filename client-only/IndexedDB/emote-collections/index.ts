dcc9f1a9177967e0a9f9f63199461bcd3a330a07
geforseN <geforsenjs@gmail.com>
2023-12-10 17:04:13 +0700

    emote-collections idb: refactor

--- client-only/IndexedDB/emote-collections/index.ts
+++ client-only/IndexedDB/emote-collections/index.ts
@@ -2,6 +2,11 @@ import { openDB } from "idb";
 import { GlobalEmoteCollections } from "./GlobalEmoteCollections";
 import { UsersEmoteCollections } from "./UsersEmoteCollections";
 import { type EmoteCollectionsSchema } from "~/client-only/IndexedDB";
+import {
+  getGlobalCollection,
+  type AvailableEmoteSource,
+  type IGlobalEmoteCollection,
+} from "~/integrations";
 
 class EmoteCollections {
   // eslint-disable-next-line no-useless-constructor
@@ -11,11 +16,10 @@ class EmoteCollections {
   ) {}
 }
 
-const openEmoteCollectionsIdb = openDB<EmoteCollectionsSchema>(
-  "emote-collections",
-  3,
-  {
-    upgrade(database) {
+const openEmoteCollectionsIdb = async () =>
+  (typeof window !== "undefined" || {}) &&
+  openDB<EmoteCollectionsSchema>("emote-collections", 4, {
+    async upgrade(database, _, __, tx) {
       if (!database.objectStoreNames.contains("users")) {
         database.createObjectStore("users", {
           keyPath: "twitch.username",
@@ -26,14 +30,62 @@ const openEmoteCollectionsIdb = openDB<EmoteCollectionsSchema>(
           keyPath: "source",
         });
       }
+      if (!database.objectStoreNames.contains("key-value")) {
+        database.createObjectStore("key-value");
+      }
+      const activeUserCollectionUsername = await tx
+        .objectStore("key-value")
+        .get("active-user-collection-username");
+      if (activeUserCollectionUsername === undefined) {
+        await tx
+          .objectStore("key-value")
+          .put(null, "active-user-collection-username");
+      }
     },
-  },
-);
+  });
 
-export const emoteCollectionsIdb = openEmoteCollectionsIdb.then(
+export const emoteCollectionsIdb = openEmoteCollectionsIdb().then(
   (idb) =>
     new EmoteCollections(
       new GlobalEmoteCollections(idb),
       new UsersEmoteCollections(idb),
     ),
 );
+
+export async function addGlobalCollection(collection: IGlobalEmoteCollection) {
+  const { idb } = await import("~/client-only/IndexedDB");
+  const { global } = await idb.emoteCollections;
+  return await global.addCollection(collection);
+}
+
+export async function loadMissingGlobalCollections(
+  missingSources: AvailableEmoteSource[],
+) {
+  const [collections] = await tupleSettledPromises<IGlobalEmoteCollection>(
+    missingSources.map((source) => getGlobalCollection(source)),
+  );
+  const { addGlobalCollection } = await import(
+    "~/client-only/IndexedDB/emote-collections"
+  );
+  await Promise.all(
+    collections.map((collection) => addGlobalCollection(collection)),
+  );
+  return collections;
+}
+
+export async function refreshGlobalCollection(
+  collection: IGlobalEmoteCollection,
+) {
+  const { idb } = await import("~/client-only/IndexedDB");
+  const emoteCollections = await idb.emoteCollections;
+  const newIdbCollection =
+    await emoteCollections.global.refreshCollection(collection);
+  return newIdbCollection;
+}
+
+export async function getAllGlobalCollections() {
+  const emoteCollections = await import("~/client-only/IndexedDB").then(
+    ({ idb }) => idb.emoteCollections,
+  );
+  return emoteCollections.global.getAllCollections();
+}
