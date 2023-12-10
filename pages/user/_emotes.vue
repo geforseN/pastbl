981b9a99f7551084e977ff4c8a04c86b27aaf249
geforseN <geforsenjs@gmail.com>
2023-12-10 21:37:40 +0700

    _emotes: update

--- pages/user/_emotes.vue
+++ pages/user/_emotes.vue
@@ -1,49 +1,97 @@
 <template>
-  <div class="rounded-box border-2 p-2">
-    <div>Global emotes</div>
-    <div
-      role="tablist"
-      class="tabs tabs-lifted w-96 rounded-[10px] border border-base-300"
-    >
-      <template
-        v-for="source in ['BetterTTV', 'FrankerFaceZ', 'SevenTV']"
-        :key="source"
+  <div class="flex flex-col gap-2">
+    <global-emotes />
+    <div class="rounded-box border-2 p-2">
+      <label for="active-user-collection">Select active user collection</label>
+      <div class="flex flex-col">
+        <span
+          >state:
+          {{ userCollectionsStore.activeUserCollectionUsername.state }}
+          {{
+            typeof userCollectionsStore.activeUserCollectionUsername.state
+          }}</span
+        >
+        <span
+          >error :
+          {{ userCollectionsStore.activeUserCollectionUsername.error }}</span
+        >
+        <span
+          >isLoading:
+          {{
+            userCollectionsStore.activeUserCollectionUsername.isLoading
+          }}</span
+        >
+        <span
+          >isReady:
+          {{ userCollectionsStore.activeUserCollectionUsername.isReady }}</span
+        >
+      </div>
+      <select
+        v-show="userCollectionsStore.usernamesToSelect.isReady"
+        id="active-user-collection"
+        v-model="userCollectionsStore.activeUserCollectionUsername.state"
+        name="active-user-collection"
+        @change="
+          (event) => {
+            const { value } = event.target as HTMLSelectElement;
+            if (!value) {
+              return;
+            }
+            userCollectionsStore.activeUserCollectionUsername.execute(0, value);
+          }
+        "
       >
-        <input
-          type="radio"
-          name="my_tabs_2"
-          role="tab"
-          class="tab bg-base-300"
-          :aria-label="source"
-        />
-        <div role="tabpanel" class="tab-content rounded-box bg-base-100 p-2">
-          <emote-collection-bttv-sets
-            v-if="source === 'BetterTTV'"
-            class="pt-1"
-            :sets="
-              collectionsStore.global.getCollectionWithSource('BetterTTV')?.sets
-            "
-          />
-          <emote-collection-ffz-sets
-            v-else-if="source === 'FrankerFaceZ'"
-            class="flex flex-col gap-1 pt-1"
-            :sets="
-              collectionsStore.global.getCollectionWithSource('FrankerFaceZ')
-                ?.sets
-            "
-          />
-          <emote-collection-seventv-sets
-            v-else-if="source === 'SevenTV'"
-            class="pt-1"
-            :sets="
-              collectionsStore.global.getCollectionWithSource('SevenTV')?.sets
-            "
-          />
-        </div>
-      </template>
+        <option value="" />
+        <option
+          v-for="name of userCollectionsStore.usernamesToSelect.state"
+          :key="name"
+          :value="name"
+          class="bg-red-500"
+        >
+          {{ name }}
+        </option>
+      </select>
     </div>
+    <div class="flex">
+      <input
+        v-model="nickname"
+        class="input input-secondary grow"
+        type="text"
+      />
+      <button class="btn btn-secondary" @click="hg">Add collecn</button>
+    </div>
+    <go-to-main-page />
   </div>
 </template>
 <script setup lang="ts">
-const collectionsStore = useCollectionsStore();
+const userCollectionsStore = useUserCollectionsStore();
+
+const collections = useUserIntegrations();
+
+const nickname = ref("");
+
+async function hg() {
+  if (
+    !nickname.value ||
+    userCollectionsStore.usernamesToSelect.state.some(
+      (name) => name.toLowerCase() === nickname.value.toLowerCase(),
+    )
+  ) {
+    return;
+  }
+  const [emoteCollectionsIdb, emotesIdb] = await import(
+    "~/client-only/IndexedDB"
+  ).then(({ idb }) => Promise.all([idb.emoteCollections, idb.emotes]));
+
+  const collection =
+    (await collections.integrations.execute(0, nickname)) ||
+    raise("Failed to load emote collection");
+
+  await Promise.all([
+    emoteCollectionsIdb.users.putCollection(collection),
+    emotesIdb.putEmotesOfUserCollection(collection),
+  ]);
+  // TODO: add collection in store
+  // NOTE: 'collection' already is filled with emotes
+}
 </script>
