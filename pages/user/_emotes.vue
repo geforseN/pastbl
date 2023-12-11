24752cc4c8ffcfae044e878663e416320c07a209
geforseN <semen.neb@gmail.com>
2023-12-11 18:01:31 +0700

    _emotes: update

--- pages/user/_emotes.vue
+++ pages/user/_emotes.vue
@@ -1,88 +1,54 @@
 <template>
   <div class="flex flex-col gap-2">
     <global-emotes />
-    <div class="rounded-box border-2 p-2">
-      <label for="active-user-collection">Select active user collection</label>
-      <div class="flex flex-col">
-        <span
-          >state:
-          {{ userCollectionsStore.selectedCollectionUsername.state }}
-          {{ typeof userCollectionsStore.selectedCollectionUsername.state }}
-        </span>
-        <span
-          >error:
-          {{ userCollectionsStore.selectedCollectionUsername.error }}
-        </span>
-        <span
-          >isLoading:
-          {{ userCollectionsStore.selectedCollectionUsername.isLoading }}
-        </span>
-        <span
-          >isReady:
-          {{ userCollectionsStore.selectedCollectionUsername.isReady }}
-        </span>
-      </div>
 
-      <select
-        id="active-user-collection"
-        name="active-user-collection"
-        class="select select-secondary"
-        :disabled="userCollectionsStore.selectedCollectionUsername.isLoading"
-        :class="[
-          userCollectionsStore.selectedCollectionUsername.isLoading &&
-            'skeleton rounded-btn',
-        ]"
-        @change="
-          (event) => {
-            userCollectionsStore.selectedCollectionUsername.execute(
-              3_000,
-              (event.target as HTMLSelectElement).value,
-            );
-          }
-        "
-      >
-        <option value="" />
-        <option
-          v-for="name of userCollectionsStore.usernamesToSelect.state"
-          :key="name"
-          :value="name"
-          :selected="
-            name === userCollectionsStore.selectedCollectionUsername.state
-          "
+    <div class="rounded-box border-2 p-2">
+      <section class="p-2">
+        <label for="active-user-collection">
+          <h2 class="text-3xl font-bold">Select active user collection</h2>
+        </label>
+        <select
+          id="active-user-collection"
+          ref="userEmoteCollectionSelectRef"
+          name="active-user-collection"
+          class="select select-secondary mt-1 w-full"
+          :disabled="userCollectionsStore.selectedCollectionUsername.isLoading"
+          @input="handleSelectInput"
         >
-          {{ name }}
-        </option>
-      </select>
-    </div>
-    <div>
-      selectedUserCollections
-      <div class="flex flex-col">
-        <span>
-          nickname:
-          {{ userCollectionsStore.selectedCollection.state?.twitch?.nickname }}
-        </span>
-        <span>
-          typeof state:
-          {{ typeof userCollectionsStore.selectedCollection.state }}
-        </span>
-        <span>error: {{ userCollectionsStore.selectedCollection.error }}</span>
-        <span
-          >isLoading:
-          {{ userCollectionsStore.selectedCollection.isLoading }}
-        </span>
-        <span
-          >isReady: {{ userCollectionsStore.selectedCollection.isReady }}
-        </span>
-      </div>
-    </div>
-    <div class="flex">
-      <input
-        v-model="nickname"
-        class="input input-secondary grow"
-        type="text"
+          <option value="" />
+          <option
+            v-for="name of userCollectionsStore.usernamesToSelect.state"
+            :key="name"
+            :value="name"
+            :selected="
+              name === userCollectionsStore.selectedCollectionUsername.state
+            "
+          >
+            {{ name }}
+          </option>
+        </select>
+      </section>
+      <selected-user-collection
+        :collection="userCollectionsStore.selectedCollection"
+        :is-collection-selected="true"
       />
-      <button class="btn btn-secondary" @click="hg">Add collecn</button>
     </div>
+    <section class="rounded-box border-2 p-2">
+      <h2 class="-mb-1 p-1 text-3xl font-bold">Load user collection</h2>
+      <div class="join p-1">
+        <input
+          id="nickname"
+          v-model="nickname"
+          name="nickname"
+          placeholder="Enter twitch nickname"
+          class="input join-item input-secondary grow"
+          type="text"
+        />
+        <button class="btn btn-secondary join-item" @click="hg">
+          Load collection
+        </button>
+      </div>
+    </section>
     <go-to-main-page />
   </div>
 </template>
@@ -92,6 +58,16 @@ const userCollectionsStore = useUserCollectionsStore();
 const collections = useUserIntegrations();
 
 const nickname = ref("");
+
+const userEmoteCollectionSelectRef = ref<HTMLSelectElement>();
+
+async function handleSelectInput(event: Event) {
+  await userCollectionsStore.selectedCollectionUsername.execute(
+    0,
+    (event.currentTarget as HTMLSelectElement).value,
+  );
+  userEmoteCollectionSelectRef.value?.focus();
+}
 
 async function hg() {
   if (
