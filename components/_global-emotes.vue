d87fe3b0683c3681070634604d7690c2efa22121
geforseN <geforsenjs@gmail.com>
2023-12-11 22:53:55 +0700

    emotes pages: update

--- components/_global-emotes.vue
+++ components/_global-emotes.vue
@@ -1,38 +1,49 @@
 <template>
-  <div class="rounded-box border-2 p-2">
+  <div class="w-96 rounded-box border-2 p-2">
     <h2 class="px-2 text-3xl font-bold">Global emotes</h2>
-    <template v-if="!globalCollectionsStore.collections.isReady">
-      <div>not ready</div>
-    </template>
-    <template v-if="globalCollectionsStore.collections.isLoading">
-      <div>loading</div>
-    </template>
-    <div
+    <ul
       v-if="globalCollectionsStore.collections.isReady"
-      role="tablist"
-      class="tabs tabs-lifted w-96 rounded-[10px] border border-base-300"
+      class="flex flex-col gap-2"
     >
-      <template
-        v-for="collection of globalCollectionsStore.collections.state"
-        :key="collection.source"
-      >
-        <input
-          type="radio"
-          name="my_tabs_2"
-          role="tab"
-          class="tab bg-base-300"
-          :aria-label="collection.source"
-        />
-        <global-emote-collection
-          role="tabpanel"
-          class="tab-content"
-          :collection="collection"
-          @update="globalCollectionsStore.refreshGlobalCollection(collection)"
-        />
-      </template>
+      <emote-collection-ffz-sync
+        v-if="frankerFaceZCollection"
+        class="rounded-btn"
+        :sets="frankerFaceZCollection.sets"
+      />
+      <emote-collection-bttv-sync
+        v-if="betterTTVCollection"
+        class="rounded-btn"
+        :sets="betterTTVCollection.sets"
+      />
+      <emote-collection-seventv-sync
+        v-if="sevenTvCollection"
+        class="rounded-btn"
+        :sets="sevenTvCollection.sets"
+      />
+    </ul>
+    <div
+      v-else-if="!globalCollectionsStore.collections.isLoading"
+      class="flex flex-col items-center p-2"
+    >
+      <span>Failed to load global emotes</span>
+      <span>Please reload the page</span>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 const globalCollectionsStore = useGlobalCollectionsStore();
+
+const frankerFaceZCollection = computed(() =>
+  globalCollectionsStore.collections.state.find(
+    (c) => c.source === "FrankerFaceZ",
+  ),
+);
+const betterTTVCollection = computed(() =>
+  globalCollectionsStore.collections.state.find(
+    (c) => c.source === "BetterTTV",
+  ),
+);
+const sevenTvCollection = computed(() =>
+  globalCollectionsStore.collections.state.find((c) => c.source === "SevenTV"),
+);
 </script>
---
+++ pages/emotes/active.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <div class="rounded-box border-2 p-2">
+      <section class="p-2">
+        <label for="active-user-collection">
+          <h2 class="text-3xl font-bold">Select active user collection</h2>
+        </label>
+        <select
+          id="active-user-collection"
+          ref="userEmoteCollectionSelectRef"
+          name="active-user-collection"
+          class="select select-secondary mt-1 w-full"
+          :disabled="userCollectionsStore.selectedCollectionUsername.isLoading"
+          @input="handleSelectInput"
+        >
+          <option value="" />
+          <option
+            v-for="name of userCollectionsStore.usernamesToSelect.state"
+            :key="name"
+            :value="name"
+            :selected="
+              name === userCollectionsStore.selectedCollectionUsername.state
+            "
+          >
+            {{ name }}
+          </option>
+        </select>
+      </section>
+      <selected-user-collection
+        :collection="userCollectionsStore.selectedCollection"
+        :is-collection-selected="true"
+        @update="
+          () => {
+            /* TODO */
+          }
+        "
+      />
+    </div>
+    <div class="rounded-box border-2 px-4 py-2">
+      <nuxt-link to="/emotes">
+        <span
+          class="flex items-center justify-between gap-2 text-3xl font-bold"
+        >
+          <span class="flex items-center gap-2">
+            <icon name="carbon:link" />
+            Look for emotes
+          </span>
+          <div class="flex items-center gap-1">
+            <span class="mr-4 flex flex-col gap-1">
+              <icon-emote-integration-logo
+                v-for="source of availableEmoteSources"
+                v-once
+                :key="source"
+                :class="
+                  source === 'BetterTTV' &&
+                  'absolute translate-x-4 translate-y-2'
+                "
+                :source="source"
+                width="16"
+                heigth="16"
+                class="max-h-[16px] text-xs"
+              />
+            </span>
+          </div>
+        </span>
+      </nuxt-link>
+    </div>
+    <go-to-main-page />
+  </div>
+</template>
+<script lang="ts" setup>
+import { availableEmoteSources } from "~/integrations";
+
+const userCollectionsStore = useUserCollectionsStore();
+
+const userEmoteCollectionSelectRef = ref<HTMLSelectElement>();
+
+async function handleSelectInput(event: Event) {
+  await userCollectionsStore.selectedCollectionUsername.execute(
+    0,
+    (event.currentTarget as HTMLSelectElement).value,
+  );
+  userEmoteCollectionSelectRef.value?.focus();
+}
+</script>
