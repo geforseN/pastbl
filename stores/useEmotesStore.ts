994f3ae3f45ba7f1f8fd0f33b576f79bbfb5d20c
geforseN <semen.neb@gmail.com>
2023-12-12 18:21:20 +0700

    useEmotesStore: update

--- stores/useEmotesStore.ts
+++ stores/useEmotesStore.ts
@@ -1,20 +1,114 @@
 import { defineStore } from "pinia";
-import type { AvailableEmoteSource, IEmote } from "~/integrations";
+import {
+  availableEmoteSources,
+  type AvailableEmoteSource,
+  type IEmote,
+} from "~/integrations";
 
 export const useEmotesStore = defineStore("emotes", () => {
-  const users: Record<AvailableEmoteSource, Map<IEmote["id"], IEmote>> = {
-    BetterTTV: new Map(),
-    FrankerFaceZ: new Map(),
-    SevenTV: new Map(),
-  };
-  const global: Record<AvailableEmoteSource, Map<IEmote["id"], IEmote>> = {
-    BetterTTV: new Map(),
-    FrankerFaceZ: new Map(),
-    SevenTV: new Map(),
-  };
+  const userCollectionsStore = useUserCollectionsStore();
+  const globalCollectionsStore = useGlobalCollectionsStore();
+
+  const usersEmotesCache = new Map<
+    AvailableEmoteSource,
+    Map<IEmote["id"], IEmote>
+  >([
+    ["BetterTTV", new Map()],
+    ["FrankerFaceZ", new Map()],
+    ["SevenTV", new Map()],
+  ]);
+
+  const activeUserEmotesCache = new Map<IEmote["token"], IEmote>();
+
+  // FIXME: fix ts-expect-error comments
+  const activeUserEmotes = computedWithControl(
+    () => userCollectionsStore.selectedCollection?.state,
+    () => {
+      const activeUserEmotes = groupBy2(
+        Object.values(
+          userCollectionsStore.selectedCollection?.state?.collections ?? {},
+        ),
+        (collection) => collection.source,
+        (collection) => {
+          return new Map<IEmote["token"], IEmote>(
+            // @ts-expect-error idk for now
+            collection.sets
+              // @ts-expect-error idk for now
+              .flatMap((set) => set.emotes)
+              // @ts-expect-error idk for now
+              .map((emote) => [emote.token, emote]),
+          );
+        },
+      );
+      console.log({ activeUserEmotes });
+      for (const source of availableEmoteSources) {
+        if (!activeUserEmotes[source]) {
+          activeUserEmotes[source] = new Map();
+        }
+      }
+      console.log({ activeUserEmotes });
+      return activeUserEmotes;
+    },
+  );
+  const globalEmotes = computedWithControl(
+    () => globalCollectionsStore.collections.state,
+    () => {
+      const globalEmotes = groupBy2(
+        globalCollectionsStore.collections.state,
+        (collection) => collection.source,
+        (collection) => {
+          return new Map<IEmote["token"], IEmote>(
+            collection.sets
+              .flatMap((set) => set.emotes)
+              .map((emote) => [emote.token, emote]),
+          );
+        },
+      );
+      console.log({ globalEmotes });
+      for (const source of availableEmoteSources) {
+        if (!globalEmotes[source]) {
+          globalEmotes[source] = new Map();
+        }
+      }
+      console.log({ globalEmotes });
+      return globalEmotes;
+    },
+  );
+
+  watch(
+    () => activeUserEmotes.value,
+    (activeUserEmotes) => {
+      console.log("activeUserEmotes changed", { activeUserEmotes });
+      activeUserEmotesCache.clear();
+    },
+  );
 
   return {
-    users,
-    global,
+    usersEmotesCache,
+    activeUserEmotes,
+    globalEmotes,
+    findEmoteInActiveUser(token: IEmote["token"]) {
+      const cachedEmote = activeUserEmotesCache.get(token);
+      if (cachedEmote) {
+        return cachedEmote;
+      }
+      for (const source of ["FrankerFaceZ", "BetterTTV", "SevenTV"] as const) {
+        const sourceEmote = activeUserEmotes.value[source].get(token);
+        if (sourceEmote) {
+          activeUserEmotesCache.set(token, sourceEmote);
+          return sourceEmote;
+        }
+      }
+    },
+    findEmoteInGlobal(token: IEmote["token"]) {
+      for (const source of ["FrankerFaceZ", "BetterTTV", "SevenTV"] as const) {
+        const sourceEmote = globalEmotes.value[source].get(token);
+        if (sourceEmote) {
+          activeUserEmotesCache.set(token, sourceEmote);
+          return sourceEmote;
+        }
+      }
+    },
+    _activeUserEmotesCache: activeUserEmotesCache,
   };
 });
