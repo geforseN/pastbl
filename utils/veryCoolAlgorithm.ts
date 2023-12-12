b54187cdc412b5a48d48bc966c33704430096bc7
geforseN <semen.neb@gmail.com>
2023-12-12 18:02:30 +0700

    findEmotesInPastas: add

--- utils/veryCoolAlgorithm.ts
+++ utils/veryCoolAlgorithm.ts
@@ -66,3 +66,25 @@ export async function veryCoolAlgorithm(newPastas: IDBMegaPasta[]) {
   }
   return foundEmotes;
 }
+
+export function findEmotesInPastas(
+  pastas: IDBMegaPasta[],
+  findEmoteInStorage: (token: IEmote["token"]) => IEmote | undefined,
+) {
+  return pastas.reduce(
+    (pastasWithEmotes, pasta) => {
+      const foundEmotes = pasta.validTokens.reduce((foundEmotes, token) => {
+        const emote = findEmoteInStorage(token);
+        if (emote) {
+          foundEmotes.push(emote);
+        }
+        return foundEmotes;
+      }, [] as IEmote[]);
+      if (foundEmotes.length) {
+        pastasWithEmotes[pasta.id] = foundEmotes;
+      }
+      return pastasWithEmotes;
+    },
+    {} as Record<IDBMegaPasta["id"], IEmote[]>,
+  );
+}
