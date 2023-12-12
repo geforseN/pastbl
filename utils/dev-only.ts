7ab097f25a460a75f52603a8a4165057b30eddc8
geforseN <semen.neb@gmail.com>
2023-12-12 18:10:32 +0700

    withLog util: update param

--- utils/dev-only.ts
+++ utils/dev-only.ts
@@ -1,14 +1,24 @@
 export async function withLog<T>(
   cb: () => T | Promise<T>,
-  {
-    logKey,
-    additionalMessage = {},
-  }: { logKey: string; additionalMessage?: Record<string, unknown | never> },
+  optionsOrKey:
+    | string
+    | {
+        logKey: string;
+        additionalMessages?: Record<string, unknown | never>;
+      },
 ): Promise<T> {
   const returnValue = await cb();
   if (process.dev) {
-    // eslint-disable-next-line no-console
-    console.log({ [logKey]: returnValue, ...additionalMessage });
+    if (typeof optionsOrKey === "string") {
+      // eslint-disable-next-line no-console
+      console.log({ [optionsOrKey]: returnValue });
+    } else {
+      // eslint-disable-next-line no-console
+      console.log({
+        [optionsOrKey.logKey]: returnValue,
+        ...optionsOrKey.additionalMessages,
+      });
+    }
   }
   return returnValue;
 }
