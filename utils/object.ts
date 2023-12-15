38c68c2151665c452d2e26b8fe57ea7a7b83bc5c
geforseN <semen.neb@gmail.com>
2023-12-15 20:54:42 +0700

    object utils: remove makeObjectFromMap

--- utils/object.ts
+++ utils/object.ts
@@ -3,23 +3,6 @@ export function makeObjectFrozen<T extends object>(object: T): Readonly<T> {
   return object;
 }
 
-export function makeObjectFromMap<K extends string, V>(
-  map: ReadonlyMap<K, V>,
-  mustBeFrozen = true,
-): typeof mustBeFrozen extends true ? Readonly<Record<K, V>> : Record<K, V> {
-  const object = [...map].reduce(
-    (record, [key, value]) => {
-      record[key] = value;
-      return record;
-    },
-    {} as Record<K, V>,
-  );
-  if (mustBeFrozen) {
-    return makeObjectFrozen(object);
-  }
-  return object;
-}
-
 export function groupBy<T, V extends string | number | symbol>(
   array: T[],
   cb: (value: T, index: number, array: T[]) => V,
