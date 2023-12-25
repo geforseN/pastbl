80d3a87a02ea9155739bd9ba44d94c7555dc9d00
geforseN <semen.neb@gmail.com>
2023-12-25 23:07:41 +0700

    update pasta form

--- stores/usePastaStore.ts
+++ stores/usePastaStore.ts
@@ -4,21 +4,21 @@ export const usePastaStore = defineStore("pasta", () => {
   const pasta = usePasta();
   const toast = useNuxtToast();
 
-  function handleTagAddToPasta() {
-    try {
-      pasta.addTag(pasta.tag.value);
-    } catch (error) {
-      assert.ok(error instanceof ExtendedError);
-      toast.add({
-        description: error.description,
-        title: error.title,
-        color: error.color,
-      });
-    }
-  }
-
   return {
-    ...pasta,
-    handleTagAddToPasta,
+    pasta,
+    handleTagAddToPasta() {
+      try {
+        pasta.addTag(pasta.tag.value);
+      } catch (error) {
+        assert.ok(error instanceof ExtendedError);
+        toast.add({
+          description: error.description,
+          title: error.title,
+          color: error.color,
+        });
+      }
+    },
   };
 });
+
+// watchThrottled on input and then put data to idb
