7c6fc7c77185c6256fb78aeed669c751a0d4b713
geforseN <semen.neb@gmail.com>
2023-12-28 17:03:56 +0700

    chat-pasta: add populate code

--- components/chat-pasta/chat-pasta.vue
+++ components/chat-pasta/chat-pasta.vue
@@ -6,6 +6,7 @@
     probably this can be refactored and visual regression wont happened
   -->
   <div
+    ref="pastaRef"
     class="flex flex-col gap-x-2 gap-y-1 border border-info p-2 xs:flex-row xs:gap-y-0"
   >
     <div class="flex w-[340px] flex-col">
@@ -13,12 +14,12 @@
         <span class="block w-full px-[10px] py-[5px]">
           <slot name="creatorData" />
           <span aria-hidden="true">{{ ": " }}</span>
-          <!-- eslint-disable vue/no-v-html -->
           <span
             class="twitch-text p-0 text-[13px]/[18px]"
-            v-html="props.pasta.populatedText || props.pasta.text"
-          />
-          <!-- eslint-enable vue/no-v-html -->
+            :data-pasta-text-container="true"
+          >
+            {{ props.pasta.text }}
+          </span>
         </span>
       </span>
       <div
@@ -38,7 +39,7 @@
         :title="new Date(props.pasta.createdAt).toString()"
       >
         <use-time-ago #="{ timeAgo }" :time="props.pasta.createdAt">
-          <time>Created {{ timeAgo }}</time>
+          <time>Published {{ timeAgo }}</time>
         </use-time-ago>
         <time>{{ new Date(props.pasta.createdAt).toDateString() }}</time>
       </div>
@@ -48,12 +49,39 @@
 </template>
 <script lang="ts" setup>
 import { UseTimeAgo } from "@vueuse/components";
-const props = defineProps<{ pasta: MegaPasta }>();
+const props = defineProps<{ pasta: IDBMegaPasta }>();
 
 defineSlots<{
   creatorData?: () => unknown;
   sidebar: () => unknown;
 }>();
+
+const emit = defineEmits<{
+  populate: [pastaTextContainer: HTMLElement, pasta: IDBMegaPasta];
+}>();
+
+// NOTE: maybe no need isPopulateEmitCalledOnce ref
+// maybe can use stop, pause, resume inside callback param
+// isPopulateEmitCalledOnce MUST be ref, otherwise will be SSR bugs with pasta populate
+const isPopulateEmitCalledOnce = ref(false);
+
+const pastaRef = ref();
+const {
+  stop: _,
+  pause: __,
+  resume: ___,
+  isActive: ____,
+} = useIntersectionObserver(pastaRef, ([entry]) => {
+  if (!entry.isIntersecting || isPopulateEmitCalledOnce.value) {
+    return;
+  }
+  const pastaTextContainer = entry.target.querySelector(
+    "[data-pasta-text-container]",
+  );
+  assert.ok(pastaTextContainer instanceof HTMLElement);
+  emit("populate", pastaTextContainer, props.pasta);
+  isPopulateEmitCalledOnce.value = true;
+});
 </script>
 <style scoped>
 :deep(img) {
