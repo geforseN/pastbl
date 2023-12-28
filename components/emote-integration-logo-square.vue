33f99d3c18b9193b757ad9b90f38d8b211d71df1
geforseN <semen.neb@gmail.com>
2023-12-28 16:51:23 +0700

    emote-integration-logo-square: init

---
+++ components/emote-integration-logo-square.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center gap-1">
+    <span class="mr-4 flex flex-col gap-1">
+      <icon-emote-integration-logo
+        v-for="(source, index) of emoteSources"
+        v-once
+        :key="source"
+        :class="[
+          index % 2 && `absolute translate-x-5`,
+          index === 3 && ' translate-y-[21px]',
+        ]"
+        :source="source"
+        width="16"
+        heigth="16"
+        class="max-h-[16px] text-xs"
+      />
+    </span>
+  </div>
+</template>
+<script setup lang="ts">
+import { emoteSources } from "~/integrations";
+</script>
