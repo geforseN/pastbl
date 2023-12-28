fbaf4fea8cfeb2b8c66157494fa50cb759007084
geforseN <geforsenjs@gmail.com>
2023-12-28 16:07:17 +0700

    find-my-pasta-list: remove carousel code

--- components/find-my-pasta/find-my-pasta-list.vue
+++ components/find-my-pasta/find-my-pasta-list.vue
@@ -37,50 +37,24 @@
         &gt&gt
       </button>
     </div>
-    <div class="container">
-      <div class="carousel">
-        <chat-pasta v-if="prevPasta" class="prev-pasta" :pasta="prevPasta">
-          <template #creatorData>
-            <chat-pasta-creator-data
-              :badges-count="userStore.user.badges.count"
-              :nickname="userStore.user.nickname"
-              :nickname-color="userStore.user.preferences.nickname.color"
-            />
-          </template>
-        </chat-pasta>
-        <chat-pasta
-          v-if="selectedPasta"
-          :pasta="selectedPasta"
-          class="curr-pasta"
+    <chat-pasta v-if="selectedPasta" :pasta="selectedPasta" class="curr-pasta">
+      <template #creatorData>
+        <chat-pasta-creator-data
+          :badges-count="userStore.user.badges.count"
+          :nickname="userStore.user.nickname"
+          :nickname-color="userStore.user.preferences.nickname.color"
+        />
+      </template>
+      <template #sidebar>
+        <button
+          class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
+          :disabled="!clipboard.isSupported.value"
+          @click="copyPasta(selectedPasta)"
         >
-          <template #creatorData>
-            <chat-pasta-creator-data
-              :badges-count="userStore.user.badges.count"
-              :nickname="userStore.user.nickname"
-              :nickname-color="userStore.user.preferences.nickname.color"
-            />
-          </template>
-          <template #sidebar>
-            <button
-              class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
-              :disabled="!clipboard.isSupported.value"
-              @click="copyPasta(selectedPasta)"
-            >
-              copy pasta
-            </button>
-          </template>
-        </chat-pasta>
-        <chat-pasta v-if="nextPasta" class="next-pasta" :pasta="nextPasta">
-          <template #creatorData>
-            <chat-pasta-creator-data
-              :badges-count="userStore.user.badges.count"
-              :nickname="userStore.user.nickname"
-              :nickname-color="userStore.user.preferences.nickname.color"
-            />
-          </template>
-        </chat-pasta>
-      </div>
-    </div>
+          copy pasta
+        </button>
+      </template>
+    </chat-pasta>
   </div>
 </template>
 <script lang="ts" setup>
@@ -91,20 +65,6 @@ const props = defineProps<{
 const selectedPastaNumber = ref(1);
 const selectedPastaIndex = computed(() => selectedPastaNumber.value - 1);
 const selectedPasta = computed(() => props.pastas[selectedPastaIndex.value]);
-
-const prevPasta = computed(() => {
-  if (props.pastas.length === 1) {
-    return;
-  }
-  return props.pastas.at(selectedPastaIndex.value - 1);
-});
-
-const nextPasta = computed(() => {
-  if (props.pastas.length === 1) {
-    return;
-  }
-  return props.pastas.at((selectedPastaIndex.value + 1) % props.pastas.length);
-});
 
 watch(
   () => props.pastas.length,
@@ -119,23 +79,3 @@ const clipboard = useClipboard();
 
 const { copyPasta } = usePastaCopy({ userStore, clipboard });
 </script>
-<style scoped>
-.prev-pasta,
-.next-pasta {
-  --pasta-width: 414px;
-  will-change: transform;
-  position: absolute;
-  top: 0;
-  display: none;
-}
-
-.prev-pasta {
-  transform: rotateY(-55deg) translateZ(-100px);
-  right: var(--pasta-width);
-}
-
-.next-pasta {
-  transform: rotateY(55deg) translateZ(-100px);
-  left: var(--pasta-width);
-}
-</style>
