1477d134cb1a40ae7323195b78518e16138cdeb7
geforseN <semen.neb@gmail.com>
2024-01-02 13:29:30 +0700

    app.vue: remove dead code

--- app.vue
+++ app.vue
@@ -41,99 +41,10 @@
 </template>
 <script setup lang="ts">
 import { themeChange } from "theme-change";
-import { templateStrings } from "./integrations";
-
 useHead({ title: process.dev ? "pastbl - dev" : "pastbl" });
-
-// use new DomParser()
-// показывать процесс загрузки коллекции (можно переиспользовать headers вместе с asyncState  )
-// добавить возможность удалить коллекцию пользователя
 
 onMounted(() => {
   themeChange(false);
   document.documentElement.classList.remove("dark", "light");
 });
 </script>
-<style>
-.ffz-x {
-}
-.ffz-y {
-}
-.ffz-w {
-}
-.ffz-cursed {
-}
-</style>
-
-<!-- 
-  pages:
-   - index: показывает все возможные действия, а  что именно смотри ниже  
-     - pastas/create
-     - pastas/edit/:pastaId
-     - pastas/find?...MUST_BE_MANY_PARAMS_HERE
-
-    по нажатию на кнопку 'Change pasta' n-ой пасты из списка паст (список отображается в левой колонке)
-      - форма для редактирования пасты (форма отображается в правой колонке)
- -->
-
-<!--     [() => newPastas.value, () => emotesStore.activeUserEmotes],
-    async (
-      [newPastas, newActiveUserEmotes],
-      [oldPastas, oldActiveUserEmotes],
-    ) => {
-      const userEmoteSizes = {
-        next: Object.values(newActiveUserEmotes || {})
-          .map((emoteMap) => emoteMap.size)
-          .join(", "),
-        prev: Object.values(oldActiveUserEmotes || {})
-          .map((emoteMap) => emoteMap.size)
-          .join(", "),
-      };
-      const pastasLengths = {
-        next: newPastas.length,
-        prev: oldPastas.length,
-      };
-      console.log({
-        pastasLengths,
-        userEmoteSizes,
-      });
-      if (
-        !(newPastas.length || oldPastas.length) &&
-        userEmoteSizes.next === userEmoteSizes.prev
-      ) {
-        return console.log("FAST RETURN");
-      } -->
-
-<!-- 
-         // hooks: {
-  //   async close() {
-  //     console.log("Closing nitro server...");
-  //     await new Promise((resolve) => setTimeout(resolve, 500));
-  //     console.log("Task is done!");
-  //     const stream = fs.createWriteStream("./index.txt");
-  //     stream.write("Hello, World!");
-  //     stream.end();
-  //   },
-  //   ready(nuxt) {
-  //     const signals = ["SIGINT", "SIGTERM"] as const;
-  //     const handler = async (signal: (typeof signals)[number]) => {
-  //       try {
-  //         signals.forEach((signal) => process.removeListener(signal, handler));
-  //         await nuxt.close();
-  //         console.log("Closing nitro server...");
-  //         console.log("Closing nitro server...");
-  //         console.log("Closing nitro server...");
-  //         console.log("Closing nitro server...");
-  //         const stream = fs.createWriteStream(`./${signal}.txt`);
-  //         stream.write("Hello, World!");
-  //         stream.end();
-  //         process.kill(process.pid, signal);
-  //       } catch (error) {
-  //         console.error("error during shutdown", error);
-  //         process.exit(1);
-  //       }
-  //     };
-  //     signals.forEach((signal) => process.on(signal, handler));
-  //   },
-  // },
-       -->
