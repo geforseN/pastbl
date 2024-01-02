530843a1bc2b872a0f62fd1693d186d044071675
geforseN <semen.neb@gmail.com>
2024-01-02 17:11:53 +0700

    pastas idb: move code

---
+++ client-only/IndexedDB/pastas/PastasShared.ts
@@ -0,0 +1,25 @@
+import type { IDBPDatabase } from "idb";
+import type { PastasSchema } from "..";
+
+export class PastasShared {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private readonly idb: IDBPDatabase<PastasSchema>) {}
+
+  async movePastaFromListToBin(pasta: IDBMegaPasta) {
+    const tx = this.idb.transaction(["list", "bin"], "readwrite");
+    return await Promise.all([
+      tx.objectStore("list").delete(pasta.id),
+      tx.objectStore("bin").put(pasta),
+      tx.done,
+    ]);
+  }
+
+  async movePastaFromBinToList(pasta: IDBMegaPasta) {
+    const tx = this.idb.transaction(["bin", "list"], "readwrite");
+    return await Promise.all([
+      tx.objectStore("bin").delete(pasta.id),
+      tx.objectStore("list").put(pasta),
+      tx.done,
+    ]);
+  }
+}
