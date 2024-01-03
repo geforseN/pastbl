8bdd5713817764159a00991ea2c9f2b0f05c42ea
geforseN <semen.neb@gmail.com>
2024-01-03 19:51:34 +0700

    move pasta length range composable

--- composables/index.ts
+++ composables/index.ts
@@ -52,8 +52,33 @@ export function useFindPastasTags(
   };
 }
 
+export function useFindPastaLengthRange() {
+  const pastasStore = usePastasStore();
+
+  const minValue = computed(() => pastasStore.minPastaTextLengthInPastas);
+  const maxValue = computed(() => pastasStore.maxPastaTextLengthInPastas);
+
+  const range = ref<[number, number]>([minValue.value, maxValue.value]);
+
+  watchOnce(
+    () => minValue.value,
+    (newMinValue) => (range.value[0] = newMinValue),
+  );
+
+  watchOnce(
+    () => maxValue.value,
+    (newMaxValue) => (range.value[1] = newMaxValue),
+  );
+
+  return {
+    range,
+    minValue,
+    maxValue,
+  };
+}
+
 export function useFindPastasLength(pastas: Ref<IDBMegaPasta[]>) {
-  const { range, minValue: min, maxValue: max } = useFindMyPastaRange();
+  const { range, minValue: min, maxValue: max } = useFindPastaLengthRange();
   const mustRespectLengthRange = ref(true);
 
   function isPastaInRange(this: Ref<[number, number]>, pasta: IDBMegaPasta) {
