57a358affdff50d0ee905ea3e411b5c21fa6252b
geforseN <semen.neb@gmail.com>
2024-01-08 20:07:30 +0700

    add new components for userEmoteCollection

---
+++ components/user-emote-collection-btnlike.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex gap-2">
+    <div
+      class="h-8 w-8 rounded-full border border-twitch focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-twitch"
+    >
+      <nuxt-link
+        v-if="props.avatarUrl"
+        :to="`https://twitch.tv/${props.nickname.toLowerCase()}`"
+      >
+        <img
+          :src="props.avatarUrl"
+          :alt="props.nickname + ' avatar'"
+          width="32"
+          height="32"
+          class="rounded-full bg-twitch/20"
+        />
+      </nuxt-link>
+    </div>
+    <span class="text-2xl font-bold">{{ props.nickname }}</span>
+    <div class="ml-auto flex items-center gap-1">
+      <button class="btn btn-success btn-xs" @click="emit('refresh')">
+        <icon name="ic:round-refresh" />
+      </button>
+      <button class="btn btn-error btn-xs" @click="emit('delete')">
+        <icon name="ic:round-delete-outline" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  nickname: string;
+  avatarUrl?: string;
+}>();
+
+const emit = defineEmits<{
+  refresh: [];
+  delete: [];
+}>();
+</script>
---
+++ components/user-emote-collections.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-auto-animate class="flex max-h-72 flex-col gap-2 overflow-y-scroll">
+    <user-emote-collection-btnlike
+      v-for="collection of userCollectionsStore.collectionsToSelect.state"
+      :key="collection.twitch.nickname"
+      class="rounded-btn border-2 p-2"
+      :nickname="collection.twitch.nickname"
+      :avatar-url="collection.integrations.FrankerFaceZ?.owner.avatarUrl"
+      @delete="
+        userCollectionsStore.deleteCollection(collection.twitch.username)
+      "
+      @refresh="
+        userCollectionsStore.refreshCollection(collection.twitch.username)
+      "
+    />
+  </div>
+</template>
+<script setup lang="ts">
+const userCollectionsStore = useUserCollectionsStore();
+</script>
