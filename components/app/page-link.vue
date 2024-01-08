55d9603640a7fe01043753e872e33d06118ebb7d
geforseN <semen.neb@gmail.com>
2024-01-08 19:54:23 +0700

    ape-page-link: init

---
+++ components/app/page-link.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="rounded-box border-2 px-4 py-2">
+    <nuxt-link :to="path">
+      <span class="flex items-center justify-between gap-2 text-3xl font-bold">
+        <span class="flex items-center gap-2">
+          <icon name="carbon:link" />
+          <slot>{{ text }}</slot>
+        </span>
+      </span>
+    </nuxt-link>
+  </div>
+</template>
+<script setup lang="ts">
+const routePageLinkRecord = {
+  "global-emotes": {
+    path: "/emotes/global",
+    text: "Look for global emotes",
+  },
+  "__active-emotes__": {
+    path: "/emotes/active",
+    text: "Select active emotes",
+  },
+} as const;
+
+const props = defineProps<{
+  to: keyof typeof routePageLinkRecord;
+}>();
+
+const { text, path } = routePageLinkRecord[props.to];
+</script>
