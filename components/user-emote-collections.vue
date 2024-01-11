4be9ce46a6e0bed71993e2ca9fad472508df43c7
geforseN <geforsenjs@gmail.com>
2024-01-11 22:13:03 +0700

    user-emote-collections: change
    add style for selected
    add section and heading

--- components/user-emote-collections.vue
+++ components/user-emote-collections.vue
@@ -1,19 +1,26 @@
 <template>
-  <div v-auto-animate class="flex max-h-72 flex-col gap-2 overflow-y-scroll">
-    <user-emote-collection-btnlike
-      v-for="collection of userCollectionsStore.collectionsToSelect.state"
-      :key="collection.twitch.nickname"
-      class="rounded-btn border-2 p-2"
-      :nickname="collection.twitch.nickname"
-      :avatar-url="collection.integrations.FrankerFaceZ?.owner.avatarUrl"
-      @delete="
-        userCollectionsStore.deleteCollection(collection.twitch.username)
-      "
-      @refresh="
-        userCollectionsStore.refreshCollection(collection.twitch.username)
-      "
-    />
-  </div>
+  <section class="rounded-box border-2 p-2">
+    <h2 class="p-2 text-3xl font-bold">Users collections</h2>
+    <div v-auto-animate class="flex max-h-72 flex-col gap-2 overflow-auto">
+      <user-emote-collection-btnlike
+        v-for="collection of userCollectionsStore.collectionsToSelect.state"
+        :key="collection.twitch.nickname"
+        class="rounded-btn border-2 p-2"
+        :class="
+          userCollectionsStore.isSelectedUsername(collection.twitch.username) &&
+          'border-twitch'
+        "
+        :nickname="collection.twitch.nickname"
+        :avatar-url="collection.integrations.FrankerFaceZ?.owner.avatarUrl"
+        @delete="
+          userCollectionsStore.deleteCollection(collection.twitch.username)
+        "
+        @refresh="
+          userCollectionsStore.refreshCollection(collection.twitch.username)
+        "
+      />
+    </div>
+  </section>
 </template>
 <script setup lang="ts">
 const userCollectionsStore = useUserCollectionsStore();
