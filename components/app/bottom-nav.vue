2e9f24e9cd3bece0526974ff1709488092b9d9c8
geforseN <geforsenjs@gmail.com>
2024-01-11 21:42:49 +0700

    update links for navs

--- components/app/bottom-nav.vue
+++ components/app/bottom-nav.vue
@@ -1,28 +1,18 @@
 <template>
-  <!-- NOTE: z index is necessary to keep click on click more important that what below link  -->
+  <!-- NOTE: z-index is necessary to keep below links more important  -->
   <nav class="btm-nav btm-nav-md z-10 gap-0 go-brr:hidden">
     <ol class="contents">
       <li class="w-1/2">
         <nuxt-link
           class="btn btn-ghost h-16 w-full flex-nowrap rounded-none border-0 border-twitch bg-twitch/10 text-twitch hover:bg-twitch/20"
-          to="/emotes"
+          to="/collections"
           active-class="border-t-2 border-t-twitch"
         >
-          <span class="w-min break-words uppercase xs:w-auto">emotes</span>
-          <span class="flex flex-col gap-1 xs:flex-row">
-            <icon-emote-integration-logo
-              v-for="(source, index) of availableEmoteSources"
-              v-once
-              :key="source"
-              :class="
-                index === 1 &&
-                'absolute translate-x-6 translate-y-2 xs:static xs:translate-x-0 xs:translate-y-0'
-              "
-              :source="source"
-              width="24"
-              heigth="24"
-              class="max-h-[24px] text-xs"
-            />
+          <span class="w-min break-words uppercase xs:w-auto">
+            look emotes
+          </span>
+          <span class="flex gap-1">
+            <emote-integration-logo-square />
           </span>
         </nuxt-link>
       </li>
@@ -39,6 +29,3 @@
     </ol>
   </nav>
 </template>
-<script lang="ts" setup>
-import { availableEmoteSources } from "~/integrations";
-</script>
