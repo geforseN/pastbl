3a13cd1d63dfdbb18397c8950e079e3de3d04c84
geforseN <semen.neb@gmail.com>
2024-01-11 21:59:13 +0700

    pages: change links

--- pages/index.vue
+++ pages/index.vue
@@ -26,19 +26,9 @@
         </span>
       </nuxt-link>
     </div>
-    <div class="rounded-box border-2 px-4 py-2">
-      <nuxt-link to="/emotes">
-        <span
-          class="flex items-center justify-between gap-2 text-3xl font-bold"
-        >
-          <span class="flex items-center gap-2">
-            <icon name="carbon:link" />
-            Look for emotes
-          </span>
-          <emote-integration-logo-square />
-        </span>
-      </nuxt-link>
-    </div>
+    <app-page-link to="emotes">
+      <template #right><emote-integration-logo-square /></template>
+    </app-page-link>
   </div>
 </template>
 <style>
