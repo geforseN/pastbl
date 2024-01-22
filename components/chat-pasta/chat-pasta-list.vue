77a8fb9f1a27e9ce6bf6232be69eca2c82a2d805
geforseN <geforsenjs@gmail.com>
2024-01-22 18:42:48 +0700

    chat-pasta-list: update sidebar buttons

--- components/chat-pasta/chat-pasta-list.vue
+++ components/chat-pasta/chat-pasta-list.vue
@@ -23,7 +23,7 @@
     </div>
   </client-only>
   <div
-    class="flex max-h-[60dvh] flex-col gap-y-2 overflow-y-auto go-brr:max-h-[80dvh]"
+    class="flex max-h-[60dvh] flex-col gap-y-2 overflow-y-auto go-brr:max-h-[77dvh]"
   >
     <chat-pasta
       v-for="pasta of pastasStore.pastasSortedByNewest"
@@ -37,21 +37,59 @@
       <template #creatorData><slot name="creatorData" /></template>
       <template #sidebar>
         <div
-          class="flex flex-row-reverse gap-x-2 xs:flex-col xs:justify-between xs:gap-x-0"
+          class="flex flex-row-reverse justify-between gap-x-2 gap-y-0.5 xs:flex-col xs:justify-between xs:gap-x-0"
         >
           <button
-            class="btn btn-square btn-md ml-auto rounded-none border-2 border-accent text-xs xs:ml-0"
+            class="btn btn-square btn-accent btn-md border-2 border-accent-content text-xs"
             :disabled="!userStore.clipboard.isSupported"
             @click="userStore.copyPasta(pasta)"
           >
             Copy pasta
           </button>
-          <button
-            class="btn btn-square btn-warning btn-md rounded-none border-2 border-neutral-content text-xs text-warning-content"
-            @click="pastasStore.removePasta(pasta)"
-          >
-            Delete pasta
-          </button>
+          <div class="dropdown dropdown-top dropdown-hover xs:dropdown-end">
+            <div
+              tabindex="0"
+              role="button"
+              class="btn btn-square btn-primary border-2 border-primary-content xs:btn-xs xs:w-full"
+            >
+              <span class="-translate-y-2 text-xl xs:-translate-y-2.5">
+                ...
+              </span>
+            </div>
+            <ul
+              tabindex="0"
+              class="menu dropdown-content z-[1] flex w-min flex-col gap-1 rounded-btn border border-base-content bg-base-100 p-2 shadow"
+            >
+              <li>
+                <button
+                  class="btn btn-accent"
+                  :disabled="!userStore.clipboard.isSupported"
+                  @click="userStore.copyPasta(pasta)"
+                >
+                  Copy pasta
+                  <icon name="ic:baseline-content-copy" />
+                </button>
+              </li>
+              <li>
+                <nuxt-link
+                  :to="`/pastas/edit/${pasta.id}`"
+                  class="btn btn-info w-full"
+                >
+                  Change pasta
+                  <icon name="ic:outline-edit" />
+                </nuxt-link>
+              </li>
+              <li>
+                <button
+                  class="btn btn-error w-full"
+                  @click="pastasStore.removePasta(pasta)"
+                >
+                  Delete pasta
+                  <icon name="ic:baseline-delete-outline" />
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </template>
     </chat-pasta>
