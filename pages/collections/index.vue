3566739890aa30e3ccc8e50edd9e05770bdf01d3
geforseN <geforsenjs@gmail.com>
2024-01-28 00:17:28 +0700

    emojis page: add

--- pages/collections/index.vue
+++ pages/collections/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex w-96 flex-col gap-2">
-    <app-page-link to="global-emotes" />
+    <app-page-link to="emojis">
+      <template #right>👀</template>
+    </app-page-link>
+    <app-page-link to="global-emotes">
+      <template #right><emote-integration-logo-square /></template>
+    </app-page-link>
     <user-emote-collections />
     <fetch-emote-collection />
     <select-user-collection />
