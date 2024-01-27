128ce0361bebb1fb61fa79e1a6fe69d4c8bbbbe9
geforseN <geforsenjs@gmail.com>
2024-01-27 23:52:25 +0700

    icon-emote-integration-logo: refactor
    use record
    add props.mustWrapToLink

--- components/icon-emote-integration-logo.vue
+++ components/icon-emote-integration-logo.vue
@@ -1,27 +1,40 @@
 <template>
-  <img
-    v-if="props.source === 'FrankerFaceZ'"
-    src="/img/ffz-logo-32.png"
-    alt="FrankerFaceZ logo"
-  />
-  <img
-    v-else-if="props.source === 'BetterTTV'"
-    src="/img/bttv-logo-32.png"
-    alt="BetterTTV logo"
-  />
-  <img
-    v-else-if="props.source === 'SevenTV'"
-    src="/img/7tv-logo-32.png"
-    alt="SevenTV logo"
-  />
-  <img
-    v-else-if="props.source === 'Twitch'"
-    src="/img/twitch-logo-32.png"
-    alt="Twitch logo"
-  />
+  <nuxt-link v-if="props.mustWrapToLink" :to="data.href">
+    <img v-bind="$attrs" :src="data.src" :alt="data.alt" />
+  </nuxt-link>
+  <img v-else v-bind="$attrs" :src="data.src" :alt="data.alt" />
 </template>
+<script lang="ts">
+const dataRecord = {
+  BetterTTV: {
+    src: "/img/bttv-logo-32.png",
+    alt: "BetterTTV logo",
+    href: "https://betterttv.com",
+  },
+  FrankerFaceZ: {
+    src: "/img/ffz-logo-32.png",
+    alt: "FrankerFaceZ logo",
+    href: "https://www.frankerfacez.com",
+  },
+  SevenTV: {
+    src: "/img/7tv-logo-32.png",
+    alt: "SevenTV logo",
+    href: "https://7tv.app",
+  },
+  Twitch: {
+    src: "/img/twitch-logo-32.png",
+    alt: "Twitch logo",
+    href: "https://twitch.tv",
+  },
+};
+</script>
 <script lang="ts" setup>
+defineOptions({ inheritAttrs: false });
+
 const props = defineProps<{
   source: import("~/integrations").EmoteSource;
+  mustWrapToLink?: boolean;
 }>();
+
+const data = dataRecord[props.source];
 </script>
