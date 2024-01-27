2ccf845c12f82caf884fc4694c591f4d76331004
geforseN <semen.neb@gmail.com>
2024-01-28 00:38:35 +0700

    add emotes hint components

---
+++ components/app/hint/add-emotes.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col rounded-b-btn border-2 px-2">
+    <span>
+      Showing pastas with only
+      <nuxt-link
+        :to="{
+          path: '/collections/global',
+          hash: '#global-emotes-heading',
+        }"
+        class="link"
+      >
+        global emotes
+      </nuxt-link>
+    </span>
+    <span>
+      Add
+      <nuxt-link
+        :to="{
+          path: '/collections',
+          query: {
+            'focus-fetch-input': 'true',
+          },
+          hash: '#load-user-collection-heading',
+        }"
+        class="link"
+      >
+        user emotes
+      </nuxt-link>
+    </span>
+  </div>
+</template>
