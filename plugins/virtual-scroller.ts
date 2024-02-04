679e1460739ac618fd2153f5bbf41b7238a4b527
geforseN <semen.neb@gmail.com>
2024-02-04 18:46:15 +0700

    chat-pasta-list: change
    add DynamicScrollerItem
    add sort select
    change style: remove first-child

---
+++ plugins/virtual-scroller.ts
@@ -0,0 +1,6 @@
+import VueVirtualScroller from "vue-virtual-scroller";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  vueApp.use(VueVirtualScroller);
+});
