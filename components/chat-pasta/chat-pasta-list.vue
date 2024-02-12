fc64e2b79ae1e2f4074378d9406facd3f76ee049
geforseN <semen.neb@gmail.com>
2024-02-12 23:31:57 +0700

    chat-pasta-list-hints: add

--- components/chat-pasta/chat-pasta-list.vue
+++ components/chat-pasta/chat-pasta-list.vue
@@ -1,83 +1,60 @@
 <template>
-  <!-- TODO: i18n -->
-  <div
-    v-if="pastasStore.pastas.isLoading"
-    class="skeleton flex h-[60dvh] w-[429px] justify-center rounded-none p-2 go-brr:h-[80dvh]"
-  >
-    Loading pastas
-  </div>
-  <template v-if="pastasStore.pastas.isReady">
-    <div v-if="pastasStore.selectedShowStrategy === 'selected-user'">
-      You need to add tag={{ `@${selectedLogin}` }} to some pasta
+  <chat-pasta-list-hints>
+    <div class="rounded-btn rounded-b-none border-2 border-b-0 px-2 py-1.5">
+      <chat-pasta-list-sort-select v-model="pastasStore.selectedSortStrategy" />
+      <chat-pasta-list-show-select
+        v-model="pastasStore.selectedShowStrategy"
+        :selected-login="selectedLogin"
+      />
     </div>
-    <div
-      v-if="!pastasStore.pastas.state.length"
-      class="mt-4 flex justify-center font-bold"
+    <dynamic-scroller
+      v-if="pastasStore.canShowPastas"
+      :items="pastasStore.pastasToShow"
+      :min-item-size="100"
+      class="pasta-list flex max-h-[50dvh] flex-col overflow-y-auto xs:w-[420px] go-brr:max-h-[66dvh]"
     >
-      No pastas were added yet!
-    </div>
-  </template>
-  <client-only>
-    <div
-      v-if="!userStore.clipboard.isSupported"
-      class="alert alert-warning flex justify-center"
-    >
-      <span>
-        Your browser does not support Clipboard API!. Copy of pasta by clicking
-        on the button will fail
-      </span>
-    </div>
-  </client-only>
-  <chat-pasta-list-sort-select v-model="pastasStore.selectedSortStrategy" />
-  <chat-pasta-list-show-select
-    v-model="pastasStore.selectedShowStrategy"
-    :selected-login="selectedLogin"
-  />
-  <dynamic-scroller
-    v-if="pastasStore.canShowPastas"
-    :items="pastasStore.pastasToShow"
-    :min-item-size="100"
-    class="pasta-list flex max-h-[60dvh] flex-col gap-y-2 overflow-y-auto xs:w-[426px] go-brr:max-h-[77dvh]"
-  >
-    <template #default="{ item: pasta, index, active }">
-      <dynamic-scroller-item
-        :item="pasta"
-        :active="active"
-        :size-dependencies="[pasta.message]"
-        :data-index="index"
-      >
-        <chat-pasta
-          :key="`${pasta.id}:${pasta.text}`"
-          :pasta="pasta"
-          @populate="
-            (pastaTextContainer) => {
-              populatePasta(pastaTextContainer, pasta, emotesStore);
-            }
-          "
+      <template #default="{ item: pasta, index, active }">
+        <dynamic-scroller-item
+          :item="pasta"
+          :active="active"
+          :size-dependencies="[pasta.message]"
+          :data-index="index"
         >
-          <template #creatorData><slot name="creatorData" /></template>
-          <template #sidebar>
-            <chat-pasta-sidebar
-              dropdown-class="dropdown dropdown-top xs:dropdown-end dropdown-hover"
-              :pasta-id="pasta.id"
-              :is-clipboard-supported="userStore.clipboard.isSupported"
-              @copy="userStore.copyPasta(pasta)"
-              @delete="pastasStore.removePasta(pasta)"
-            />
-          </template>
-        </chat-pasta>
-      </dynamic-scroller-item>
-    </template>
-  </dynamic-scroller>
+          <chat-pasta
+            :key="`${pasta.id}:${pasta.text}`"
+            :pasta="pasta"
+            @populate="
+              (pastaTextContainer) => {
+                populatePasta(pastaTextContainer, pasta, emotesStore);
+              }
+            "
+          >
+            <template #creatorData>
+              <chat-pasta-creator-data
+                :badges-count="userStore.user.badges.count.state"
+                :nickname="userStore.user.nickname.text.state"
+                :nickname-color="userStore.user.nickname.color.state"
+              />
+            </template>
+            <template #sidebar>
+              <chat-pasta-sidebar
+                dropdown-class="dropdown dropdown-top xs:dropdown-end dropdown-hover"
+                :pasta-id="pasta.id"
+                :is-clipboard-supported="userStore.clipboard.isSupported"
+                @copy="userStore.copyPasta(pasta)"
+                @delete="pastasStore.removePasta(pasta)"
+              />
+            </template>
+          </chat-pasta>
+        </dynamic-scroller-item>
+      </template>
+    </dynamic-scroller>
+  </chat-pasta-list-hints>
 </template>
 <script lang="ts">
 export const l = "pasta.list." as const;
 </script>
 <script setup lang="ts">
-defineSlots<{
-  creatorData?: () => unknown;
-}>();
-
 const pastasStore = usePastasStore();
 const userStore = useUserStore();
 const emotesStore = useEmotesStore();
