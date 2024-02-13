5ed6a39daf46cd462ab75573b4c9c0d40321c5b9
geforseN <semen.neb@gmail.com>
2024-02-13 23:25:08 +0700

    utils: add getLengthStatus

--- utils/index.ts
+++ utils/index.ts
@@ -4,3 +4,19 @@ export function isNotNullish<T>(value: T): value is NonNullable<T> {
 export function booleanish<B extends boolean>(value: B) {
   return String(value) as "true" | "false";
 }
+
+export function getLengthStatus(
+  length: number,
+  options: { min: number; max: number },
+) {
+  if (!length) {
+    return "empty";
+  }
+  if (length < options.min) {
+    return "tooShort";
+  }
+  if (length > options.max) {
+    return "tooLong";
+  }
+  return "ok";
+}
