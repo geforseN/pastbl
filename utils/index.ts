e6b7215c17eb49e543515dc765910e508b1db477
geforseN <geforsenjs@gmail.com>
2024-02-14 17:00:14 +0700

    utils: add writableComputedForKey

--- utils/index.ts
+++ utils/index.ts
@@ -20,3 +20,18 @@ export function getLengthStatus(
   }
   return "ok";
 }
+
+export function writableComputedForKey<
+  T extends object,
+  K extends keyof T,
+  V = T[K],
+>(ref_: Ref<T>, key: K) {
+  return computed({
+    get() {
+      return ref_.value[key] as V;
+    },
+    set(value: V) {
+      (ref_.value[key] as V) = value;
+    },
+  });
+}
