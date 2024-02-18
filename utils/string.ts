da7be7f41b1363cfd5c57aa8ef456a9b3317fe11
geforseN <geforsenjs@gmail.com>
2024-02-18 19:34:19 +0700

    megaTrim: move

--- utils/string.ts
+++ utils/string.ts
@@ -1,3 +1,12 @@
 export function toLowerCase(str: string) {
   return str.toLowerCase() as Lowercase<string>;
 }
+
+export function megaTrim(str: string) {
+  return str
+    .trim()
+    .split(" ")
+    .map((word) => word.trim())
+    .filter((word) => word.length)
+    .join(" ");
+}
