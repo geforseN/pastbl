2b76d922cbe76e724a9386954950a237bacd25e0
geforseN <geforsenjs@gmail.com>
2024-02-21 23:56:39 +0700

    hovered-emote-hint: change styles

--- components/hovered-emote-hint.vue
+++ components/hovered-emote-hint.vue
@@ -1,10 +1,13 @@
 <template>
   <div
     ref="hoveredEmoteContainerRef"
-    class="pointer-events-none absolute z-50 touch-none bg-slate-600"
+    class="pointer-events-none absolute z-50 touch-none"
   >
-    <div v-if="emote_" class="flex flex-col">
-      <div class="flex items-center gap-1">
+    <div
+      v-if="emote_"
+      class="flex flex-col items-center gap-1 rounded-lg border bg-base-100 p-2"
+    >
+      <div class="flex items-center gap-1 p-1">
         <template v-for="image of images" :key="image">
           <img
             :src="image.src"
@@ -15,13 +18,25 @@
           />
         </template>
       </div>
-      {{ emote_.token }}
-      {{ emote_.source }}
-      {{ emote_.type }}
-      <span v-if="emote_.isAnimated">animated</span>
-      <span v-if="emote_.isListed">listed</span>
-      <span v-if="emote_.isModifier">modifier</span>
-      <span v-if="emote_.isWrapper">wrapper</span>
+      <div class="flex items-baseline text-lg">
+        <span class="text-xl font-bold">{{ emote_.token }}</span>
+        &nbsp;{{ "-" }}&nbsp;
+        <div class="flex items-baseline">
+          <emote-integration-logo
+            :source="emote_.source"
+            width="20"
+            class="h-min self-center"
+          />&nbsp;{{ emote_.source }}&nbsp;{{ emote_.type }}&nbsp;emote
+        </div>
+      </div>
+      <dev-only>
+        <div class="flex gap-1 text-yellow-400">
+          <span v-if="emote_.isAnimated">animated</span>
+          <span v-if="emote_.isListed">listed</span>
+          <span v-if="emote_.isModifier">modifier</span>
+          <span v-if="emote_.isWrapper">wrapper</span>
+        </div>
+      </dev-only>
     </div>
   </div>
 </template>
