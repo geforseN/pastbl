72fceb5641cb4e534ecbe5cbd847a2c3a213f7ae
geforseN <semen.neb@gmail.com>
2024-02-22 17:48:43 +0700

    pastas page: add

---
+++ pages/pastas/index.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <app-page-link to="main" />
+    <app-page-link to="find-pasta">
+      <template #right>🔍</template>
+    </app-page-link>
+    <app-page-link to="user-settings">
+      <template #right>⚙️</template>
+    </app-page-link>
+    <app-page-link to="emotes">
+      <template #right><emote-integration-logos /></template>
+    </app-page-link>
+    <div class="flex flex-col gap-1">
+      <button
+        class="btn btn-primary"
+        @click="() => savePastasToFile(pastasStore.pastas.state)"
+      >
+        Save pastas to file
+      </button>
+      <article class="form-control rounded-btn border p-2">
+        <label for="load-pastas">
+          <h3 class="text-xl font-bold">Load pastas</h3>
+        </label>
+        <input
+          id="load-pastas"
+          name="load-pastas"
+          class="file-input file-input-primary"
+          type="file"
+          accept="application/json"
+          @change="
+            (event) => {
+              assert.ok(reader);
+              loadPastasFromFile(event, reader);
+            }
+          "
+        />
+      </article>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { pastasService } from "~/client-only/services";
+
+type MinimalPasta = {
+  id?: number;
+  tags?: string[];
+  text: string;
+  createdAt?: ReturnType<Date["toISOString"]>;
+};
+
+function isMinimalPasta(data: unknown): data is MinimalPasta {
+  return (
+    isObject(data) &&
+    (data.id === undefined ||
+      (typeof data.id === "number" &&
+        Number.isInteger(data.id) &&
+        data.id > 0)) &&
+    typeof data.text === "string" &&
+    getTextStatus(data.text) !== "error" &&
+    (typeof data.tags === "undefined" || isStringArray(data.tags)) &&
+    (data.createdAt === undefined ||
+      (typeof data.createdAt === "string" && isIsoDate(data.createdAt)))
+  );
+}
+
+function makeMinimalPasta(pasta: IDBMegaPasta) {
+  return {
+    id: pasta.id,
+    tags: pasta.tags.length ? pasta.tags : undefined,
+    text: pasta.text,
+    createdAt: new Date(pasta.createdAt).toISOString(),
+  };
+}
+
+function loadPastasFromFile(event: Event, reader: FileReader) {
+  assert.ok(reader && event.target);
+  const { files } = event.target as unknown as { files: FileList };
+  assert.ok(files instanceof FileList);
+  reader.readAsText(files[0]);
+}
+
+function savePastasToFile(pastasToSave: IDBMegaPasta[]) {
+  const link = document.createElement("a");
+  const minimalPastas = pastasToSave
+    .filter(isMinimalPasta)
+    .map(makeMinimalPasta);
+  link.href = window.URL.createObjectURL(
+    new Blob([JSON.stringify(minimalPastas, null, 2)], {
+      type: "application/json",
+    }),
+  );
+  link.download = "pastas.json";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function getMegaPastasOnFileLoad(event: ProgressEvent<FileReader>) {
+  assert.ok(event.target);
+  const { result: fileContent } = event.target;
+  try {
+    assert.ok(typeof fileContent === "string");
+    const pastasJson = JSON.parse(fileContent);
+    assert.ok(isArray(pastasJson));
+    const minimalPastas = pastasJson.filter(isMinimalPasta);
+    const megaPastas = minimalPastas.map((pasta) => {
+      const trimmedText = megaTrim(pasta.text);
+      return createMegaPasta(trimmedText, pasta.tags);
+    });
+    return megaPastas;
+  } catch (error) {
+    assert.isError(error);
+    throw error;
+  }
+}
+</script>
+<script lang="ts" setup>
+const pastasStore = usePastasStore();
+
+const reader = process.client ? new FileReader() : null;
+
+if (process.client) {
+  assert.ok(reader);
+  reader.onload = async function (event) {
+    const megaPastas = getMegaPastasOnFileLoad(event);
+    const [success, failed] = await tupleSettledPromises(
+      megaPastas.map((pasta) => pastasService.add(pasta)),
+    );
+    pastasStore.pastas.state = [
+      ...pastasStore.pastas.state,
+      ...success.toSorted((a, b) => a.id - b.id),
+    ];
+    assert.ok(!failed.length);
+  };
+}
+</script>
