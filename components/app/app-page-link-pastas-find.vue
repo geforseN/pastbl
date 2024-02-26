a1675138db5d1aee8f39f80692bb5e4fcca6736d
geforseN <semen.neb@gmail.com>
2024-02-26 15:12:56 +0700

    page-link: change
    make text smaller
    add left slot (unused right now)
    add components with slots

---
+++ components/app/app-page-link-pastas-find.vue
@@ -0,0 +1,14 @@
+<template>
+  <app-page-link to="find-pasta">
+    <template #right>
+      <div class="flex items-center gap-2">
+        <span class="text-nowrap text-xs">
+          <kbd class="kbd pt-1">Alt</kbd>
+          <span class="mx-0.5">+</span>
+          <kbd class="kbd pt-1">F</kbd>
+        </span>
+        🔍
+      </div>
+    </template>
+  </app-page-link>
+</template>
