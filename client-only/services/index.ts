bca9d17857be8e18236ff78a54dd49a9135fd55e
geforseN <geforsenjs@gmail.com>
2024-03-06 18:14:23 +0700

    add emotesIDB to userCollections

--- client-only/services/index.ts
+++ client-only/services/index.ts
@@ -2,3 +2,4 @@ export { userCollectionsService } from "./userCollections";
 export { pastasService } from "./pastas";
 export { globalCollectionsService } from "./globalCollections";
 export { keyValueService } from "./keyValue";
+export { emotesIDB } from "./emotes";
