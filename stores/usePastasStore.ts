112bb7af3b1dd2cfdfd32720dd0026d8fc4063f9
geforseN <semen.neb@gmail.com>
2024-03-06 18:21:46 +0700

    fix serverless toSorted

--- stores/usePastasStore.ts
+++ stores/usePastasStore.ts
@@ -14,18 +14,18 @@ function usePastasSort(allPastas: Ref<IDBMegaPasta[]>) {
   );
   const sortedPastas = {
     "newest-first": computed(() =>
-      allPastas.value.toSorted((a, b) => b.createdAt - a.createdAt),
+      [...allPastas.value].sort((a, b) => b.createdAt - a.createdAt),
     ),
     "oldest-first": computed(() =>
-      allPastas.value.toSorted((a, b) => a.createdAt - b.createdAt),
+      [...allPastas.value].sort((a, b) => a.createdAt - b.createdAt),
     ),
     "last-updated": computed(() =>
-      allPastas.value.toSorted(
+      [...allPastas.value].sort(
         (a, b) => (b.updatedAt || b.createdAt) - (a.updatedAt || a.createdAt),
       ),
     ),
     "last-copied": computed(() =>
-      allPastas.value.toSorted((a, b) => {
+      [...allPastas.value].sort((a, b) => {
         if (a.lastCopiedAt && b.lastCopiedAt) {
           return b.lastCopiedAt - a.lastCopiedAt;
         }
