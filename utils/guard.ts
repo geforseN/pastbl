a4945783b3701be4c645b85e3be2181bae2088d4
geforseN <semen.neb@gmail.com>
2024-03-06 18:31:31 +0700

    move to guard utils & rename booleanish

---
+++ utils/guard.ts
@@ -0,0 +1,37 @@
+export function isNotNullable<T>(value: T): value is NonNullable<T> {
+  return value !== undefined && value !== null;
+}
+
+export const isObject = (obj: unknown): obj is Record<string, unknown> =>
+  obj !== null && typeof obj === "object";
+
+export function isIsoDate(str: string) {
+  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) {
+    return false;
+  }
+  const d = new Date(str);
+  return (
+    d instanceof Date && !Number.isNaN(d.getTime()) && d.toISOString() === str
+  );
+}
+
+export function isIsoDate2(str: string) {
+  if (str.includes("T")) {
+    if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(str)) {
+      return false;
+    }
+  } else if (!/\d{4}-\d{2}-\d{2}/.test(str)) {
+    return false;
+  }
+  const d = new Date(str);
+  return (
+    d instanceof Date &&
+    !Number.isNaN(d.getTime()) &&
+    d.toISOString().startsWith(str)
+  );
+}
+
+export const isArray = Array.isArray;
+
+export const isStringArray = (value: unknown): value is string[] =>
+  isArray(value) && value.every((v) => typeof v === "string");
