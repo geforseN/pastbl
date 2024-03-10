2a28364dd2ae773286bfbdf85278fd188e45dcbd
geforseN <semen.neb@gmail.com>
2024-03-10 23:13:49 +0700

    pastas/edit/[pastaId] page: remove links

--- pages/pastas/edit/[pastaId].vue
+++ pages/pastas/edit/[pastaId].vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex flex-col gap-2">
     <chat-pasta-edit />
-    <app-page-link-pastas-find />
-    <app-page-link-user-settings />
     <app-page-link-emotes />
     <app-page-link-main />
   </div>
