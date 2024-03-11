e97933fcb00e67d3e9f808d668c48aaa3297305d
geforseN <geforsenjs@gmail.com>
2024-03-11 18:36:58 +0700

    pasta-form-tags-input: add length assert

--- components/pasta-form/pasta-form-tags-input.vue
+++ components/pasta-form/pasta-form-tags-input.vue
@@ -1,35 +1,45 @@
 <template>
-  <div class="join w-full">
-    <input
-      id="add-tag"
-      v-model="modelValue"
-      class="input join-item input-bordered box-content w-full border-base-content placeholder:text-base-content/80 empty:pr-0 xl:w-auto xl:min-w-[320px]"
-      type="text"
-      :placeholder="$t('pasta.formCommon.tag.inputPlaceholder')"
-      list="add-tag-suggestions"
-      @keyup="
-        (event) => {
-          if (event.key === ',') {
-            addTagOnCommaPress(event);
+  <div class="flex flex-col">
+    <div class="join">
+      <input
+        id="add-tag"
+        v-model="modelValue"
+        class="input join-item input-bordered box-content w-full border-base-content placeholder:text-base-content/80 empty:pr-0 xl:w-auto xl:min-w-[320px]"
+        :class="isTagTooLong && 'border-error focus:outline-error'"
+        type="text"
+        :placeholder="$t('pasta.formCommon.tag.inputPlaceholder')"
+        list="add-tag-suggestions"
+        @keyup="
+          (event) => {
+            if (event.key === ',') {
+              addTagOnCommaPress(event);
+            }
           }
-        }
-      "
-      @keyup.enter.prevent="emitTag"
-    />
-    <datalist id="add-tag-suggestions">
-      <slot name="addTagSuggestions" />
-    </datalist>
-    <button
-      class="btn btn-secondary join-item box-content grow px-2"
-      @click.prevent="emitTag"
-    >
-      <span class="text-xl">+</span>
-      {{ $t("pasta.formCommon.tag.addButton") }}
-    </button>
+        "
+        @keyup.enter.prevent="emitTag"
+      />
+      <datalist id="add-tag-suggestions">
+        <slot name="addTagSuggestions" />
+      </datalist>
+      <button
+        class="btn btn-secondary join-item box-content grow px-2"
+        @click.prevent="emitTag"
+      >
+        <span class="text-xl">+</span>
+        {{ $t("pasta.formCommon.tag.addButton") }}
+      </button>
+    </div>
+    <span v-if="isTagTooLong" class="underline decoration-error">
+      {{ $t("toast.addTag.fail.tooLongMessage", pastaTagLength) }}
+    </span>
   </div>
 </template>
 <script setup lang="ts">
 const modelValue = defineModel<string>({ required: true });
+
+const isTagTooLong = computed(
+  () => modelValue.value.length > pastaTagLength.max,
+);
 
 const emit = defineEmits<{ addTag: [tagToAdd: string] }>();
 
