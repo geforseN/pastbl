5cf80043394ff5a983e26f4cac435c68d4e669c1
geforseN <semen.neb@gmail.com>
2024-03-13 20:19:43 +0700

    tailwind colors: update

--- components/app/top-nav.vue
+++ components/app/top-nav.vue
@@ -10,7 +10,7 @@
       </li>
       <li class="hidden go-brr:block">
         <nuxt-link-locale
-          class="btn btn-outline flex-nowrap text-wrap border-twitch text-twitch"
+          class="btn btn-outline flex-nowrap text-wrap border-twitch-accent text-twitch-accent"
           to="/collections"
         >
           <strong>{{ $t("collections.index.link") }}</strong>
@@ -33,6 +33,55 @@
         </nuxt-link-locale>
       </li>
       <li class="xs:!ml-auto" />
+      <li class="absolute right-[6rem] top-2 sm:static">
+        <div
+          v-if="userSession.loggedIn && userSession.user"
+          class="flex h-12 items-center space-x-2 divide-x rounded-btn border p-2"
+        >
+          <div class="flex h-6 items-center gap-1">
+            <img
+              width="24"
+              height="24"
+              class="min-h-6 min-w-6 rounded-full"
+              :src="userSession.user.twitch.profileImageUrl"
+              :alt="`${userSession.user.twitch.nickname} avatar`"
+            />
+            {{ userSession.user.twitch.nickname }}
+          </div>
+          <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button">
+              <icon name="ic:arrow-drop-down" class="min-w-6" size="24" />
+            </div>
+            <div
+              tabindex="0"
+              class="menu dropdown-content z-[1] w-max rounded-box bg-base-100 p-2 shadow"
+            >
+              <button class="btn btn-outline" @click="userSession.clear">
+                <span
+                  class="group-hover:underline group-hover:decoration-2 group-hover:underline-offset-2"
+                >
+                  {{ $t("account.logout") }}
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <a
+          v-else
+          href="/api/auth/twitch"
+          class="group btn btn-ghost flex-nowrap bg-twitch-accent text-base-300"
+        >
+          <span
+            class="text-lg/1 group-hover:underline group-hover:decoration-2 group-hover:underline-offset-2"
+          >
+            {{ t("account.loginWithTwitch") }}
+          </span>
+          <emote-integration-logo
+            source="Twitch"
+            class="h-6 w-6 translate-y-0.5"
+          />
+        </a>
+      </li>
       <li>
         <select
           id="select-locale"
@@ -78,6 +127,8 @@ const themes = computedWithControl(locale, () => ({
   dark: t("theme.dark"),
   light: t("theme.light"),
 }));
+
+const userSession = reactive(useUserSession());
 
 async function changeLocale(event: Event) {
   assert.ok(event instanceof Event && event.target);
--- components/emote-collection/index.ts
+++ components/emote-collection/index.ts
@@ -13,28 +13,28 @@ export type Colors = {
 
 export const colorsClassRecord: Record<EmoteSource, Colors> = {
   BetterTTV: {
-    border: "border-[#63b3ed]",
-    background: "bg-[#181d1f]",
-    outline: "outline-[#63b3ed]",
-    asd: "bg-[#63b3ed]",
+    border: "border-bttv-accent",
+    background: "bg-bttv-base",
+    outline: "outline-bttv-accent",
+    asd: "bg-bttv-accent",
   },
   FrankerFaceZ: {
-    border: "border-ffz",
-    background: "bg-[#222222]",
-    outline: "outline-ffz",
-    asd: "bg-ffz",
+    border: "border-ffz-accent",
+    background: "bg-ffz-base",
+    outline: "outline-ffz-accent",
+    asd: "bg-ffz-accent",
   },
   SevenTV: {
-    border: "border-[#2599cd]",
-    background: "bg-[#181d1f]",
-    outline: "outline-[#2599cd]",
-    asd: "bg-[#2599cd]",
+    border: "border-7tv-accent",
+    background: "bg-7tv-base",
+    outline: "outline-7tv-accent",
+    asd: "bg-7tv-accent",
   },
   Twitch: {
-    border: "border-[#a970ff]",
-    background: "bg-[#0E0E10]",
-    outline: "outline-[#a970ff]",
-    asd: "bg-[#a970ff]",
+    border: "border-twitch-accent",
+    background: "bg-twitch-base",
+    outline: "outline-twitch-accent",
+    asd: "bg-twitch-accent",
   },
 };
 
