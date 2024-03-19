84369e2bf5f68fd145d23ffcf216067f6610fa9f
geforseN <semen.neb@gmail.com>
2024-03-19 18:57:49 +0700

    refactor delete-button

--- components/user-collection/delete-button-dialog.vue
+++ components/user-collection/delete-button-dialog.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="relative">
-    <button
-      v-if="!mustRevealConfirmDeleteDialog"
+    <slot
       ref="deleteButtonRef"
-      :class="props.buttonClass"
-      @click="
+      name="default"
+      :reveal-dialog="
         () => {
           mustRevealConfirmDeleteDialog = true;
           nextTick(() => {
-            cancelDeleteButtonRef?.focus();
+            cancelButtonRef!.focus();
           });
         }
       "
+    />
+    <div
+      v-if="mustRevealConfirmDeleteDialog"
+      class="card card-compact absolute z-[1] w-72 border-2 bg-base-100 p-2 text-base-content"
+      :class="$attrs"
     >
-      {{ $t("collections.users.ready.button.delete") }}
-      <icon name="ic:round-delete-outline" class="-ml-2" />
-    </button>
-    <div v-else :class="props.cardClass">
       <div ref="cardRef" class="card-body">
         <h3 class="card-title">
           {{ $t("collections.users.ready.delete-text") }}
         </h3>
         <div class="flex gap-2">
           <button class="btn btn-error btn-sm grow" @click="emit('delete')">
-            {{ $t("collections.users.ready.button.delete") }}
+            {{ $t("delete") }}
           </button>
           <button
-            ref="cancelDeleteButtonRef"
+            ref="cancelButtonRef"
             class="btn btn-outline btn-sm grow"
             @click="
               () => {
@@ -45,26 +45,16 @@
   </div>
 </template>
 <script setup lang="ts">
+defineOptions({ inheritAttrs: false });
+
 const deleteButtonRef = ref<HTMLButtonElement>();
-const cancelDeleteButtonRef = ref<HTMLDivElement>();
+const cancelButtonRef = ref<HTMLDivElement>();
 const cardRef = ref<HTMLDivElement>();
 
-const props = withDefaults(
-  defineProps<{
-    nickname: string;
-    buttonClass?: string;
-    cardClass?: string;
-  }>(),
-  {
-    buttonClass: "btn btn-error btn-sm border border-error-content",
-    cardClass:
-      "card card-compact absolute -top-4 left-0 z-[1] w-72 border-2 bg-base-100 p-2 text-base-content",
-  },
-);
+const mustRevealConfirmDeleteDialog = ref(false);
 
 const emit = defineEmits<{ delete: [] }>();
 
-const mustRevealConfirmDeleteDialog = ref(false);
 onClickOutside(cardRef, () => {
   mustRevealConfirmDeleteDialog.value = false;
 });
