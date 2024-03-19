586b277e490fce6ed3a284c681a9c141b83ad7fc
geforseN <geforsenjs@gmail.com>
2024-03-19 19:06:17 +0700

    updated-at: refactor

--- components/emote-collection/__updated-at.vue
+++ components/emote-collection/__updated-at.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="flex items-baseline justify-between gap-2">
     <div>
-      <use-time-ago :time="date" #="{ timeAgo }">
-        <time :datetime="date.toISOString()"> Updated {{ timeAgo }} </time>
-      </use-time-ago>
+      <time :datetime="time.toISOString()">
+        {{ $t("updated") }}
+        {{ timeAgo }}
+      </time>
       <div class="dropdown dropdown-end dropdown-hover ml-1">
         <div tabindex="0" role="button">
           <icon name="ic:outline-info" size="20" class="text-info" />
@@ -12,27 +13,20 @@
           class="card dropdown-content compact z-[1] w-max rounded-box border bg-base-100 shadow"
         >
           <div class="card-body font-bold text-base-content">
-            Updated at
-            {{ date.toLocaleString() }}
+            {{ $t("updated") }}
+            {{ time.toLocaleString() }}
           </div>
         </div>
       </div>
     </div>
-    <button class="btn btn-success btn-xs w-min" @click="emit('refresh')">
-      Refresh
-    </button>
   </div>
 </template>
 <script setup lang="ts">
-import { UseTimeAgo } from "@vueuse/components";
-
-const { updatedAt } = defineProps<{
-  updatedAt: number;
+const props = defineProps<{
+  time: Date | number | string;
 }>();
 
-const emit = defineEmits<{
-  refresh: [];
-}>();
+const time = computed(() => new Date(props.time));
 
-const date = computed(() => new Date(updatedAt));
+const timeAgo = useI18TimeAgo(time);
 </script>
