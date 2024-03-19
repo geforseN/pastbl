aa781f39c448b46b2be011a90798b2c712858050
geforseN <semen.neb@gmail.com>
2024-03-19 19:01:29 +0700

    add buttonComponentsStyles

--- components/emote-collection/index.ts
+++ components/emote-collection/index.ts
@@ -66,3 +66,18 @@ export type GlobalCollectionProps<SourceT extends EmoteSource> = Props<
   SourceT,
   IGlobalEmoteCollectionRecord
 >;
+
+export const buttonComponentsStyles = {
+  xs: {
+    btn: "btn-xs",
+    icon: "18",
+    iconClass: "min-w-[18px]",
+  },
+  sm: {
+    btn: "btn-sm",
+    icon: "20",
+    iconClass: "min-w-5",
+  },
+} as const;
+
+export type ButtonSize = keyof typeof buttonComponentsStyles;
