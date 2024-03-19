3ba552a721261cd236b9140878c3e9bade24a68e
geforseN <geforsenjs@gmail.com>
2024-03-19 18:59:49 +0700

    refresh-button: add

---
+++ components/emote-collection/refresh-button.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+    class="btn btn-accent"
+    :class="[size.btn, props.isRefreshing && props.refreshingClass]"
+    :disabled="props.isRefreshing"
+  >
+    {{ props.isRefreshing ? $t("refreshing") : $t("refresh") }}
+    <icon
+      name="ic:round-refresh"
+      :size="size.icon"
+      :class="[
+        size.iconClass,
+        props.iconClass,
+        props.isRefreshing && props.iconRefreshingClass,
+      ]"
+    />
+  </button>
+</template>
+<script lang="ts" setup>
+import { buttonSizes, type ButtonSize } from ".";
+
+const props = withDefaults(
+  defineProps<{
+    size: ButtonSize;
+    isRefreshing: boolean;
+    iconClass?: string;
+    iconRefreshingClass?: string;
+    refreshingClass?: string;
+  }>(),
+  {
+    iconClass: "",
+    iconRefreshingClass: "animate-spin",
+    refreshingClass:
+      "animate-pulse disabled:border-2 disabled:border-base-content disabled:border-opacity-100 disabled:bg-accent disabled:text-opacity-100",
+  },
+);
+
+const size = computed(() => buttonSizes[props.size]);
+</script>
