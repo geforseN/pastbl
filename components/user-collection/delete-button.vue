a3420f6a2655520063af65407c9591153a1f9d2e
geforseN <geforsenjs@gmail.com>
2024-03-19 18:57:49 +0700

    refactor delete-button

---
+++ components/user-collection/delete-button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="btn btn-error flex-nowrap" :class="[size.btn]">
+    {{ $t("delete") }}
+    <icon
+      name="ic:round-delete-outline"
+      :size="size.icon"
+      :class="[size.iconClass, props.iconClass]"
+    />
+  </button>
+</template>
+<script lang="ts" setup>
+import { buttonSizes, type ButtonSize } from "../emote-collection";
+
+const props = defineProps<{
+  size: ButtonSize;
+  iconClass?: string;
+}>();
+
+const size = computed(() => buttonSizes[props.size]);
+</script>
