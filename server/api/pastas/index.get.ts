6142fc4020d50de3ff09cb92154d01a209e39d10
geforseN <semen.neb@gmail.com>
2024-03-27 02:11:46 +0700

    change pastas find

--- server/api/pastas/index.get.ts
+++ server/api/pastas/index.get.ts
@@ -1,7 +1,35 @@
-export default defineEventHandler(async (event) => {
-  const session = await requireUserSession(event);
+import assert from "node:assert";
+import { z } from "zod";
+import { getPastas } from "~/db";
 
+const querySchema = z
+  .object({
+    next: z.string().nullish(),
+  })
+  .transform(({ next }) => {
+    if (next === null || next === undefined) {
+      return { next: null };
+    }
+    const number = Number(next);
+    assert.ok(
+      Number.isInteger(number),
+      createError({
+        statusCode: 400,
+        message: "Next cursor must be a integer",
+      }),
+    );
+    return {
+      next: number,
+    };
+  });
+
+export default defineEventHandler(async (event) => {
+  const { user } = await requireUserSession(event);
+  const { next } = querySchema.parse(getQuery(event));
+  const { pastas, next: newNext } = await getPastas(user.twitch.id, next);
   return {
-    session,
+    pastas,
+    next: newNext,
+    user,
   };
 });
