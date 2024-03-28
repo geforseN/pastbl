a8a39c150e77007dc4b1bb0b6b175dd921afc4a6
geforseN <semen.neb@gmail.com>
2024-03-28 20:31:05 +0700

    update db methods

--- db/index.ts
+++ db/index.ts
@@ -1,11 +1,13 @@
-import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { and, desc, eq, lt } from "drizzle-orm";
-import { pastas, pastasTags, type Pasta, type PastaPublicity } from "./schema";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { and, eq, sql } from "drizzle-orm";
+import { pastas, pastasTags, type Pasta } from "./schema";
 import * as schema from "./schema";
+import { setDifferenceOtTwoSets } from "~/utils/set";
+import type { PastaPublicity } from "~/server/utils/pastas";
 
 export const client = postgres(env.databaseUrl);
-export const db = drizzle(client, { schema });
+export const db = drizzle(client, { schema, logger: true });
 
 export function createPasta(
   text: string,
@@ -22,7 +24,8 @@ export function createPasta(
       const pastaId = pasta.id;
       await tx
         .insert(pastasTags)
-        .values(tags.map((value) => ({ value, pastaId })));
+        .values(tags.map((value) => ({ value, pastaId })))
+        .onConflictDoNothing();
     }
     return {
       ...pasta,
@@ -33,80 +36,165 @@ export function createPasta(
 
 // TODO: use 'with clause' or/and 'prepared statement' in SQL queries
 
-const FIRST_PAGE_PASTAS_COUNT = 15;
+const CURSOR_FIRST_PAGE_PASTAS_COUNT = 15 as const;
 
-function getFirstPagePastas(publisherTwitchId: Pasta["publisherTwitchId"]) {
-  return db.query.pastas.findMany({
+const getFirstPagePastas = db.query.pastas
+  .findMany({
     columns: {
       updatedAt: false,
       publisherTwitchId: false,
     },
-    where: eq(pastas.publisherTwitchId, publisherTwitchId),
-    orderBy: [pastas.publisherTwitchId, desc(pastas.id)],
-    limit: FIRST_PAGE_PASTAS_COUNT,
+    where: (pastas, { eq }) =>
+      eq(pastas.publisherTwitchId, sql.placeholder("publisherTwitchId")),
+    orderBy: (pastas, { desc }) => [pastas.publisherTwitchId, desc(pastas.id)],
+    limit: CURSOR_FIRST_PAGE_PASTAS_COUNT,
     with: {
-      tags: true,
+      tags: {
+        columns: { value: true },
+      },
     },
-  });
-}
+  })
+  .prepare("get_first_pastas");
 
-const CURSOR_BASED_PASTAS_COUNT = 10;
+const CURSOR_NEXT_PAGES_PASTAS_COUNT = 10 as const;
 
-function getNextPagePastas(
-  publisherTwitchId: Pasta["publisherTwitchId"],
-  pastaId: Pasta["id"],
-) {
-  return db.query.pastas.findMany({
+const getNextPagePastas = db.query.pastas
+  .findMany({
     columns: {
       updatedAt: false,
       publisherTwitchId: false,
     },
-    where: and(
-      eq(pastas.publisherTwitchId, publisherTwitchId),
-      lt(pastas.id, pastaId),
-    ),
-    orderBy: [pastas.publisherTwitchId, desc(pastas.id)],
-    limit: CURSOR_BASED_PASTAS_COUNT,
+    where: (pastas, { and, eq, lt }) =>
+      and(
+        eq(pastas.publisherTwitchId, sql.placeholder("publisherTwitchId")),
+        lt(pastas.id, sql.placeholder("pastaId")),
+      ),
+    orderBy: (pastas, { desc }) => [pastas.publisherTwitchId, desc(pastas.id)],
+    limit: CURSOR_NEXT_PAGES_PASTAS_COUNT,
     with: {
-      tags: true,
+      tags: {
+        columns: { value: true },
+      },
     },
-  });
+  })
+  .prepare("get_next_pastas");
+
+function getPastasCursor(length: number, pastas: { id: number }[]) {
+  if (pastas.length !== length) {
+    return null;
+  }
+  return pastas[length - 1].id;
 }
+
+const getFirstPastasCursor = getPastasCursor.bind(
+  null,
+  CURSOR_FIRST_PAGE_PASTAS_COUNT,
+);
+const getNextPastasCursor = getPastasCursor.bind(
+  null,
+  CURSOR_NEXT_PAGES_PASTAS_COUNT,
+);
+
+// LINK: https://orm.drizzle.team/learn/guides/cursor-based-pagination
 
 export async function getPastas(
-  twitchUserId: string,
-  next: Pasta["id"] | null,
+  publisherTwitchId: string,
+  pastaId: Pasta["id"] | null,
 ) {
-  if (next === null) {
-    const pastas = await getFirstPagePastas(twitchUserId);
-    const next =
-      pastas.length === FIRST_PAGE_PASTAS_COUNT
-        ? pastas[pastas.length - 1].id
-        : null;
+  if (pastaId === null) {
+    const pastas = await getFirstPagePastas.execute({
+      publisherTwitchId,
+    });
+    const cursor = getFirstPastasCursor(pastas);
     return {
       pastas,
-      next,
+      cursor,
     };
   }
-  const pastas = await getNextPagePastas(twitchUserId, next);
+  const pastas = await getNextPagePastas.execute({
+    publisherTwitchId,
+    pastaId,
+  });
   return {
     pastas,
-    next:
-      pastas.length === CURSOR_BASED_PASTAS_COUNT
-        ? pastas[pastas.length - 1].id
-        : null,
+    cursor: getNextPastasCursor(pastas),
   };
 }
 
-export function deletePasta(pastaId: Pasta["id"]) {
-  return db.delete(pastas).where(eq(pastas.id, pastaId)).returning();
+export async function deletePasta(
+  publisherTwitchId: Pasta["publisherTwitchId"],
+  pastaId: Pasta["id"],
+) {
+  await db
+    .delete(pastas)
+    .where(
+      and(
+        eq(pastas.publisherTwitchId, publisherTwitchId),
+        eq(pastas.id, pastaId),
+      ),
+    );
 }
 
-// LINK: https://orm.drizzle.team/docs/joins#many-to-many-example
+export async function patchPastaText(
+  pastaId: Pasta["id"],
+  publisherTwitchId: Pasta["publisherTwitchId"],
+  text: Pasta["text"],
+) {
+  // TODO: with transaction add old pastas in previousPastas table
+  return await db
+    .update(pastas)
+    .set({ text, updatedAt: sql`now()` })
+    .where(
+      and(
+        eq(pastas.publisherTwitchId, publisherTwitchId),
+        eq(pastas.id, pastaId),
+      ),
+    )
+    .returning();
+}
 
-export function getPastasWithNoTags(publisherTwitchId: string) {
-  return db
-    .select()
-    .from(pastas)
-    .where(eq(pastas.publisherTwitchId, publisherTwitchId));
+function addPastaTag(pastaId: Pasta["id"], value: string) {
+  return db.transaction(async (tx) => {
+    const tags = await tx.query.pastasTags.findMany({
+      where: (pastasTags, { eq }) => eq(pastasTags.pastaId, pastaId),
+      columns: { value: true },
+    });
+    assert.ok(tags.length < MAX_TAGS_IN_PASTA);
+    await tx
+      .insert(pastasTags)
+      .values({ pastaId, value })
+      .onConflictDoNothing();
+  });
+}
+
+export function addPastaTags(pastaId: Pasta["id"], values: string[]) {
+  assert.ok(values.length && values.length <= MAX_TAGS_IN_PASTA);
+  if (values.length === 1) {
+    return addPastaTag(pastaId, values[0]);
+  }
+  return db.transaction(async (tx) => {
+    const tags = await tx.query.pastasTags.findMany({
+      where: (pastasTags, { eq }) => eq(pastasTags.pastaId, pastaId),
+      columns: { value: true },
+    });
+    assert.ok(tags.length < MAX_TAGS_IN_PASTA);
+    const existingTags = new Set(tags.map((tag) => tag.value));
+    const tagsToAdd = [
+      ...setDifferenceOtTwoSets(new Set(values), existingTags),
+    ].slice(0, MAX_TAGS_IN_PASTA - existingTags.size);
+    await tx
+      .insert(pastasTags)
+      .values(tagsToAdd.map((value) => ({ pastaId, value })))
+      .onConflictDoNothing();
+  });
+}
+
+export async function removePastaTag(pastaId: Pasta["id"], value: string) {
+  await db
+    .delete(pastasTags)
+    .where(and(eq(pastasTags.pastaId, pastaId), eq(pastasTags.value, value)));
+}
+
+export async function removeAllPastaTags(pastaId: Pasta["id"]) {
+  await db.delete(pastasTags).where(eq(pastasTags.pastaId, pastaId));
 }
