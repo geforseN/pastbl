7dd1e9631a5e90b4ab03c939a83352cac64b3ab7
geforseN <geforsenjs@gmail.com>
2024-03-28 21:26:45 +0700

    change auth via twitch

--- server/api/auth/twitch.get.ts
+++ server/api/auth/twitch.get.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";
-import { upsertTwitchUser } from "~/db";
 
-const twitchUserSchema = z
+const userTwitchSchema = z
   .object({
     id: z.string(),
     login: z.string(),
     display_name: z.string(),
     profile_image_url: z.string(),
-    description: z.string().optional(),
   })
   .transform((twitchUser) => ({
     twitch: {
@@ -15,17 +13,15 @@ const twitchUserSchema = z
       login: twitchUser.login,
       profileImageUrl: twitchUser.profile_image_url,
       nickname: twitchUser.display_name,
-      description: twitchUser.description,
     },
   }));
 
+export type UserTwitch = z.infer<typeof userTwitchSchema>;
+
 export default oauth.twitchEventHandler({
   async onSuccess(event, { user: twitchUser, tokens: _tokens }) {
-    const user = twitchUserSchema.parse(twitchUser);
-    await upsertTwitchUser(user.twitch);
-    await setUserSession(event, {
-      user,
-    });
+    const user = userTwitchSchema.parse(twitchUser);
+    await setUserSession(event, { user });
     return sendRedirect(event, "/");
   },
   onError(event, error) {
