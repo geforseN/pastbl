86fcf1734d3fe9612102b41b2a64951580d2e547
geforseN <semen.neb@gmail.com>
2024-03-29 17:09:25 +0700

    db schema: change
    add not null for pastas.publicity
    add *Columns vars

--- db/schema.ts
+++ db/schema.ts
@@ -20,20 +20,24 @@ const TWITCH_USER_ID_LENGTH = 64;
 
 export const pastasPublicityEnum = pgEnum("pasta_publicity", pastasPublicity);
 
+const pastasColumns = {
+  id: serial("id").primaryKey(),
+  text: varchar("text", { length: PASTA_TEXT_LENGTH }).notNull(),
+  publishedAt: timestamp("published_at", { mode: "string" })
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp("updated_at", { mode: "string" }),
+  publisherTwitchId: varchar("publisher_twitch_id", {
+    length: TWITCH_USER_ID_LENGTH,
+  }).notNull(),
+  publicity: pastasPublicityEnum("publicity")
+    .notNull()
+    .default(defaultPastaPublicity),
+} as const;
+
 export const pastas = pgTable(
   "pastas",
-  {
-    id: serial("id").primaryKey(),
-    text: varchar("text", { length: PASTA_TEXT_LENGTH }).notNull(),
-    publishedAt: timestamp("published_at", { mode: "string" })
-      .notNull()
-      .defaultNow(),
-    updatedAt: timestamp("updated_at", { mode: "string" }),
-    publisherTwitchId: varchar("publisher_twitch_id", {
-      length: TWITCH_USER_ID_LENGTH,
-    }).notNull(),
-    publicity: pastasPublicityEnum("publicity").default(defaultPastaPublicity),
-  },
+  pastasColumns,
   ({ publisherTwitchId, publishedAt }) => ({
     publisher: index("publisher").on(publisherTwitchId),
     publisherNewest: index("publisher_newest")
@@ -48,16 +52,18 @@ export const pastasRelations = relations(pastas, ({ many }) => ({
 
 export type Pasta = typeof pastas.$inferSelect;
 
+const pastasTagsColumns = {
+  pastaId: integer("pasta_id")
+    .notNull()
+    .references(() => pastas.id, {
+      onDelete: "cascade",
+    }),
+  value: varchar("tag", { length: TAG_MAX_LENGTH }).notNull(),
+} as const;
+
 export const pastasTags = pgTable(
   "pastas_tags",
-  {
-    pastaId: integer("pasta_id")
-      .notNull()
-      .references(() => pastas.id, {
-        onDelete: "cascade",
-      }),
-    value: varchar("tag", { length: TAG_MAX_LENGTH }).notNull(),
-  },
+  pastasTagsColumns,
   ({ pastaId, value }) => ({
     pastas: index("pastas").on(pastaId),
     values: index("values").on(value),
