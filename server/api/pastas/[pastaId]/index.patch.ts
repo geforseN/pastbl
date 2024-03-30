8c1fc4b09fcf62d9e202fa6228c18d6bdb3c8412
geforseN <geforsenjs@gmail.com>
2024-03-30 21:39:43 +0700

    server/api/pastas: use getPastaIdParam

--- server/api/pastas/[pastaId]/index.patch.ts
+++ server/api/pastas/[pastaId]/index.patch.ts
@@ -2,7 +2,7 @@ import { patchPasta } from "~/db/pastas";
 
 export default defineEventHandler(async (event) => {
   const userTwitchId = await requireUserTwitchIdFromSession(event);
-  const pastaId = pastaIdParamSchema.parse(getRouterParam(event, "pastaId"));
+  const pastaId = getPastaIdParam(event);
   const patches = await getPastaPatchesFromBody(event);
   const pasta = await patchPasta(pastaId, userTwitchId, patches);
   return {
--- server/api/pastas/[pastaId]/text.patch.ts
+++ server/api/pastas/[pastaId]/text.patch.ts
@@ -2,7 +2,7 @@ import { patchPasta } from "~/db/pastas";
 
 export default defineEventHandler(async (event) => {
   const userTwitchId = await requireUserTwitchIdFromSession(event);
-  const pastaId = pastaIdParamSchema.parse(getRouterParam(event, "pastaId"));
+  const pastaId = getPastaIdParam(event);
   const text = await getPastaTextFromBody(event);
   const pasta = await patchPasta(pastaId, userTwitchId, { text });
   return {
