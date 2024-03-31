12ed1d7668058ea562c3ee684092a34ddacbe29e
geforseN <geforsenjs@gmail.com>
2024-03-31 17:05:01 +0700

    add TWITCH_LOGINS_MAX_QUERY_STRING_COUNT to env

--- server/utils/env.ts
+++ server/utils/env.ts
@@ -5,12 +5,16 @@ export const env = z
     TWITCH_APP_CLIENT_SECRET: z.string(),
     TWITCH_APP_CLIENT_ID: z.string(),
     DATABASE_URL: z.string(),
+    TWITCH_LOGINS_MAX_QUERY_STRING_COUNT: z
+      .optional(z.coerce.number().int().positive())
+      .default(10),
   })
   .transform((env) => {
     return {
       twitchClientSecret: env.TWITCH_APP_CLIENT_SECRET,
       twitchClientId: env.TWITCH_APP_CLIENT_ID,
       databaseUrl: env.DATABASE_URL,
+      twitchLoginsMaxQueryStringCount: env.TWITCH_LOGINS_MAX_QUERY_STRING_COUNT,
     };
   })
   .parse(process.env);
