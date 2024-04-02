fc99e2d7a4eb50458907a4cf32b9d458aca4d1a4
geforseN <geforsenjs@gmail.com>
2024-04-02 19:52:06 +0700

    server/utils/pastas-tags: change schemas

--- server/utils/pastas-tags.ts
+++ server/utils/pastas-tags.ts
@@ -2,42 +2,23 @@ import { z } from "zod";
 import { megaTrim } from "~/utils/string";
 import { pastaTagLength, pastaTagsCount } from "~/config/const";
 
-const lowercaseMentionTag = (tag: string) =>
-  tag.startsWith("@") ? tag.toLowerCase() : tag;
-
 const pastaTagSchema = z
   .string()
   .min(pastaTagLength.min)
   .max(pastaTagLength.max)
-  .refine((tag) => !tag.includes(","))
-  .transform(megaTrim)
-  .transform(lowercaseMentionTag);
+  .refine((tag) => !tag.includes(","));
 
 export const pastaTagsSchema = z
-  .string()
-  .min(pastaTagLength.min)
-  .max(pastaTagLength.max * pastaTagsCount.max + pastaTagsCount.max - 1)
-  .or(z.array(pastaTagSchema).min(pastaTagsCount.min).max(pastaTagsCount.max))
-  .transform((value) => {
-    if (typeof value === "string") {
-      value = value.split(",");
-    } else {
-      value = value.filter((tag) => !tag.includes(","));
-    }
-    return value
+  .array(pastaTagSchema)
+  .min(pastaTagsCount.min)
+  .max(pastaTagsCount.max)
+  .transform((tags) => {
+    return tags
+      .filter((tag) => pastaTagSchema.safeParse(tag).success)
       .map(megaTrim)
-      .filter((tag) => tag.length)
-      .map(lowercaseMentionTag);
-  })
-  .refine(
-    (tags) =>
-      tags.length >= pastaTagsCount.min &&
-      tags.length <= pastaTagsCount.max &&
-      tags.every(
-        (tag) =>
-          tag.length >= pastaTagLength.min && tag.length <= pastaTagLength.max,
-      ),
-  );
+      .filter((tag) => tag.length >= pastaTagLength.min)
+      .map((tag) => (tag.startsWith("@") ? tag.toLowerCase() : tag));
+  });
 
 export async function getPastaTagFromBody(event: H3E) {
   const body = await readBody(event);
