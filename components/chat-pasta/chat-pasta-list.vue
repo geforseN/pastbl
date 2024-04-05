9c953b4ee954a0b378afd07d516d0bdd076104bc
geforseN <geforsenjs@gmail.com>
2024-04-05 14:49:11 +0700

    chat-pasta-list: change slot syntax

--- components/chat-pasta/chat-pasta-list.vue
+++ components/chat-pasta/chat-pasta-list.vue
@@ -1,40 +1,39 @@
 <template>
-  <dynamic-scroller
-    v-slot="{ item: pasta, index, active }"
-    :min-item-size="100"
-  >
-    <dynamic-scroller-item
-      :item="pasta"
-      :active="active"
-      :size-dependencies="[pasta.text]"
-      :data-index="index"
-    >
-      <chat-pasta
-        :key="`${pasta.id}:${pasta.text}`"
-        :pasta="pasta"
-        @populate="
-          (pastaTextContainer) => {
-            populatePasta(pastaTextContainer, pasta, emotesStore);
-          }
-        "
+  <dynamic-scroller :min-item-size="100">
+    <template #default="{ item: pasta, index, active }">
+      <dynamic-scroller-item
+        :item="pasta"
+        :active="active"
+        :size-dependencies="[pasta.text]"
+        :data-index="index"
       >
-        <template #creatorData>
-          <chat-pasta-creator-data
-            :badges-count="userStore.user.badges.count.state"
-            :nickname="userStore.user.nickname.text.state"
-            :nickname-color="userStore.user.debounced.nickname.color"
-          />
-        </template>
-        <template #sidebar>
-          <chat-pasta-sidebar
-            dropdown-class="dropdown dropdown-top xs:dropdown-end dropdown-hover"
-            :pasta-edit-page-path="`/pastas/edit/${pasta.id}`"
-            @copy="userStore.copyPasta(pasta)"
-            @delete="emit('removePasta', pasta)"
-          />
-        </template>
-      </chat-pasta>
-    </dynamic-scroller-item>
+        <chat-pasta
+          :key="`${pasta.id}:${pasta.text}`"
+          :pasta="pasta"
+          @populate="
+            (pastaTextContainer) => {
+              populatePasta(pastaTextContainer, pasta, emotesStore);
+            }
+          "
+        >
+          <template #creatorData>
+            <chat-pasta-creator-data
+              :badges-count="userStore.user.badges.count.state"
+              :nickname="userStore.user.nickname.text.state"
+              :nickname-color="userStore.user.debounced.nickname.color"
+            />
+          </template>
+          <template #sidebar>
+            <chat-pasta-sidebar
+              dropdown-class="dropdown dropdown-top xs:dropdown-end dropdown-hover"
+              :pasta-edit-page-path="`/pastas/edit/${pasta.id}`"
+              @copy="userStore.copyPasta(pasta)"
+              @delete="emit('removePasta', pasta)"
+            />
+          </template>
+        </chat-pasta>
+      </dynamic-scroller-item>
+    </template>
   </dynamic-scroller>
 </template>
 <script lang="ts">
