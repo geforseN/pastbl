a27294abb70555bd15b0d74cc010fc6a75897e92
geforseN <geforsenjs@gmail.com>
2024-04-09 15:06:41 +0700

    integrations/FrankerFaceZEmote: fix type and change syntax

--- integrations/FrankerFaceZ/entity/FrankerFaceZEmote.ts
+++ integrations/FrankerFaceZ/entity/FrankerFaceZEmote.ts
@@ -4,7 +4,7 @@ import type { IEmote } from "~/integrations";
 export interface FrankerFaceZEmote extends IEmote {
   source: "FrankerFaceZ";
   type: "channel" | "global";
-  url: `//cdn.frankerfacez.com/emote/${IEmote["id"]}`;
+  url: `//cdn.frankerfacez.com/emote/${IEmote["id"]}/1`;
   width?: number;
   height?: number;
 }
@@ -25,7 +25,7 @@ export class FFZEmote implements FrankerFaceZEmote {
   height;
 
   constructor(apiEmote: FrankerFaceZApiEmote, type: FrankerFaceZEmote["type"]) {
-    this.id = `${apiEmote.id}`;
+    this.id = apiEmote.id.toString();
     this.isListed = apiEmote.status === 1;
     this.isModifier = "modifier" in apiEmote && apiEmote.modifier;
     this.isWrapper = apiEmote.modifier_flags % 2 === 0;
