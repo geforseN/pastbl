eca720adae0aa51d895736eaa3b8fced40d4bd39
geforseN <semen.neb@gmail.com>
2024-04-10 22:11:50 +0700

    pages/update: update
    add nav
    change classes
    use combine-keyboard-keys

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="flex flex-col gap-2">
+  <div class="space-y-2">
     <client-only>
       <keep-alive>
         <pasta-form-collapse />
       </keep-alive>
     </client-only>
-    <app-page-link-pastas-find />
-    <app-page-link-user-settings />
-    <app-page-link-emotes />
-    <app-page-link to="pastas">
-      <template #right>
-        <span class="relative">
-          <div class="flex items-center gap-2">
-            <span class="text-nowrap text-xs">
-              <kbd class="kbd pt-1">Alt</kbd>
-              <span class="mx-0.5">+</span>
-              <kbd class="kbd pt-1">P</kbd>
-            </span>
-            <icon name="mdi:pasta" size="32" class="-translate-y-0.5" />
-          </div>
-        </span>
-      </template>
-    </app-page-link>
+    <nav class="space-y-2">
+      <app-page-link-pastas-find />
+      <app-page-link-user-settings />
+      <app-page-link-emotes />
+      <app-page-link to="pastas">
+        <template #right>
+          <span class="relative">
+            <div class="flex items-center gap-2">
+              <combine-keyboard-keys first="Alt" last="P" />
+              <icon name="mdi:pasta" size="32" class="-translate-y-0.5" />
+            </div>
+          </span>
+        </template>
+      </app-page-link>
+    </nav>
   </div>
 </template>
 <style>
