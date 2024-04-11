15d7283b4c4c0e2d19c6db6b9f86dc0515d700de
geforseN <semen.neb@gmail.com>
2024-04-11 22:47:00 +0700

    chat-pasta-preview: change template

--- components/chat-pasta/chat-pasta-preview.vue
+++ components/chat-pasta/chat-pasta-preview.vue
@@ -1,33 +1,31 @@
 <template>
-  <article class="mt-2 rounded-box border p-4">
-    <h3 class="text-xl font-bold">
+  <article class="flex flex-col rounded-box border p-3">
+    <h3 class="mb-1 ml-1 text-2xl font-bold">
       {{ $t("pasta.create.preview.heading") }}
     </h3>
-    <div class="flex w-[340px] flex-col">
-      <span class="block grow border border-secondary">
-        <span class="block w-full px-[10px] py-[5px]">
-          <chat-pasta-creator-data
-            :badges-count="userStore.user.badges.count.state"
-            :nickname="userStore.user.nickname.text.state"
-            :nickname-color="userStore.user.nickname.color.state"
-          />
-          <span aria-hidden="true">:&nbsp;</span>
-          <span
-            ref="pastaTextContainerRef"
-            class="twitch-text p-0 text-[13px]/[18px]"
-            @mouseover="throttledMouseover"
-          >
-            {{ props.text }}
-          </span>
+    <span class="w-[340px] grow border border-secondary">
+      <span class="block w-full px-[10px] py-[5px]">
+        <chat-pasta-creator-data
+          :badges-count="userStore.user.badges.count.state"
+          :nickname="userStore.user.nickname.text.state"
+          :nickname-color="userStore.user.nickname.color.state"
+        />
+        <span aria-hidden="true">{{ ": " }}</span>
+        <span
+          ref="pastaTextContainerRef"
+          class="twitch-text p-0 text-[13px]/[18px]"
+          @mouseover="throttledMouseover"
+        >
+          {{ props.text }}
         </span>
       </span>
-      <button
-        class="btn btn-accent btn-md text-lg"
-        @click="() => userStore.copyText(props.text)"
-      >
-        {{ $t("pasta.create.preview.copyButton") }}
-      </button>
-    </div>
+    </span>
+    <button
+      class="btn btn-accent btn-md -ml-px w-[342px] rounded-t-none text-lg"
+      @click="() => userStore.copyText(props.text)"
+    >
+      {{ $t("pasta.create.preview.copyButton") }}
+    </button>
   </article>
 </template>
 <script lang="ts" setup>
