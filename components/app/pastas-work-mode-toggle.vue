a8b094d3f3ac699a64435825d4bb4b19290cc7eb
geforseN <geforsenjs@gmail.com>
2024-04-11 23:20:48 +0700

    pastas-work-mode-toggle: change template

--- components/app/pastas-work-mode-toggle.vue
+++ components/app/pastas-work-mode-toggle.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="fixed bottom-0 right-1/2 translate-x-1/2 rounded-t-btn border-2 border-b-0 bg-base-300 px-2 pb-1.5 pt-1.5 text-center"
+    class="fixed bottom-0 right-1/2 flex translate-x-1/2 flex-col rounded-t-btn border-2 border-b-0 bg-base-300 px-2 pb-1.5 pt-1.5 text-center"
     :class="
       userStore.pastasWorkMode.isClient
         ? 'border-secondary'
         : 'border-twitch-accent'
     "
   >
-    <div class="flex items-center gap-1">
+    <div class="flex items-center gap-1 self-center">
       Server mode
       <input
         id="pastas-work-mode"
@@ -19,24 +19,31 @@
       />
       Client mode
     </div>
-    <template v-if="!userStore.pastasWorkMode.canHaveServerMode">
-      <div
-        v-if="
-          userStore.pastasWorkMode.canHaveServerModeStatus.includes(
-            'not-logged-in',
-          )
-        "
-      >
-        GO LOGIN
-      </div>
-      <div
-        v-if="
-          userStore.pastasWorkMode.canHaveServerModeStatus.includes('offline')
-        "
-      >
-        GO ONLINE
-      </div>
-    </template>
+    <div
+      v-if="!userStore.pastasWorkMode.canHaveServerMode"
+      class="flex flex-col"
+    >
+      <ul>
+        <li
+          v-if="
+            userStore.pastasWorkMode.canHaveServerModeStatus.includes(
+              'not-logged-in',
+            )
+          "
+        >
+          <samp>
+            <auth-twitch-login-btnlink class="btn-xs w-full text-sm" />
+          </samp>
+        </li>
+        <li
+          v-if="
+            userStore.pastasWorkMode.canHaveServerModeStatus.includes('offline')
+          "
+        >
+          <samp>Restore Internet Connection</samp>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
