8e853fc092cb897da38b40c000f18c97f5b84afc
geforseN <semen.neb@gmail.com>
2024-04-14 17:28:35 +0700

    chat-pasta-list-[show|sort]-select: refactor

--- components/chat-pasta/chat-pasta-list-show-select.vue
+++ components/chat-pasta/chat-pasta-list-show-select.vue
@@ -3,31 +3,26 @@
     <label for="show-pastas" class="label font-bold">
       {{ t(s + "label") }}
     </label>
-    <client-only>
-      <select
-        id="show-pastas"
-        v-model="selectedShowStrategy"
-        name="show-pastas"
-        class="select select-secondary select-sm w-1/2"
+    <select
+      id="show-pastas"
+      v-model="selectedShowStrategy"
+      name="show-pastas"
+      class="select select-secondary select-sm w-1/2"
+    >
+      <option
+        v-for="[showStrategy, translated] of showEntries"
+        :key="showStrategy"
+        :value="showStrategy"
+        :disabled="!selectedLogin && translated.mustHaveUserCollection"
+        :title="
+          !selectedLogin && translated.mustHaveUserCollection
+            ? $t(s + 'requestForSelect')
+            : undefined
+        "
       >
-        <option
-          v-for="[showStrategy, translated] of Object.entries(showOptions)"
-          :key="showStrategy"
-          :value="showStrategy"
-          :disabled="!selectedLogin && translated.mustHaveUserCollection"
-          :title="
-            !selectedLogin && translated.mustHaveUserCollection
-              ? $t(s + 'requestForSelect')
-              : undefined
-          "
-        >
-          {{ translated.text }}
-        </option>
-      </select>
-      <template #fallback>
-        <select class="select select-secondary select-sm w-1/2"></select>
-      </template>
-    </client-only>
+        {{ translated.text }}
+      </option>
+    </select>
   </div>
 </template>
 <script lang="ts" setup>
@@ -48,44 +43,65 @@ const tOptions = reactive({
   login: computed(() => selectedLogin.value || "username"),
 });
 
-const showOptions = computedWithControl(
+const showEntries = computedWithControl(
   [locale, () => tOptions.login],
   () =>
-    ({
-      all: {
-        text: t(so + "all"),
-        mustHaveUserCollection: false,
-      },
-      "selected-user": {
-        text: t(so + "selected-user", tOptions),
-        mustHaveUserCollection: true,
-      },
-      "only-selected-user": {
-        text: t(so + "only-selected-user", tOptions),
-        mustHaveUserCollection: true,
-      },
-      "except-selected-user": {
-        text: t(so + "except-selected-user", tOptions),
-        mustHaveUserCollection: true,
-      },
-      "all-selectable-users": {
-        text: t(so + "all-selectable-users"),
-        mustHaveUserCollection: false,
-      },
-      "all-without-selectable-users": {
-        text: t(so + "all-without-selectable-users"),
-        mustHaveUserCollection: false,
-      },
-      none: {
-        text: t(so + "none"),
-        mustHaveUserCollection: false,
-      },
-    }) satisfies Record<
+    [
+      [
+        "all",
+        {
+          text: t(so + "all"),
+          mustHaveUserCollection: false,
+        },
+      ],
+      [
+        "selected-user",
+        {
+          text: t(so + "selected-user", tOptions),
+          mustHaveUserCollection: true,
+        },
+      ],
+      [
+        "only-selected-user",
+        {
+          text: t(so + "only-selected-user", tOptions),
+          mustHaveUserCollection: true,
+        },
+      ],
+      [
+        "except-selected-user",
+        {
+          text: t(so + "except-selected-user", tOptions),
+          mustHaveUserCollection: true,
+        },
+      ],
+      [
+        "all-selectable-users",
+        {
+          text: t(so + "all-selectable-users"),
+          mustHaveUserCollection: false,
+        },
+      ],
+      [
+        "all-without-selectable-users",
+        {
+          text: t(so + "all-without-selectable-users"),
+          mustHaveUserCollection: false,
+        },
+      ],
+      [
+        "none",
+        {
+          text: t(so + "none"),
+          mustHaveUserCollection: false,
+        },
+      ],
+    ] as const satisfies [
       PastaShowStrategy,
       {
         text: string;
         mustHaveUserCollection: boolean;
-      }
-    >,
+      },
+    ][],
 );
 </script>
