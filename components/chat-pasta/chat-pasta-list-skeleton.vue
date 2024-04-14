237f5a7df052ae2b1705a1bbb7b74b9f5852a6c6
geforseN <geforsenjs@gmail.com>
2024-04-14 17:43:12 +0700

    chat-pasta-list-skeleton: init

---
+++ components/chat-pasta/chat-pasta-list-skeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="skeleton flex h-[50dvh] w-[340px] justify-center rounded-btn rounded-t-none p-2 go-brr:h-[66dvh]"
+  >
+    {{ $t(l + "loading") }}
+  </div>
+</template>
+<script lang="ts" setup>
+import { l } from "~/components/chat-pasta/chat-pasta-list.vue";
+</script>
