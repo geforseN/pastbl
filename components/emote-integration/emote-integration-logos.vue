25da77365f819d6540a8b30165a0e6cd456a86e7
geforseN <geforsenjs@gmail.com>
2024-04-14 17:30:20 +0700

    emote-integration-logos: change template

--- components/emote-integration/emote-integration-logos.vue
+++ components/emote-integration/emote-integration-logos.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="flex items-center gap-1">
-    <span class="mr-4 flex flex-col gap-1">
-      <emote-integration-logo
-        v-for="(source, index) of emoteSources"
-        v-once
-        :key="source"
-        :class="[
-          index % 2 && `absolute translate-x-5`,
-          index === 3 && ' translate-y-[21px]',
-        ]"
-        :source="source"
-        width="16"
-        height="16"
-        class="max-h-[16px] text-xs"
-      />
-    </span>
-  </div>
+  <span class="flex w-8 flex-col gap-1">
+    <emote-integration-logo
+      v-for="(source, index) of emoteSources"
+      v-once
+      :key="source"
+      :class="[
+        index % 2 && `absolute translate-x-5`,
+        index === 3 && ' translate-y-[21px]',
+      ]"
+      :source="source"
+      width="16"
+      height="16"
+      class="max-h-[16px] text-xs"
+    />
+  </span>
 </template>
 <script setup lang="ts">
 import { emoteSources } from "~/integrations";
