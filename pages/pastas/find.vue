626cee8b1945b68c6d0318f403cb00dccf446516
geforseN <semen.neb@gmail.com>
2024-04-14 19:18:44 +0700

    pages/pastas/find: fix components names

--- pages/pastas/find.vue
+++ pages/pastas/find.vue
@@ -3,9 +3,9 @@
     class="mt-2 flex flex-col-reverse items-center justify-center gap-x-12 gap-y-4 go-brr:flex-row go-brr:items-start"
   >
     <!-- NOTE: for below component w-full and max-w-[414px] is necessary for no layout shift -->
-    <find-my-pasta-list class="w-full max-w-[414px]" />
+    <chat-pastas-find-list class="w-full max-w-[414px]" />
     <div class="w-96 space-y-2">
-      <find-my-pasta-params />
+      <chat-pastas-find-params />
       <app-page-link-main />
     </div>
   </div>
