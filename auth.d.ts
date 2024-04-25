fdb867cbdf12fa2b9744d27bdd314963cb87c4f7
geforseN <semen.neb@gmail.com>
2024-04-25 14:00:58 +0700

    pastas-work-mode-toggle: add i18n

--- auth.d.ts
+++ auth.d.ts
@@ -1,4 +1,4 @@
-import { UserTwitch } from "~/server/utils/twitch/twitch-user";
+import type { UserTwitch } from "~/server/utils/twitch/twitch-user";
 
 declare module "#auth-utils" {
   interface User extends UserTwitch {}
