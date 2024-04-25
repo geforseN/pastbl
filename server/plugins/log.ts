8f9641c0d3d7756a2a767fcea0c06d1c4a793fd3
geforseN <semen.neb@gmail.com>
2024-04-25 12:57:10 +0700

    server/plugins/log: change

--- server/plugins/log.ts
+++ server/plugins/log.ts
@@ -1,27 +1,19 @@
 export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook("q", () => {
-    console.log(1111);
-  });
+  /* eslint-disable no-console */
   nitroApp.hooks.hook("error", (error, { event }) => {
-    // eslint-disable-next-line no-console
     console.error(`${event?.path} Application error:`, error);
   });
   nitroApp.hooks.hook("request", (event) => {
-    // eslint-disable-next-line no-console
     console.log("on request", event.path);
-    nitroApp.hooks.callHook("q");
-    nitroApp.hooks.callHook("q");
   });
   nitroApp.hooks.hook("beforeResponse", (event /*, { body } */) => {
-    // eslint-disable-next-line no-console
     console.log("on response", event.path /*, { body } */);
   });
   nitroApp.hooks.hook("afterResponse", (event /*, { body } = {} */) => {
-    // eslint-disable-next-line no-console
     console.log("on after response", event.path /*, { body } */);
   });
   nitroApp.hooks.hookOnce("close", () => {
-    // eslint-disable-next-line no-console
     console.log("on close");
   });
+  /* eslint-enable no-console */
 });
