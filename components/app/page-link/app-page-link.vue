2a010ed9038445ea2f2382c637cf03c236321cb8
geforseN <geforsenjs@gmail.com>
2024-04-25 13:04:23 +0700

    app-page-link: update routePageLinkRecord

--- components/app/page-link/app-page-link.vue
+++ components/app/page-link/app-page-link.vue
@@ -7,59 +7,59 @@
       <span class="flex items-center gap-2">
         <slot name="left"><icon name="carbon:link" /></slot>
         <span class="group-hover:underline">
-          <slot name="default">{{ text }}</slot>
+          <slot name="default">{{ $t(textKey) }}</slot>
         </span>
       </span>
       <slot name="right" />
     </span>
   </nuxt-link-locale>
 </template>
+<script lang="ts">
+const routePageLinkRecord = {
+  "global-emotes": {
+    path: "/collections/global",
+    textKey: "collections.global.link",
+  },
+  "users-emotes": {
+    path: "/collections/users",
+    textKey: "collections.users.link",
+  },
+  emotes: {
+    path: "/collections",
+    textKey: "collections.index.link",
+  },
+  main: {
+    path: "/",
+    textKey: "main.link",
+  },
+  "user-settings": {
+    path: "/user/settings#heading",
+    textKey: "user.settings.link",
+  },
+  pastas: {
+    path: "/pastas",
+    textKey: "pastas.link",
+  },
+  "find-pasta": {
+    path: "/pastas/find#heading",
+    textKey: "pasta.find.link",
+  },
+  emojis: {
+    path: "/collections/emojis",
+    textKey: "collections.emojis.link",
+  },
+} as const satisfies Record<string, { path: string; textKey: string }>;
+</script>
 <script setup lang="ts">
+const props = defineProps<{
+  to: keyof typeof routePageLinkRecord;
+}>();
+
 defineSlots<{
   left?: () => unknown;
   right?: () => unknown;
   default?: () => unknown;
 }>();
 
-const { t } = useI18n();
-
-const routePageLinkRecord = {
-  "global-emotes": {
-    path: "/collections/global",
-    text: t("collections.global.link"),
-  },
-  "users-emotes": {
-    path: "/collections/users",
-    text: t("collections.users.link"),
-  },
-  emotes: {
-    path: "/collections",
-    text: t("collections.index.link"),
-  },
-  main: {
-    path: "/",
-    text: t("main.link"),
-  },
-  "user-settings": {
-    path: "/user/settings#heading",
-    text: t("user.settings.link"),
-  },
-  pastas: {
-    path: "/pastas",
-    text: t("pastas.link"),
-  },
-  "find-pasta": {
-    path: "/pastas/find#heading",
-    text: t("pasta.find.link"),
-  },
-  emojis: {
-    path: "/collections/emojis",
-    text: t("collections.emojis.link"),
-  },
-} as const;
-const props = defineProps<{
-  to: keyof typeof routePageLinkRecord;
-}>();
-
-const { text, path } = routePageLinkRecord[props.to];
+const { textKey, path } = routePageLinkRecord[props.to];
 </script>
