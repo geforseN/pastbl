8e41712e6d85f684493cc142d67beb4315ad24fe
geforseN <geforsenjs@gmail.com>
2024-05-02 16:51:55 +0700

    emotesIDB: add getEmote

--- client-only/services/emotes.ts
+++ client-only/services/emotes.ts
@@ -5,12 +5,14 @@ export const emotesIDB = {
   async put(emotes: IEmote[]) {
     return (await idb.emotes).put(emotes);
   },
-
   async makeDeleteEmotesFn(source: EmoteSource) {
     return (await idb.emotes).deleteManyWithSource(source);
   },
   async makeEmoteGetter(source: EmoteSource) {
     return (await idb.emotes).getWithSource(source);
+  },
+  async getEmote(source: EmoteSource, emoteId: string) {
+    return (await idb.emotes).getWithSource(source)(emoteId);
   },
   get emotesTransaction() {
     return idb.emotes.then((idb) => idb.emotesTransaction);
