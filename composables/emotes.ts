ae4f84dd513abf01f8144a55d16d36a5b5349840
geforseN <semen.neb@gmail.com>
2024-05-02 16:56:57 +0700

    useEmoteIntegrationsLoad: make from useGlobalIntegrationsLoad

--- composables/emotes.ts
+++ composables/emotes.ts
@@ -2,7 +2,7 @@ export {
   useEmotes,
   useEmotesWithInitialReady,
 } from "~/composables/emotes/useEmotes";
-export { useGlobalIntegrationsLoad } from "~/composables/emotes/useGlobalIntegrationsLoad";
+export { useEmoteIntegrationsLoad } from "~/composables/emotes/useEmoteIntegrationsLoad";
 export { useSelectedUserCollection } from "~/composables/emotes/useSelectedUserCollection";
 export { useSelectedUserCollectionLogin } from "~/composables/emotes/useSelectedUserCollectionLogin";
 export { useUsersCollectionsLoad } from "~/composables/emotes/useUsersCollectionsLoad";
