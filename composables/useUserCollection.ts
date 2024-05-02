d4441e0479a24ad7427908e89412bccf6ca63f5c
geforseN <semen.neb@gmail.com>
2024-05-02 18:51:53 +0700

    useUserCollection: update (wip)

--- composables/useUserCollection.ts
+++ composables/useUserCollection.ts
@@ -1,116 +1,98 @@
 import { userCollectionsService } from "~/client-only/services";
-import { USERS_COLLECTIONS_API } from "~/client-only/services/userCollections";
-import {
-  isReadyUserIntegration,
-  type IUserEmoteCollection,
-  type IUserEmoteIntegration,
-  type IUserEmoteIntegrationRecord,
-} from "~/integrations";
+import { USERS_COLLECTIONS_API as API } from "~/client-only/services/userCollections";
+import { type IUserEmoteCollection } from "~/integrations";
 
+function useCollectionState(initFn) {}
+
+function __updateIntegration__(collection, integration) {
+  return {
+    ...collection,
+    integrations: {
+      ...collection.integrations,
+      [integration.source]: integration,
+    },
+  };
+}
+
+// TODO: add i18n
 export function useUserCollection(login: TwitchUserLogin) {
   const userCollectionsStore = useUserCollectionsStore();
 
   const collection = useMyAsyncState(
-    async (
-      strategy: "get" | "refresh" | "refresh-integration" = "get",
-      newIntegration?: IUserEmoteIntegration,
-    ) => {
-      if (process.server) {
-        return null;
-      }
-      // TODO: add i18n
-      // FIXME: refactor, move to service
-      // FIXME: refactor, move to service
-      // FIXME: refactor, move to service
-      switch (strategy) {
-        case "get": {
-          const collection_ = await userCollectionsService
-            .get(login)
-            .catch(async () => {
-              const collection = await USERS_COLLECTIONS_API.get(login);
-              await userCollectionsService.put(collection);
-              return collection;
-            });
-          assert.ok(collection_, `No collection found with login: ${login}`);
-          return collection_;
-        }
-        case "refresh": {
-          return userCollectionsStore.loadCollection(login);
-        }
-        case "refresh-integration": {
-          const collection_ = collection.state.value;
-          assert.ok(
-            collection_ && typeof newIntegration !== "undefined",
-            `Failed to refresh emote integration ${newIntegration ? `: ${newIntegration.source}` : ""}`,
-          );
-          const newCollection = {
-            ...collection_,
-            integrations: {
-              ...collection_.integrations,
-              [newIntegration.source]: newIntegration,
-            },
-          } as IUserEmoteCollection;
-          await userCollectionsService.put(newCollection);
-          return newCollection;
-        }
-        default: {
-          assert.fail(`Unknown strategy for user collection: ${strategy}`);
-        }
-      }
+    async (getCollection: () => MaybePromise<IUserEmoteCollection>) => {
+      return await getCollection();
     },
     null,
+    { immediate: false },
   );
+
+  if (process.client) {
+    collection.execute(0, async () => {
+      const collection = await userCollectionsService
+        .get(login)
+        .catch(async () => {
+          const collection = await API.get(login);
+          await userCollectionsService.put(collection);
+          return collection;
+        });
+      assert.ok(collection, `No collection found with login: ${login}`);
+      return collection;
+    });
+  }
 
   const emotesEntries = computed(() => {
     const integrations = collection.state.value?.integrations || {};
     return getEmotesMapFromIntegrations(integrations);
   });
 
-  const user = computed(() => collection.state.value?.user);
+  function __getCollection__() {
+    const collection_ = collection.state.value;
+    assert.ok(collection_, "TODO add i18n message");
+    return collection_;
+  }
 
   const integrationsLoad = useEmoteIntegrationsLoad<
-    IUserEmoteIntegration,
-    IUserEmoteIntegrationRecord
+    IUserEmoteCollection["integrations"][keyof IUserEmoteCollection["integrations"]],
+    IUserEmoteCollection["integrations"]
   >({
-    async load(source) {
-      const login = collection.state.value?.user.twitch.login;
-      assert.ok(login);
-      const newIntegration = await USERS_COLLECTIONS_API.integrations.get(
-        source,
-        login,
-      );
-      // NOTE: this assert is important
-      // TEST: try call load on failed integration
-      // assert.ok(newIntegration.status === "ready");
-      return newIntegration;
+    load(source) {
+      return API.integrations.get(source, login);
     },
     loadAll() {
-      const login = collection.state.value?.user.twitch.login;
-      assert.ok(login);
-      return USERS_COLLECTIONS_API.integrations.getAll(login);
+      return API.integrations.getAll(login);
     },
     loadMany(sources) {
-      const login = collection.state.value?.user.twitch.login;
-      assert.ok(login);
-      return USERS_COLLECTIONS_API.integrations.getMany(sources, login);
+      return API.integrations.getMany(sources, login);
     },
   });
 
   return {
     ...collection,
-    user,
+    user: computed(() => collection.state.value?.user),
     integrations: computed(() => collection.state.value?.integrations || {}),
     formedAt: computed(() => collection.state.value?.formedAt),
     isSelected: computed(() =>
       userCollectionsStore.isCollectionSelected(login),
     ),
-    async refreshIntegration(integration) {
+    refreshIntegration(integration) {
       assert.ok(collection.state.value);
-      const newIntegration = await integrationsLoad.execute(integration.source);
-      return collection.execute(0, "refresh-integration", newIntegration);
+      return collection.execute(0, async () => {
+        const newIntegration = await integrationsLoad.execute(
+          integration.source,
+        );
+        const collection = __getCollection__();
+        const newCollection = __updateIntegration__(
+          collection,
+          newIntegration,
+        ) as IUserEmoteCollection;
+        await userCollectionsService.put(newCollection);
+        return newCollection;
+      });
     },
     refresh() {
-      return collection.execute(0, "refresh");
+      return collection.execute(0, () => {
+        return userCollectionsStore.loadCollection(login);
+      });
     },
     select() {
       return userCollectionsStore.selectCollection(login);
@@ -128,8 +110,7 @@ export function useUserCollection(login: TwitchUserLogin) {
       if (!collection.state.value) {
         return [];
       }
-      const values = Object.values(collection.state.value.integrations);
-      return values.filter(isReadyUserIntegration);
+      return getReadyUserIntegrations(collection.state.value);
     }),
   };
 }
