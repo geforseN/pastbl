9e1258c47c5e84c0ee3a5c31cdce91fc6ab1c9d0
geforseN <semen.neb@gmail.com>
2024-05-14 17:25:44 +0700

    auth-logged-in-dropdown: wrap in link

--- components/auth/logged-in-dropdown.vue
+++ components/auth/logged-in-dropdown.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="dropdown dropdown-end dropdown-hover">
-    <div class="btn btn-outline w-36 flex-nowrap justify-start gap-1.5 p-2">
+    <nuxt-link-locale
+      :to="`/collections/users/${user.twitch.login}`"
+      class="btn btn-outline w-36 flex-nowrap justify-start gap-1.5 p-2"
+    >
       <twitch-user-avatar :user="user.twitch" :size="24" />
       <div
         class="line-clamp-2 w-[5.2rem] items-center text-ellipsis break-all text-start text-sm/tight"
@@ -11,7 +14,7 @@
       <button class="absolute right-2">
         <icon name="ic:arrow-drop-down" size="20" />
       </button>
-    </div>
+    </nuxt-link-locale>
     <auth-logged-in-dropdown-content
       :login="user.twitch.login"
       @logout="emit('logout')"
