5b7b0742e962e68c6c59d261b36dd0f81187fb1d
geforseN <semen.neb@gmail.com>
2024-05-14 15:54:30 +0700

    app-page-list: update font styles for min-width media

--- components/app/page-link/app-page-link.vue
+++ components/app/page-link/app-page-link.vue
@@ -3,7 +3,9 @@
     :to="path"
     class="group block rounded-box border-2 px-4 py-2 hover:border-base-content hover:bg-base-300"
   >
-    <span class="flex items-center justify-between gap-2 text-2xl font-bold">
+    <span
+      class="flex items-center justify-between gap-2 text-lg font-bold xs:text-2xl"
+    >
       <span class="flex items-center gap-2">
         <slot name="left"><icon name="carbon:link" /></slot>
         <span class="group-hover:underline">
