7236ca0b5583d05924242709f8c0b3e3bf0f6550
geforseN <semen.neb@gmail.com>
2024-05-16 20:30:09 +0700

    add flat eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+  overrides: {
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "never",
+        },
+        svg: "always",
+        math: "always",
+      },
+    ],
+  },
+});
