86fd8dd74cf60c51f0b29a0cb1e1d38f4f2652ae
geforseN <geforsenjs@gmail.com>
2024-05-17 21:59:52 +0700

    settings-badges-count: change
    remove dead code
    remove log
    change fast return in set

--- components/settings/settings-badges-count.vue
+++ components/settings/settings-badges-count.vue
@@ -17,30 +17,32 @@
     </div>
     <button
       class="btn btn-square join-item border border-secondary pb-1 text-2xl font-medium focus-within:bg-secondary focus-within:outline-secondary hover:bg-secondary"
-      @click="model = model - 1"
+      @click="model--"
     >
       -
     </button>
     <button
       class="btn btn-square join-item border border-secondary text-2xl font-medium focus-within:bg-secondary focus-within:outline-secondary hover:bg-secondary"
-      @click="model = model + 1"
+      @click="model++"
     >
       +
     </button>
   </div>
 </template>
-<script lang="ts">
-import { badgesCount } from "~/config/const";
-</script>
+<!-- 
+ TODO: use ValidityState
+ LINK: https://developer.mozilla.org/en-US/docs/Web/API/ValidityState
+ -->
 <script lang="ts" setup>
+import { badgesCount } from "~/config/const";
+
 const model = defineModel<number>({
   required: true,
   set(value) {
-    if (typeof value === "string") {
+    if (typeof value !== "number" || Number.isNaN(value)) {
       return model.value;
     }
     const nextErrorMessage = getNextErrorMessage(value);
-    console.log({ value, nextErrorMessage });
     errorMessage.value = nextErrorMessage;
     if (nextErrorMessage) {
       return model.value;
@@ -50,37 +52,8 @@ const model = defineModel<number>({
 });
 
 const errorMessage = ref("");
+
 const { t } = useI18n();
-
-// TODO: use ValidityState
-// LINK: https://developer.mozilla.org/en-US/docs/Web/API/ValidityState
-function handleInputChange(event: Event) {
-  const value = parseEventTargetIntegerValue(event);
-  if (value > 10) {
-    return (errorMessage.value = "The badges count can not be more than 10");
-  }
-  if (value < 0) {
-    return (errorMessage.value = "The badges count can not be negative number");
-  }
-  if (errorMessage.value) {
-    errorMessage.value = "";
-  }
-  model.value = value;
-}
-
-function parseEventTargetIntegerValue(event: Event) {
-  assert.ok(isObject(event.target), new TypeError("No event target provided"));
-  assert.ok(
-    typeof event.target.value === "string",
-    new TypeError("Event target value must be a string"),
-  );
-  const value = Number(event.target.value);
-  assert.ok(
-    Number.isInteger(value),
-    new TypeError("Event target value is not a integer"),
-  );
-  return value;
-}
 
 function getNextErrorMessage(value: number) {
   if (value > badgesCount.max) {
@@ -90,24 +63,5 @@ function getNextErrorMessage(value: number) {
     return t("settings.badges-count.input.toSmall", badgesCount.min);
   }
   return "";
-}
-
-function tryIncrementBadgesCount() {
-  if (model.value === badgesCount.max) {
-    errorMessage.value = "The badges count can not be more than 10";
-    return;
-  }
-  model.value++;
-}
-
-function tryDecrementBadgesCount() {
-  if (model.value <= badgesCount.min) {
-    errorMessage.value = "The badges count can not be negative number";
-    return;
-  }
-  if (errorMessage) {
-    errorMessage.value = "";
-  }
-  model.value--;
 }
 </script>
