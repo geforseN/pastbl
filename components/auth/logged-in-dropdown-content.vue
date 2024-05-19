ee495a0dc556dbfec1cd82f53cb8599290438d55
geforseN <semen.neb@gmail.com>
2024-05-19 19:14:12 +0700

    add emote-collection-person-link

--- components/auth/logged-in-dropdown-content.vue
+++ components/auth/logged-in-dropdown-content.vue
@@ -4,13 +4,13 @@
   >
     <ul class="m-0 space-y-1 p-0">
       <li class="w-fit">
-        <nuxt-link-locale
+        <emote-collection-person-link
+          :login
           class="btn btn-outline btn-sm h-fit flex-nowrap justify-between text-wrap text-start hover:bg-primary"
-          :to="`/collections/persons/${login}`"
         >
           {{ $t("collection.my") }}
           <icon name="carbon:link" size="16" class="min-h-4 min-w-4" />
-        </nuxt-link-locale>
+        </emote-collection-person-link>
       </li>
       <li>
         <button
---
+++ components/emote-collection/person/emote-collection-person-link.vue
@@ -0,0 +1,10 @@
+<template>
+  <nuxt-link-locale :to="`/collections/persons/${login}`">
+    <slot />
+  </nuxt-link-locale>
+</template>
+<script setup lang="ts">
+defineProps<{
+  login: TwitchUserLogin;
+}>();
+</script>
