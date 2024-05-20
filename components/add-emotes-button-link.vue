e23befb72665fb06d92b389b900cea83c6e57fa8
geforseN <semen.neb@gmail.com>
2024-05-20 23:38:05 +0700

    chat-pasta-list-hints: add bottom hint

--- components/add-emotes-button-link.vue
+++ components/add-emotes-button-link.vue
@@ -1,15 +1,9 @@
 <template>
   <nuxt-link-locale
     to="/collections?focus=fetch#heading"
-    class="btn btn-accent btn-sm items-center justify-start gap-1.5 text-nowrap p-2"
+    class="btn btn-accent btn-sm items-center gap-1.5 bg-base-200 text-base-content"
   >
-    <icon name="carbon:link" size="16" />
-    <i18n-t keypath="emotes.$add" tag="span">
-      <template #emotes>
-        <span class="lowercase">
-          {{ $t("emotes.person") }}
-        </span>
-      </template>
-    </i18n-t>
+    {{ $t("emotes.add") }}
+    <icon name="carbon:link" size="16" class="min-h-4 min-w-4" />
   </nuxt-link-locale>
 </template>
