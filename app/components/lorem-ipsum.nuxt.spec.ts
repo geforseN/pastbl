4f1dbe85eb5ca2f9e80beca9af6935ad601d8a37
geforseN <semen.neb@gmail.com>
2024-06-27 22:11:36 +0700

    add tests support
    add simple test

---
+++ app/components/lorem-ipsum.nuxt.spec.ts
@@ -0,0 +1,18 @@
+import { mountSuspended } from "@nuxt/test-utils/runtime";
+import { LoremIpsum } from "#components";
+import { describe, expect, it } from "vitest";
+
+describe("LoremIpsum", () => {
+  it("has correct numbers inside text", async () => {
+    const component = await mountSuspended(LoremIpsum, {
+      props: {
+        count: 3,
+      },
+    });
+    const text = component.text();
+    expect(text).toContain(1);
+    expect(text).toContain(3);
+    expect(text).not.toContain(4);
+    expect(text).not.toContain(0);
+  });
+});
