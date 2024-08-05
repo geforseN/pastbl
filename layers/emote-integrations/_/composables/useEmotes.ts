ff798b89b7b46c364b83dd20442bd0c3465ac87f
geforseN <geforsenjs@gmail.com>
2024-08-06 02:14:03 +0700

    chore: use alias path for TEmoteIntegrations

--- layers/emote-integrations/_/composables/useEmotes.ts
+++ layers/emote-integrations/_/composables/useEmotes.ts
@@ -1,4 +1,4 @@
-import type { TEmoteIntegrations } from "../utils/__types__some_to_server_some_to_client";
+import type { TEmoteIntegrations } from "$/emote-integrations";
 
 type EmotesMapRecord = Record<EmoteSource, EmotesMap<IEmote>>;
 
