7ae7b15437b9c0d5209a1b3580f5a0a3ec93a918
geforseN <semen.neb@gmail.com>
2024-08-26 00:10:59 +0700

    i18n(pastas): add with layer for file-pastas

--- layers/pastas/file-pastas/nuxt.config.ts
+++ layers/pastas/file-pastas/nuxt.config.ts
@@ -1,1 +1,17 @@
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  i18n: {
+    langDir: "lang",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+        file: "en.json",
+      },
+      {
+        code: "ru",
+        name: "Русский",
+        file: "ru.json",
+      },
+    ],
+  },
+});
