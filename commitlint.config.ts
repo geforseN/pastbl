dfc33c56c1a04da5e33f2e24f313e8849408e702
geforseN <semen.neb@gmail.com>
2024-08-28 00:13:16 +0700

    chore(commitlint): add "type-enum" rule

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,24 @@
+import { RuleConfigSeverity, type UserConfig } from "@commitlint/types";
+
 export default {
   extends: ["@commitlint/config-conventional"],
-} satisfies import("@commitlint/types").UserConfig;
+  rules: {
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+} satisfies UserConfig;
