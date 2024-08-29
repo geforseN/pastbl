c388baccc27768512365eb7979c3ed6b94098f4d
geforseN <geforsenjs@gmail.com>
2024-08-29 23:49:22 +0700

    feat(utils): init my-time-string

---
+++ app/utils/my-time-string.spec.ts
@@ -0,0 +1,5 @@
+it.todo('throws on invalid format')
+it.todo('throws if parsed number is not integer')
+it.todo('throws if parsed format is not included in valid ones')
+
+test.todo('add equals test for some const values')
