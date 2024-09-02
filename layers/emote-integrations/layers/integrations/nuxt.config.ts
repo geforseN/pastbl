745b92c9d42a4e531787fd363e94924d453302fc
geforseN <geforsenjs@gmail.com>
2024-09-02 13:21:05 +0700

    feat(emote-integrations): add type namespaces auto-imports

--- layers/emote-integrations/layers/integrations/nuxt.config.ts
+++ layers/emote-integrations/layers/integrations/nuxt.config.ts
@@ -6,20 +6,36 @@ import url from "node:url";
 
 const currentDirectoryPath = path.dirname(url.fileURLToPath(import.meta.url));
 
+const allEmoteSources_ = [...allEmoteSources];
+const allLowercaseEmoteSources = allEmoteSources_.map(toLowerCase);
+
 export default defineNuxtConfig({
   alias: {
     ...flatGroupBy(
-      [...allEmoteSources].map(toLowerCase),
+      allLowercaseEmoteSources,
       (source) => `#t_${source}`,
       (source) =>
         path.join(currentDirectoryPath, `${source}/server/utils/types`),
     ),
     ...flatGroupBy(
-      [...allEmoteSources].map(toLowerCase),
+      allLowercaseEmoteSources,
       (source) => `#integrations_${source}`,
-      (source) =>
-        path.join(currentDirectoryPath, `${source}/server/utils`),
+      (source) => path.join(currentDirectoryPath, `${source}/server/utils`),
     ),
   },
-  extends: allEmoteSources.map(toLowerCase),
+  extends: allLowercaseEmoteSources,
+  imports: {
+    imports: [
+      ...allEmoteSources_.map((source) => ({
+        from: `#t_${toLowerCase(source)}`,
+        name: `T${source}`,
+        type: true,
+      })),
+      ...allEmoteSources_.map((source) => ({
+        from: `#integrations_${toLowerCase(source)}`,
+        name: `${source}Api`,
+        type: true,
+      })),
+    ],
+  },
 });
