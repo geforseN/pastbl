d1b3e67a2958a9060f3a33c684b91735cb3db989
geforseN <geforsenjs@gmail.com>
2024-09-03 14:17:03 +0700

    test: update

--- layers/emote-integrations/layers/persons-emotes-collections/layers/integrations/server/utils/-record.spec.ts
+++ layers/emote-integrations/layers/persons-emotes-collections/layers/integrations/server/utils/-record.spec.ts
@@ -1,24 +1,20 @@
 import { PersonEmoteIntegrations } from "./-record";
+import type { PersonTwitch } from "../../../../../../../twitch/twitch-user/server/utils/twitch-user";
+describe("PersonEmoteIntegrations class", () => {
+  suite("Symbol.asyncIterator", () => {
+    const geforsenTwitch = {
+      login: "geforsen",
+      id: "104788839",
+    } as unknown as PersonTwitch;
+    const personEmoteIntegration = new PersonEmoteIntegrations(geforsenTwitch);
+    it("has defined", () => {
+      expect(personEmoteIntegration[Symbol.asyncIterator]).toBeDefined();
+    });
 
-describe("Person Emote Integrations", () => {
-  test.todo("PersonEmoteIntegrations class", () => {
-    test("Symbol.asyncIterator", () => {
-      const geforsenTwitch = {
-        login: "geforsen",
-        id: "104788839",
-      } as unknown as PersonTwitch;
-      const personEmoteIntegration = new PersonEmoteIntegrations(
-        geforsenTwitch,
-      );
-      it("has defined", () => {
-        expect(personEmoteIntegration[Symbol.asyncIterator]).toBeDefined();
-      });
-
-      it("returns objects with 'status' property", async () => {
-        for await (const integration of personEmoteIntegration) {
-          expect(integration).toHaveProperty("status");
-        }
-      });
+    it("returns objects with 'status' property", async () => {
+      for await (const integration of personEmoteIntegration) {
+        expect(integration).toHaveProperty("status");
+      }
     });
   });
 });
--- server/api/v1/-test.get.spec.ts
+++ server/api/v1/-test.get.spec.ts
@@ -1,11 +1,12 @@
 import { setup, $fetch } from "@nuxt/test-utils/e2e";
 
-describe.todo("server api", async () => {
+describe("server api", async () => {
   await setup({
     server: true,
+    setupTimeout: 300 * 1000,
   });
   test("api is working", async () => {
     const response = await $fetch("/api/v1/test");
-    expect(response).toBe("Hello World2!");
+    expect(response).toBe("Hello World!");
   });
 });
