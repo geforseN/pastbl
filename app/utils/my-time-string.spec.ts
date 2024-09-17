78cdd7cb7a75a9744d41f26a2b02f746c77a357f
geforseN <geforsenjs@gmail.com>
2024-09-17 18:47:21 +0700

    test!: delete

--- app/utils/my-time-string.spec.ts
+++
@@ -1,5 +0,0 @@
-it.todo("throws on invalid format");
-it.todo("throws if parsed number is not integer");
-it.todo("throws if parsed format is not included in valid ones");
-
-test.todo("add equals test for some const values");
