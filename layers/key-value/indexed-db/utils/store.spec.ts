19114c9096da87105f765122342dd86bf58f852d
geforseN <semen.neb@gmail.com>
2024-09-17 18:57:35 +0700

    test!: remove all tests except two
    find . -type f -name '*.spec.*' ! -name 'emotes.nuxt.spec.ts' ! -name 'guard.nuxt.spec.ts' -exec rm {} +

--- layers/key-value/indexed-db/utils/store.spec.ts
+++
@@ -1,7 +0,0 @@
-describe.todo("key-value store get method", () => {
-  it.todo("throws TypeError if return value is undefined", () => {});
-  it.todo(
-    "does not throws any error if return value is not undefined",
-    () => {},
-  );
-});
--- server/api/v1/-test.get.nitro.spec.ts
+++
@@ -1,9 +0,0 @@
-import { $fetch, setup } from "nitro-test-utils/e2e";
-
-describe("server api", async () => {
-  await setup();
-  test("api is working", async () => {
-    const response = await $fetch("/api/v1/test");
-    expect(response.data).toBe("Hello World!");
-  });
-});
