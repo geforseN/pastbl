499e8c89acb1d61dd8bbb4c8efffd11e82dea908
geforseN <semen.neb@gmail.com>
2024-09-17 16:46:56 +0700

    chore(nuxt-configs): update utils

--- layers/pastas/nuxt.config.ts
+++ layers/pastas/nuxt.config.ts
@@ -1,15 +1,15 @@
-import path from "node:path";
-import url from "node:url";
 import { i18n } from "../../app/i18n.config";
-import { findNuxtLayers } from "../../server/utils/nuxt-config";
+import {
+  __layersDirname__,
+  findNuxtLayers,
+} from "../../server/utils/nuxt-config";
 
-const currentDirectoryPath = path.dirname(url.fileURLToPath(import.meta.url));
-const currentDirectoryLayersPath = path.join(currentDirectoryPath, "layers");
+const layersDirname = __layersDirname__(import.meta.url);
 
 export default defineNuxtConfig({
   imports: {
     dirs: ["utils/service"],
   },
   i18n,
-  extends: findNuxtLayers(currentDirectoryLayersPath),
+  extends: findNuxtLayers(layersDirname),
 });
