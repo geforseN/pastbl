7295fd3dfa265e48d039885bf8de411748fb5032
geforseN <geforsenjs@gmail.com>
2024-09-17 18:57:35 +0700

    test!: remove all tests except two
    find . -type f -name '*.spec.*' ! -name 'emotes.nuxt.spec.ts' ! -name 'guard.nuxt.spec.ts' -exec rm {} +

--- layers/pastas/layers/chat-pasta/components/chat-pasta/chat-pasta-tag-add-dialog.spec.ts
+++
@@ -1,7 +0,0 @@
-describe.todo("chat-pasta-tag-add-dialog", () => {
-  test.todo("call will correct data and modal is opened");
-  test.todo("call will incorrect data and modal is opened");
-  test.todo(
-    "call again with any data and modal is opened (must ensure that after incorrect data (and thrown error) modal will be opened again)",
-  );
-});
