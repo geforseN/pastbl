d2df5c8aef6ce85c6e8c4352a8cc4817e45ad61a
geforseN <semen.neb@gmail.com>
2024-09-18 11:30:10 +0700

    chore: upd

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,5 @@
 import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
-  test: {
-    include: [
-      "layers/emote-integrations/utils/emotes.nuxt.spec.ts",
-      "app/utils/guard.nuxt.spec.ts",
-    ],
-  },
+  test: {},
 });
