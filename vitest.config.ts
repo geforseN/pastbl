5fe3ff6ff7ced36a8d4c4fd884f977747134acd8
geforseN <semen.neb@gmail.com>
2024-09-18 12:07:16 +0700

    chore: update configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,4 +2,10 @@ import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
   test: {},
+  //  test: {
+  //   include: [
+  //     "{server,layers,app}/**/*.nuxt.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+  //   ],
+  //   environment: "nuxt",
+  // },
 });
--- vitest.nitro.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "nitro-test-utils/config";
-
-export default defineConfig({
-  test: {
-    include: ["server/api/v1/-test.e2e.spec.ts"],
-  }
-});
