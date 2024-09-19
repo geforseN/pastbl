5d5c1ee89f2751ea1b100a8935e5cbc5a5dd8077
geforseN <geforsenjs@gmail.com>
2024-09-19 17:08:43 +0700

    test: make work nuxt with nitro

--- vitest.nuxt.config.ts
+++ vitest.nuxt.config.ts
@@ -2,15 +2,23 @@ import { defineVitestConfig } from "@nuxt/test-utils/config";
 import { defaultExclude } from "vitest/config";
 import {
   endToEndTestsGlobs,
+  nitroTestsGlobs,
   nodejsTestsGlobs,
 } from "./test-common";
+import { fileURLToPath } from "node:url";
 
 export default defineVitestConfig({
   test: {
-    exclude: [
-      ...defaultExclude,
-      ...endToEndTestsGlobs,
-      ...nodejsTestsGlobs,
-    ],
+    include: ["**/*.spec.ts", ...nitroTestsGlobs],
+    exclude: [...defaultExclude, ...endToEndTestsGlobs, ...nodejsTestsGlobs],
+    environmentOptions: {
+      nuxt: {
+        rootDir: fileURLToPath(new URL(".", import.meta.url)),
+        mock: {
+          intersectionObserver: true,
+          indexedDb: true,
+        },
+      },
+    },
   },
 });
