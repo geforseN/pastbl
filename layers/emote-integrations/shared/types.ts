e0a18271b623459b6da5d514c635af54ca37f39f
geforseN <geforsenjs@gmail.com>
2024-09-26 19:06:00 +0700

    chore(emote-integrations): update types auto-imports

---
+++ layers/emote-integrations/shared/types.ts
@@ -0,0 +1,24 @@
+export * as Global from "./types.global";
+export * as Person from "./types.global";
+
+export type Base =
+  | TEmoteIntegrations.Global.Ready
+  | TEmoteIntegrations.Person.Ready;
+
+export type Ready =
+  | TEmoteIntegrations.Global.Ready
+  | TEmoteIntegrations.Person.Ready;
+
+export type Failed =
+  | TEmoteIntegrations.Global.Failed
+  | TEmoteIntegrations.Person.Failed;
+
+export type Settled =
+  | TEmoteIntegrations.Global.Settled
+  | TEmoteIntegrations.Person.Settled;
+
+export type SettledRecord =
+  | TEmoteIntegrations.Global.SettledRecord
+  | TEmoteIntegrations.Person.SettledRecord;
+
+export type __Some__ = SomeIntegration__MoveThis;
