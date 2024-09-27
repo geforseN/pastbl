460557be53f5a2b1c58da1af5b7048ee79ce35e4
geforseN <geforsenjs@gmail.com>
2024-09-27 20:35:27 +0700

    feat(components): [hovered-emote-images] add scrollHorizontalIfNeeded

--- layers/emote-on-hover/components/hovered-emote-images.vue
+++ layers/emote-on-hover/components/hovered-emote-images.vue
@@ -2,6 +2,7 @@
   <div
     ref="container"
     class="flex w-max max-w-xs items-center gap-1 overflow-x-auto p-1 scrollbar"
+    @wheel.passive="scrollHorizontalIfNeeded"
   >
     <template
       v-for="image of emote.images.value"
@@ -44,5 +45,15 @@ function onContainerUpdate(container: HTMLElement | null) {
     const left = container.scrollWidth / 2 - container.offsetWidth / 2;
     container.scrollBy({ left });
   }
+}
+
+function scrollHorizontalIfNeeded(event: WheelEvent) {
+  if (event.shiftKey || !containerRef.value) {
+    // images container can have horizontal scroll and should not have vertical scroll
+    // when scrolling with shift key (horizontal scroll change) then just use mouse wheel default behavior
+    return;
+  }
+  // overriding default behavior, scrolling horizontally when supposed to be vertical
+  containerRef.value.scrollBy({ left: event.deltaY / 6 });
 }
 </script>
