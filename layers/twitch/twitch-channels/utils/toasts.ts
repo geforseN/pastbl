f79b110ead93335919c460220891d7b15fe060e8
geforseN <geforsenjs@gmail.com>
2024-09-29 17:45:46 +0700

    refactor: update toasts

--- layers/twitch/twitch-channels/utils/toasts.ts
+++ layers/twitch/twitch-channels/utils/toasts.ts
@@ -1,25 +1,44 @@
+import { twitchConfig } from "$/twitch/app.config";
+
 export const personEmotesCollectionLoadToasts = createActionToasts(
   "load-person-emotes-collection",
   {
     success(this: ActionToastsThis, nickname: string, statuses: string) {
       return {
-        description: this.i18n.t("toast.loadCollection.success.message", {
+        description: this.i18n.t("persons-emotes-collection.loadCollection.success.description", {
           nickname,
           statuses,
         }),
-        title: this.i18n.t("toast.loadCollection.success.title"),
+        title: this.i18n.t("persons-emotes-collection.loadCollection.success.title"),
         timeout: 7000,
       };
     },
     failures: {
       emptyInput(this: ActionToastsThis) {
         return {
-          title: this.i18n.t("toast.loadCollection.fail.title"),
+          title: this.i18n.t("persons-emotes-collection.loadCollection.failureTitle"),
           description: this.i18n.t(
-            "toast.loadCollection.fail.emptyInputMessage",
+            "persons-emotes-collection.loadCollection.failures.emptyInput.description",
           ),
         };
       },
+      tooSmallInput(this: ActionToastsThis,providedLength: number) {
+        return {
+          title: this.i18n.t("persons-emotes-collection.loadCollection.failureTitle"),
+          description: this.i18n.t(
+            "persons-emotes-collection.loadCollection.failures.tooSmallInput.description",
+            { min: twitchConfig.twitchUser.login.length.max, provided:providedLength },
+          ),
+        };
+      },
+      tooBigInput(this: ActionToastsThis,providedLength: number) {
+        return {  
+          title: this.i18n.t("persons-emotes-collection.loadCollection.failureTitle"),
+          description: this.i18n.t(
+            "persons-emotes-collection.loadCollection.failures.tooBigInput.description",
+            { max: twitchConfig.twitchUser.login.length.max, provided:providedLength },
+          ),
+      }
     },
   },
 );
