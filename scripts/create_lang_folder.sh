e05efb4d31d98ebff1ed8e415190ef19479e9e3b
geforseN <geforsenjs@gmail.com>
2024-09-29 23:08:16 +0700

    chore(scripts): update

--- scripts/create_lang_folder.sh
+++ scripts/create_lang_folder.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # NOTE: execute firstly: chmod +x scripts/create_lang_folder.sh
 
 create_lang_folder() {
   local path=$1
-  local lang_folder="$path/lang"
 
   local content="{\n\t\n}"
   mkdir -p "$lang_folder"
   for lang in en ru; do
-    echo -e "$content" > "$lang_folder/$lang.json"
+    echo -e "$content" > "$lang_folder/$path/locales.json"
   done
 }
 
