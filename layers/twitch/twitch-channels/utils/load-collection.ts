ac09b4c7eeca840d923d2cfe28a27ce7efaefa71
Semyon Nebogatov <geforsenjs@gmail.com>
2024-09-29 21:50:16 +0700

    refactor: load person emotes collection (#77)
    * refactor: update locales
    
    * refactor: update toasts
    
    * refactor: handle error
    
    * fix: missing symbol
    
    * refactor: remove provided length
    
    * fix: locales
    
    * fix: min number
    
    * test: add
    
    * [autofix.ci] apply automated fixes
    
    * test: fix
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- layers/twitch/twitch-channels/utils/load-collection.ts
+++ layers/twitch/twitch-channels/utils/load-collection.ts
@@ -1,3 +1,10 @@
+import { FetchError } from "ofetch";
+
+const issueCodeRecord = {
+  too_small: "tooSmallInput",
+  too_big: "tooBigInput",
+};
+
 export function usePersonEmotesCollectionLoad(
   nicknameInput: Ref<string>,
   options: {
@@ -16,8 +23,20 @@ export function usePersonEmotesCollectionLoad(
         nicknameInput.value = "";
       }
       const login = toLowerCase(nickname);
-      const collection
-        = await personsEmotesCollectionsStore.loadCollection(login);
+      const collection = await personsEmotesCollectionsStore
+        .loadCollection(login)
+        .catch((error) => {
+          assert.isError(error, FetchError);
+          const issueCode = error.data?.data?.issueCode;
+          assert.ok(
+            typeof issueCode === "string" && issueCode in issueCodeRecord,
+          );
+          // @ts-expect-error `issueCode in issueCodeRecord is used`, so it's safe
+          const failKey = issueCodeRecord[
+            issueCode
+          ] as keyof typeof issueCodeRecord;
+          return toast.panic(failKey) as never;
+        });
       toast.success(
         nickname,
         getEmotesIntegrationsStatusAsEmojisString(collection.integrations),
