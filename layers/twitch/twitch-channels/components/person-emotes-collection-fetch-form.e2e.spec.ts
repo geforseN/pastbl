ebb9c0c4c8541efbf110abc74e140e7f651dc3dc
geforseN <geforsenjs@gmail.com>
2024-09-29 21:37:13 +0700

    test: fix

--- layers/twitch/twitch-channels/components/person-emotes-collection-fetch-form.e2e.spec.ts
+++ layers/twitch/twitch-channels/components/person-emotes-collection-fetch-form.e2e.spec.ts
@@ -2,18 +2,25 @@ import { test, expect } from "@nuxt/test-utils/playwright";
 
 test("person emotes collection will show alert on bad input", async ({ page }) => {
   await page.goto("/collections");
+  await expect(page.getByTestId("chat-pasta-list-skeleton")).toBeHidden();
 
   await test.step("small input", async () => {
     await page.getByTestId("person-emotes-collection-fetch-input").fill("hm");
     await page.getByRole("button", { name: "Load collection" }).click();
-    await expect(page.getByRole("alert")).toBeVisible();
-    await expect(page.getByRole("alert")).toContainText("Nickname is too short, min 3 characters");
+    await expect(
+      page
+        .getByRole("alert")
+        .filter({ hasText: "Nickname is too short, min 3 characters" }),
+    ).toBeVisible();
   });
 
   await test.step("big input", async () => {
     await page.getByTestId("person-emotes-collection-fetch-input").fill("thisisverylonganditwillnotwork");
     await page.getByRole("button", { name: "Load collection" }).click();
-    await expect(page.getByRole("alert")).toBeVisible();
-    await expect(page.getByRole("alert")).toContainText("Nickname is too long, max 25 characters");
+    await expect(
+      page
+        .getByRole("alert")
+        .filter({ hasText: "Nickname is too long, max 25 characters" }),
+    ).toBeVisible();
   });
 });
