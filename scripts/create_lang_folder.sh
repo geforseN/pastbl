edde6459d426c3a67ff6c2cc93df38fd35253f9c
geforseN <geforsenjs@gmail.com>
2024-09-30 11:01:31 +0700

    chore: update

--- scripts/create_lang_folder.sh
+++ scripts/create_lang_folder.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # NOTE: execute firstly: chmod +x scripts/create_lang_folder.sh
 
 create_lang_folder() {
   local path=$1
+  local lang_folder="$path/lang"
 
   local content="{\n\t\n}"
   mkdir -p "$lang_folder"
   for lang in en ru; do
-    echo -e "$content" > "$lang_folder/$path/locales.json"
+    echo -e "$content" > "$lang_folder/$lang.json"
   done
 }
 
--- scripts/vitest-nuxt-with-nitro.sh
+++ scripts/vitest-nuxt-with-nitro.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 function cleanup {
   echo "Stopping Nuxt server..."
