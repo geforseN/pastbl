69a8c31411684a752c7ec26403e575e6b88dc27d
geforseN <geforsenjs@gmail.com>
2024-10-05 13:51:38 +0700

    feat: add file path log

--- scripts/count-comments.js
+++ scripts/count-comments.js
@@ -5,38 +5,61 @@ const diffOutput = execSync("git diff --name-only origin/main").toString();
 const files = diffOutput.split("\n").filter(Boolean);
 
 let todoCount = 0;
+const todoEntries = [];
 const todoRegex = /(?:\/\/|\/\*+|#|<!--)\s*todo\b[\s\S]*?(?:\*\/|-->|\n|$)/i;
 
 let fixmeCount = 0;
+const fixmeEntries = [];
 const fixmeRegex = /(?:\/\/|\/\*+|#|<!--)\s*fixme\b[\s\S]*?(?:\*\/|-->|\n|$)/i;
 
 let noteCount = 0;
+const noteEntries = [];
 const noteRegex = /(?:\/\/|\/\*+|#|<!--)\s*note\b[\s\S]*?(?:\*\/|-->|\n|$)/i;
 
 for (const file of files) {
   const content = readFileSync(file, "utf-8");
-
   const lines = content.split("\n");
-  for (const line of lines) {
+
+  for (let lineNumber = 1; lineNumber <= lines.length; lineNumber++) {
+    const line = lines[lineNumber];
+
     if (todoRegex.test(line)) {
       todoCount++;
+      todoEntries.push([file, lineNumber]);
     }
     if (fixmeRegex.test(line)) {
       fixmeCount++;
+      fixmeEntries.push([file, lineNumber]);
     }
     if (noteRegex.test(line)) {
       noteCount++;
+      noteEntries.push([file, lineNumber]);
     }
   }
+}
+
+/**
+ * @param {string[]} entries
+ * @param {number} count
+ * @returns {string}
+ * */
+function formatCount(entries, count) {
+  return entries.length
+    ? ` - ${count} - ${entries
+      .map(([file, lineNumber]) => `- **File**: '${file}', **Line**: '${lineNumber}'`)
+      .join("\n")}`
+    : " - None";
 }
 
 // eslint-disable-next-line no-console
 console.log(`
 ## Summary of Comments
 
-- **TODOs**: ${todoCount}
-- **FIXMEs**: ${fixmeCount}
-- **NOTEs**: ${noteCount}
+- **TODOs**: ${formatCount(todoEntries, todoCount)}
+
+- **FIXMEs**: ${formatCount(fixmeEntries, fixmeCount)}
+
+- **NOTEs**: ${formatCount(noteEntries, noteCount)}
 
 ---
 
