8980ed33d54ba3b15dceef66d8dadaee6610f89c
Semyon Nebogatov <geforsenjs@gmail.com>
2024-10-05 14:25:43 +0700

    feat: add comments check in ci (#89)
    * ci: add comments-counts.yml
    
    * style: lint
    
    * fix: wrong name
    
    * ci(comments-counts): set fetch-depth to 0
    
    * ci: add timeout-minutes
    
    * feat(scripts): better log when no env
    
    * fix(scripts): env parse
    
    * [autofix.ci] apply automated fixes
    
    * refactor(scripts): remove fetch post
    
    * ci(comments-counts): use peter-evans/create-or-update-comment
    
    * ci: use body-path
    
    * ci: update token
    
    * ci: add permissions
    
    * feat: add file path log
    
    * [autofix.ci] apply automated fixes
    
    * refactor(scripts): change format
    
    * refactor(scripts): change format
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ scripts/count-comments.js
@@ -0,0 +1,68 @@
+import { readFileSync } from "fs";
+import { execSync } from "child_process";
+
+const diffOutput = execSync("git diff --name-only origin/main").toString();
+const files = diffOutput.split("\n").filter(Boolean);
+
+let todoCount = 0;
+const todoEntries = [];
+const todoRegex = /(?:\/\/|\/\*+|#|<!--)\s*todo\b[\s\S]*?(?:\*\/|-->|\n|$)/i;
+
+let fixmeCount = 0;
+const fixmeEntries = [];
+const fixmeRegex = /(?:\/\/|\/\*+|#|<!--)\s*fixme\b[\s\S]*?(?:\*\/|-->|\n|$)/i;
+
+let noteCount = 0;
+const noteEntries = [];
+const noteRegex = /(?:\/\/|\/\*+|#|<!--)\s*note\b[\s\S]*?(?:\*\/|-->|\n|$)/i;
+
+for (const file of files) {
+  const content = readFileSync(file, "utf-8");
+  const lines = content.split("\n");
+
+  for (let lineNumber = 1; lineNumber <= lines.length; lineNumber++) {
+    const line = lines[lineNumber];
+
+    if (todoRegex.test(line)) {
+      todoCount++;
+      todoEntries.push([file, lineNumber]);
+    }
+    if (fixmeRegex.test(line)) {
+      fixmeCount++;
+      fixmeEntries.push([file, lineNumber]);
+    }
+    if (noteRegex.test(line)) {
+      noteCount++;
+      noteEntries.push([file, lineNumber]);
+    }
+  }
+}
+
+/**
+ * @param {string[]} entries
+ * @param {number} count
+ * @returns {string}
+ * */
+function formatCount(entries, count) {
+  return entries.length
+    ? ` - ${count} 
+  - ${entries
+    .map(([file, lineNumber]) => `**${file}:${lineNumber}**`)
+    .join("\n")}`
+    : " - None";
+}
+
+// eslint-disable-next-line no-console
+console.log(`
+## Summary of Comments
+
+- **TODOs**: ${formatCount(todoEntries, todoCount)}
+
+- **FIXMEs**: ${formatCount(fixmeEntries, fixmeCount)}
+
+- **NOTEs**: ${formatCount(noteEntries, noteCount)}
+
+---
+
+Please address the above comments before merging.
+`);
