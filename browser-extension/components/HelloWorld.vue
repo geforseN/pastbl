7bf41f8fffbd7f08cb030fa7a2aadcce26b5842e
geforseN <geforsenjs@gmail.com>
2024-10-07 13:10:01 +0700

    chore: init browser-extension package with pnpm workspace

---
+++ browser-extension/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button
+      type="button"
+      @click="count++"
+    >
+      Count is {{ count }}
+    </button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Install
+    <a
+      href="https://github.com/vuejs/language-tools"
+      target="_blank"
+    >Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">
+    Click on the WXT and Vue logos to learn more
+  </p>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+defineProps<{
+  msg: string;
+}>();
+
+const count = ref(0);
+</script>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
