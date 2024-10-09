0dcd405b47a04bb7b3794c4ac7f52bf2f4ae2ea1
geforseN <geforsenjs@gmail.com>
2024-10-09 13:05:48 +0700

    fix:  global-emotes-integrations get-all handler

--- layers/emote-integrations/layers/global-emotes-integrations/server/utils/get-all.ts
+++ layers/emote-integrations/layers/global-emotes-integrations/server/utils/get-all.ts
@@ -1,14 +1,16 @@
 const getters = [
-  globalEmotesIntegrations.of("BetterTTV").get,
-  globalEmotesIntegrations.of("FrankerFaceZ").get,
-  globalEmotesIntegrations.of("SevenTV").get,
-  globalEmotesIntegrations.of("Twitch").get,
-];
+  () => globalEmotesIntegrations.of("BetterTTV").get(),
+  () => globalEmotesIntegrations.of("FrankerFaceZ").get(),
+  () => globalEmotesIntegrations.of("SevenTV").get(),
+  () => globalEmotesIntegrations.of("Twitch").get(),
+] as const;
 
 export async function getAllGlobalEmotesIntegrations() {
   const [BetterTTV, FrankerFaceZ, SevenTV, Twitch] = await Promise.all(
     getters.map((getter) => getter()),
   );
+
+  assert.ok(BetterTTV && FrankerFaceZ && SevenTV && Twitch);
 
   return {
     BetterTTV,
