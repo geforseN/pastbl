cb91dbbc7a19295b8c463189fd977e0b4e52dd78
geforseN <geforsenjs@gmail.com>
2024-10-10 19:43:15 +0700

    ci(test-api): init

--- vitest/server-api/$apiFetch.ts
+++ vitest/server-api/$apiFetch.ts
@@ -1,6 +1,9 @@
 import { $fetch as $fetch_ } from "ofetch";
+import { isCI } from "std-env";
 
-export const baseUrl = "http://127.0.0.1:3000";
+export const baseUrl = isCI
+  ? process.env.BASE_URL || "https://pastbl.vercel.app"
+  : "http://127.0.0.1:3000";
 
 const baseApiUrl = baseUrl + "/api/v1";
 
