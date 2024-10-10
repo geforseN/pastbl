c6ef2ba3cd919ed394aa7605bc8b58b7ced5777c
geforseN <geforsenjs@gmail.com>
2024-10-10 15:59:03 +0700

    chore: yesterday stuff

--- layers/emote-integrations/layers/global-emotes-integrations/server/tests/api/all.get.spec.ts
+++ layers/emote-integrations/layers/global-emotes-integrations/server/tests/api/all.get.spec.ts
@@ -4,6 +4,23 @@ import { $apiFetch } from "~~/vitest/server-api/$apiFetch";
 describe("GET /api/v1/global-emotes-integrations/all", () => {
   it("matches snapshot", async () => {
     const response = await $apiFetch("/global-emotes-integrations/all");
+
+    response.integrations.FrankerFaceZ.sets = response.integrations.FrankerFaceZ.sets.map((set) => {
+      if (set.allowedTo) {
+        assert.ok(
+          Array.isArray(set.allowedTo.twitchIds)
+          && set.allowedTo.twitchIds.every(Number.isInteger),
+        );
+        return {
+          ...set,
+          allowedTo: {
+            twitchIds: "IntegersArray",
+          },
+        };
+      }
+      return set;
+    });
+
     expect(response).toMatchSnapshot(
       {
         integrations: allEmoteSources.flatGroupBySource(() => ({
