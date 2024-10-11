454a7534b7ea9a09a5b089c87528cdd7ae9c50d4
geforseN <geforsenjs@gmail.com>
2024-10-11 18:11:22 +0700

    test: update nitro config

--- tests/server/nitro/vitest.config.ts
+++ tests/server/nitro/vitest.config.ts
@@ -1,19 +1,21 @@
+import { fileURLToPath } from "node:url";
 import { defineVitestConfig } from "@nuxt/test-utils/config";
 import { baseUrl } from "./$apiFetch";
+import { nitroTestInclude } from "./utils";
 
 // NOTE: must manually run `pnpm dev` and wait until server is loaded
+// NOTE: in scripts/dev-vitest-nitro.sh there there is such logic
 // ? Can it be done without manually run `pnpm dev` ?
 // ! If so then baseUrl must be changed to, for example 127.0.0.1:1234
 
-// FIXME: snapshots must not depend on baseUrl (localhost:3000 in snap files)
-// TODO: add import aliases for $apiFetch
 export default defineVitestConfig({
   test: {
-    include: ["**/server/tests/api/**/*.spec.ts"],
+    include: [...nitroTestInclude],
     environment: "nuxt",
     environmentOptions: {
       nuxt: {
         url: baseUrl,
+        rootDir: fileURLToPath(new URL("../../..", import.meta.url)),
       },
     },
   },
