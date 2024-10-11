b04ca46a6603fc9ff21d11eca71723fc74ec1397
geforseN <geforsenjs@gmail.com>
2024-10-11 12:36:46 +0700

    chore(scripts): update test server api

--- scripts/dev-vitest-nitro.sh
+++ scripts/dev-vitest-nitro.sh
@@ -13,7 +13,18 @@ NUXT_PID=$!
 pnpm exec wait-on http://127.0.0.1:3000 --interval 2000 || cleanup
 
 echo "Nuxt server is running, starting Vitest..."
-BASE_URL=http://127.0.0.1:3000 pnpm exec vitest --config vitest/server-api/vitest.config.ts || cleanup
-echo "Vitest finished successfully, stopping Nuxt server..."
+VITEST_SERVER_API_BASE_URL=http://127.0.0.1:3000 pnpm test:server
+TEST_EXIT_CODE=$?
 
+echo "Stopping Nuxt server..."
+kill $NUXT_PID
+
+if [ $TEST_EXIT_CODE -ne 0 ]; then
+echo "Vitest failed"
+exit $TEST_EXIT_CODE
+else
+echo "Vitest finished successfully"
+fi
+
+echo "Vitest finished successfully, stopping Nuxt server..."
 kill $NUXT_PID
