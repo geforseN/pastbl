083e051078cbf6c53aac49c61e5862bfaded2db6
geforseN <geforsenjs@gmail.com>
2024-10-11 13:14:11 +0700

    chore(scripts): update test server api

--- scripts/dev-vitest-nitro.sh
+++ scripts/dev-vitest-nitro.sh
@@ -9,23 +9,25 @@ echo "Using host: $HOST"
 echo "Using port: $PORT"
 
 cleanup() {
-  echo "Stopping Nuxt server..."
-  kill $NUXT_PID
+  if [ -n "$NUXT_PID" ]; then
+    echo "Stopping Nuxt server..."
+    kill "$NUXT_PID"
+  fi
   exit 1
 }
 
 echo "Starting Nuxt server on $HOST:$PORT..."
 pnpm exec nuxt dev --host "$HOST" --port "$PORT" & NUXT_PID=$!
 
-pnpm exec wait-on "http://$HOST:$PORT" --interval 2000
-if [ $? -ne 0 ]; then 
-  echo "Error: Nuxt server did not start in time."
-  cleanup
-fi
+pnpm exec wait-on "http://$HOST:$PORT" --interval 2000 || { 
+  echo "Error: Nuxt server did not start in time."; 
+  cleanup; 
+}
 
 echo "Nuxt server is running, starting Vitest with API base URL: http://$HOST:$PORT"
 
-VITEST_SERVER_API_BASE_URL="http://$HOST:$PORT" pnpm test:server
+VITEST_SERVER_API_BASE_URL="http://$HOST:$PORT" \
+pnpm test:server
 
 echo "Vitest $( [ $? -ne 0 ] && echo "failed" || echo "finished successfully" )"
 
