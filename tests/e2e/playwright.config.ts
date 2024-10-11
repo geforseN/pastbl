53e60a75037f05ffcf41eea71772bf7affb6cc03
geforseN <geforsenjs@gmail.com>
2024-10-11 18:04:31 +0700

    test: update playwright config

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -8,17 +8,21 @@ const options = isCI
   ? { host: process.env.PLAYWRIGHT_NUXT_HOST || "https://pastbl.vercel.app" }
   : { host: "http://127.0.0.1:3000" };
 
+const rootDir = fileURLToPath(new URL("../..", import.meta.url));
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig<ConfigOptions>({
+  testDir: rootDir,
+  outputDir: "test-results",
   testMatch: [...endToEndTestsGlobs],
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: isCI,
   retries: isCI ? 2 : 0,
   workers: isCI ? 1 : undefined,
-  globalTimeout: 60 * 1000,
+  globalTimeout: 1.5 * 60 * 1000,
+  timeout: 45 * 1000,
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -27,7 +31,7 @@ export default defineConfig<ConfigOptions>({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     nuxt: {
-      rootDir: fileURLToPath(new URL(".", import.meta.url)),
+      rootDir,
       ...options,
     },
   },
