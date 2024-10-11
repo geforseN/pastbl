cd9dba4c715f22429241c1b755664a821562846e
geforseN <geforsenjs@gmail.com>
2024-10-11 20:57:00 +0700

    chore(scripts): refactor and fix

--- scripts/dev-vitest-nitro.sh
+++ scripts/dev-vitest-nitro.sh
@@ -14,29 +14,29 @@ echo "Using port: $PORT"
 URL="http://$HOST:$PORT"
 
 cleanup() {
-  if [ -n "$NUXT_PID" ]; then
-    echo "Stopping Nuxt server..."
-    kill "$NUXT_PID"
-  fi
-  exit 1
+  local message="$1"
+  local exit_code=${2:-0}
+  # Redirecting to stderr if exit_code is not equal to 0
+  echo "$message" >&$((exit_code != 0 ? 2 : 1))
+  [ -n "$NUXT_PID" ] && { 
+    echo "Stopping Nuxt server..."; 
+    kill "$NUXT_PID";
+  }
+  exit "$exit_code"
 }
 
 echo "Starting Nuxt server on $HOST:$PORT..."
 
-TWITCH_APP_CLIENT_SECRET="$TWITCH_APP_CLIENT_SECRET" \   
+TWITCH_APP_CLIENT_SECRET="$TWITCH_APP_CLIENT_SECRET" \
 TWITCH_APP_CLIENT_ID="$TWITCH_APP_CLIENT_ID" \
 pnpm exec nuxt dev --host "$HOST" --port "$PORT" & NUXT_PID=$!
 
-pnpm exec wait-on "$URL" --interval 2000 || { 
-  echo "Error: Nuxt server did not start in time."; 
-  cleanup; 
-}
+pnpm exec wait-on "$URL" --interval 2000 || cleanup "Error: Nuxt server did not start in time." 1
 
 echo "Nuxt server is running, starting Vitest on $URL"
 
 VITEST_SERVER_API_BASE_URL="$URL" \
 pnpm test:nitro
 
-echo "Vitest $( [ $? -ne 0 ] && echo "failed" || echo "finished successfully" )"
+[ $? -ne 0 ] && cleanup "Vitest failed" $? || cleanup "Vitest finished successfully"
 
-cleanup
