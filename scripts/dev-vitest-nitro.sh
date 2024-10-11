e4ac6a5ff46e6d79ed7158ccd930321d9cf522aa
geforseN <geforsenjs@gmail.com>
2024-10-11 20:11:33 +0700

    fix(scripts): env vars

--- scripts/dev-vitest-nitro.sh
+++ scripts/dev-vitest-nitro.sh
@@ -2,6 +2,9 @@
 
 source ./scripts/_check_env_var.sh
 
+[ -n "$TWITCH_APP_CLIENT_SECRET" ] || { echo "TWITCH_APP_CLIENT_SECRET is empty" 1>&2; exit 1; }
+[ -n "$TWITCH_APP_CLIENT_ID" ] || { echo "TWITCH_APP_CLIENT_ID is empty" 1>&2; exit 1; }
+
 check_host_var "HOST" "$HOST" "localhost|127.0.0.1"
 check_port_var "PORT" "$PORT" 1024 65535
 
@@ -29,6 +32,8 @@ pnpm exec wait-on "$URL" --interval 2000 || {
 echo "Nuxt server is running, starting Vitest on $URL"
 
 VITEST_SERVER_API_BASE_URL="$URL" \
+TWITCH_APP_CLIENT_SECRET="$TWITCH_APP_CLIENT_SECRET" \   
+TWITCH_APP_CLIENT_ID="$TWITCH_APP_CLIENT_ID" \
 pnpm test:nitro
 
 echo "Vitest $( [ $? -ne 0 ] && echo "failed" || echo "finished successfully" )"
