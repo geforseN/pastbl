7076d41684dcd8a007fef33883e87fea2ee010ab
geforseN <geforsenjs@gmail.com>
2024-10-12 20:37:18 +0700

    fix: ts warn

--- layers/emote-integrations/utils/emotes.spec.ts
+++ layers/emote-integrations/utils/emotes.spec.ts
@@ -4,7 +4,10 @@ import { getEmotesMapInEmotesIntegrations } from "./emotes";
 describe("getEmotesMapInEmotesIntegrations", () => {
   suite("empty object input", () => {
     it("must return empty map on empty object input", () => {
-      const value = getEmotesMapInEmotesIntegrations({});
+      const value = getEmotesMapInEmotesIntegrations(
+        // @ts-expect-error Argument of type '{}' is not assignable to parameter of type 'SettledEmoteIntegrationsRecord'.
+        {},
+      );
       expect(value).toBeInstanceOf(Map);
       expect(value.size).toBe(0);
     });
