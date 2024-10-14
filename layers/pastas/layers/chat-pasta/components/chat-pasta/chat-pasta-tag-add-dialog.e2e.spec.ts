9896331decd40fc85fc590d67a0e6c708162eb31
geforseN <geforsenjs@gmail.com>
2024-10-14 17:00:17 +0700

    test: add

--- layers/pastas/layers/chat-pasta/components/chat-pasta/chat-pasta-tag-add-dialog.e2e.spec.ts
+++ layers/pastas/layers/chat-pasta/components/chat-pasta/chat-pasta-tag-add-dialog.e2e.spec.ts
@@ -2,22 +2,27 @@ import { expect, test } from "@nuxt/test-utils/playwright";
 import type { Page } from "@playwright/test";
 
 async function fillPastaForm(page: Page, { tag, text }: { tag: string; text: string }) {
-  await page.goto("/");
   await page.getByTestId("pasta-form-collapse").click();
-  await expect(page.getByTestId("chat-pasta")).toHaveCount(0);
   await page.getByTestId("pasta-form-textarea").fill(text);
   await page.getByTestId("add-pasta-tag-input").fill(tag);
-  await expect(page.getByTestId("chat-pasta")).toHaveCount(0);
 }
 
 function isNodeOpenDialog(node: Node) {
   return node instanceof HTMLDialogElement && node.open;
 }
 
-async function addPasta(page: Page, { dialogButtonName, expectedTextToInclude }: { dialogButtonName: string; expectedTextToInclude: RegExp }) {
+async function addPasta(page: Page, {
+  dialogButtonName,
+  expectedTextToInclude,
+  finalPastasCount,
+}: {
+  dialogButtonName: string;
+  expectedTextToInclude: RegExp;
+  finalPastasCount: number;
+
+}) {
   await expect(page.getByTestId("chat-pasta")).toHaveCount(0);
-  const textarea = page.getByTestId("pasta-form-textarea");
-  await textarea.press("Enter");
+  await page.getByTestId("pasta-form-textarea").press("Enter");
 
   const dialog = page.getByTestId("chat-pasta-tag-add-dialog");
   expect(await dialog.evaluate(isNodeOpenDialog)).toBe(true);
@@ -25,14 +30,18 @@ async function addPasta(page: Page, { dialogButtonName, expectedTextToInclude }:
 
   await dialog.getByRole("button", { name: dialogButtonName }).click();
 
-  await expect(page.getByTestId("add-pasta-tag-input")).toHaveValue("");
-  await expect(textarea).toHaveValue("");
+  await expect(page.getByTestId("pasta-form-textarea")).toBeEmpty();
+  await expect(page.getByTestId("add-pasta-tag-input")).toBeEmpty();
+
   expect(await dialog.evaluate(isNodeOpenDialog)).toBe(false);
-  await expect(page.getByTestId("chat-pasta")).toHaveCount(1);
+  await expect(page.getByTestId("chat-pasta")).toHaveCount(finalPastasCount);
 }
 
 test("add pasta with tag", async ({ page }) => {
+  await page.goto("/");
+
   await test.step("fill <pasta-form />", async () => {
+    await page.goto("/");
     await fillPastaForm(page, { text: "foo", tag: "bar" });
   });
 
@@ -40,6 +49,7 @@ test("add pasta with tag", async ({ page }) => {
     await addPasta(page, {
       dialogButtonName: "Add to pasta",
       expectedTextToInclude: /Tag: bar/,
+      finalPastasCount: 1,
     });
     await expect(page.getByTestId("chat-pasta-tag")).toHaveCount(1);
     await expect(page.getByTestId("chat-pasta-tag")).toHaveText("bar");
@@ -47,6 +57,8 @@ test("add pasta with tag", async ({ page }) => {
 });
 
 test("add pasta without tag", async ({ page }) => {
+  await page.goto("/");
+
   await test.step("fill <pasta-form />", async () => {
     await fillPastaForm(page, { text: "fiz", tag: "baz" });
   });
@@ -55,7 +67,35 @@ test("add pasta without tag", async ({ page }) => {
     await addPasta(page, {
       dialogButtonName: "Nothing",
       expectedTextToInclude: /Tag: baz/,
+      finalPastasCount: 1,
     });
     await expect(page.getByTestId("chat-pasta-tag")).toHaveCount(0);
   });
+});
+
+test("add many pastas", async ({ page }) => {
+  await page.goto("/");
+
+  await fillPastaForm(page, { text: "fiz", tag: "baz" });
+  await addPasta(page, {
+    dialogButtonName: "Nothing",
+    expectedTextToInclude: /Tag: baz/,
+    finalPastasCount: 1,
+  });
+
+  await fillPastaForm(page, { text: "foo", tag: "bar" });
+  await addPasta(page, {
+    dialogButtonName: "Add to pasta",
+    expectedTextToInclude: /Tag: bar/,
+    finalPastasCount: 2,
+  });
+  await expect(page.getByTestId("chat-pasta-tag")).toHaveText("bar");
+
+  await fillPastaForm(page, { text: "fiz", tag: "baz" });
+  await addPasta(page, {
+    dialogButtonName: "Nothing",
+    expectedTextToInclude: /Tag: baz/,
+    finalPastasCount: 3,
+  });
+  await expect(page.getByTestId("chat-pasta-tag")).toHaveCount(1);
 });
