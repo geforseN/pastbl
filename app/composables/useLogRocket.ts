5610eca697156284c0759186bd0de2b6bc9bd58d
geforseN <geforsenjs@gmail.com>
2024-10-26 18:17:48 +0700

    refactor(composables): [useLogRocket] update
    - move `getLogRocketUserId` to global scope
    - use `watchImmediate` instead of `whenever`
    - add debug log

--- app/composables/useLogRocket.ts
+++ app/composables/useLogRocket.ts
@@ -1,5 +1,18 @@
 import LogRocket from "logrocket";
 
+async function getLogRocketUserId(
+  userSession: ReturnType<typeof useUserSession>,
+  isLoggedIn: boolean,
+) {
+  const login = userSession.user.value?.twitch?.login;
+  if (isLoggedIn && login) {
+    return login;
+  }
+  const nickname = useIndexedDBKeyValue("nickname:value", "Kappa");
+  await until(nickname.isRestored).toBeTruthy();
+  return nickname.state.value || "unknown";
+}
+
 export function useLogRocket(
   strategy: "ignore-all" | "init" | "init&identify" = "init&identify",
 ) {
@@ -13,22 +26,10 @@ export function useLogRocket(
 
   const userSession = useUserSession();
 
-  async function getLogRocketUserId(
-    userSession: ReturnType<typeof useUserSession>,
-    isLoggedIn: boolean,
-  ) {
-    const login = userSession.user.value?.twitch?.login;
-    if (isLoggedIn && login) {
-      return login;
-    }
-    const nickname = useIndexedDBKeyValue("nickname:value", "Kappa");
-    await until(nickname.isRestored).toBeTruthy();
-    return nickname.state.value || "unknown";
-  }
-
-  whenever(userSession.ready, async () => {
+  watchImmediate(userSession.ready, async () => {
     const loggedIn = userSession.loggedIn.value;
     const uid = await getLogRocketUserId(userSession, loggedIn);
+    console.debug({ loggedIn, uid });
     LogRocket.identify(uid, { loggedIn });
   });
 }
