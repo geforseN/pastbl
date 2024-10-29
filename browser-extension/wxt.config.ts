7f1fed76bbf807a85ceeca15ef0b8d5d2bb9a2af
geforseN <geforsenjs@gmail.com>
2024-10-29 13:03:33 +0700

    chore(deps): [browser-extension] add `vite-plugin-vue-devtools`

--- browser-extension/wxt.config.ts
+++ browser-extension/wxt.config.ts
@@ -1,6 +1,15 @@
+import devtools from "vite-plugin-vue-devtools";
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-vue"],
+  vite: () => ({
+    plugins: [
+      devtools({
+        appendTo: "entrypoints/popup/main.ts",
+      }),
+    ],
+  }),
+
 });
