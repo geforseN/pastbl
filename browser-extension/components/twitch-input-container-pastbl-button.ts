823ed12b48e59d94bcadc8abf18c63cba305b2cc
geforseN <geforsenjs@gmail.com>
2024-10-29 17:33:18 +0700

    fix: [browser-extension] htm class

--- browser-extension/components/twitch-input-container-pastbl-button.ts
+++ browser-extension/components/twitch-input-container-pastbl-button.ts
@@ -6,7 +6,7 @@ function createButton(
 ) {
   const button = document.createElement("button");
   button.textContent = "pastbl";
-  button.classList.add(styles.pastbl__button, "asd");
+  button.classList.add(styles.pastbl__button);
   button.addEventListener("click", clickListener);
   return button;
 }
@@ -26,8 +26,7 @@ function getButtonContainer(): HTMLElement {
 
 function emplaceButton(buttonsContainer: HTMLElement, consola: ConsolaInstance) {
   consola.log({ buttonsContainer, where: "emplaceButton" });
-  // FIXME: do not use .asd
-  if (!buttonsContainer.querySelector(`.asd`)) {
+  if (!buttonsContainer.querySelector(`.${styles.pastbl__button}`)) {
     const button = createButton(() => {
       consola.log("clicked pastbl button");
     });
