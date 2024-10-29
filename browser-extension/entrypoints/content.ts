5d566805644228b9f8cc80c324cda6e64eef951e
geforseN <geforsenjs@gmail.com>
2024-10-29 14:30:31 +0700

    feat: [browser-extension] foo

--- browser-extension/entrypoints/content.ts
+++ browser-extension/entrypoints/content.ts
@@ -1,6 +1,52 @@
+function addPastblButton(buttonContainer: HTMLElement) {
+  console.log({
+    buttonContainer,
+    b: !buttonContainer.querySelector(".pastbl-button"),
+  });
+  if (buttonContainer && !buttonContainer.querySelector(".pastbl-button")) {
+    const button = document.createElement("button");
+    button.textContent = "pastbl";
+    button.classList.add("pastbl-button");
+    button.style.marginRight = "auto"; // чтобы разместить слева
+    button.addEventListener("click", () => {
+      // Ваша логика кнопки
+    });
+    buttonContainer.prepend(button);
+  }
+}
+
+const chatInputButtonsContainerSelector = "chat-input__buttons-container";
+
+function foo() {
+  const container = document.querySelector(chatInputButtonsContainerSelector);
+  if (!container) {
+    throw new Error("container not found");
+  }
+  if (!(container instanceof HTMLElement)) {
+    const error = new Error("container is not HTMLElement");
+    error.context = { container };
+    throw error;
+  }
+  return container;
+}
+
 export default defineContentScript({
-  matches: ["*://*.google.com/*"],
+  matches: ["*://*.twitch.tv/*"],
   main() {
-    console.log("Hello content.");
+    console.log("hello from content script");
+    const observer = new MutationObserver(() => {
+      console.log("hello from MutationObserver");
+      const buttonsContainer = foo();
+      console.log({ buttonsContainer, where: "MutationObserver" });
+      addPastblButton(buttonsContainer);
+    });
+
+    document.addEventListener("DOMContentLoaded", () => {
+      console.log("hello from DOMContentLoaded");
+      const buttonsContainer = foo();
+      console.log({ buttonsContainer, where: "DOMContentLoaded" });
+      addPastblButton(buttonsContainer);
+      observer.observe(buttonsContainer, { childList: true, subtree: true });
+    });
   },
 });
