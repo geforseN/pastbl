03e3a6cb52713d17e6e5107d752ec20e13241b00
geforseN <geforsenjs@gmail.com>
2024-10-29 19:50:04 +0700

    feat: [pastas] cors headers

--- layers/pastas/layers/remote-pastas/server/api/v1/pastas.get.ts
+++ layers/pastas/layers/remote-pastas/server/api/v1/pastas.get.ts
@@ -1,6 +1,10 @@
 import { getPastas } from "~~/database/pastas.ts";
 
 export default defineEventHandler(async (event) => {
+  setHeaders(event, {
+    "Access-Control-Allow-Origin": "https://www.twitch.tv",
+    "Access-Control-Allow-Credentials": "true",
+  });
   const userTwitchId = await requireUserTwitchIdFromSession(event);
   const queryCursor = getPastasCursorFromQuery(event);
   const { pastas, cursor } = await getPastas(userTwitchId, queryCursor);
