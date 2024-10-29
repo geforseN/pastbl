c9cc2fd69716d24eab66a6d062e14e6dc666818f
geforseN <geforsenjs@gmail.com>
2024-10-29 16:44:11 +0700

    feat: [browser-extension] add config prototype

--- browser-extension/entrypoints/content.ts
+++ browser-extension/entrypoints/content.ts
@@ -2,13 +2,25 @@ import { consola } from "consola";
 
 const _consola = consola.withTag("pastbl").withTag("browser-extension");
 
+const config = {
+  twitch: {
+    "chat-input": {
+      "buttons-container": {
+        selector: ".chat-input__buttons-container",
+      },
+    },
+  },
+} as const;
+
 export default defineContentScript({
   matches: ["*://*.twitch.tv/*"],
   main() {
     _consola.log("hello from content script");
     const observer = createMutationObserver(_consola);
     const buttonsInterval = setInterval(() => {
-      const container = document.querySelector(".chat-input__buttons-container");
+      const container = document.querySelector(
+        config.twitch["chat-input"]["buttons-container"].selector,
+      );
       if (container) {
         observer.observe(container, { childList: true, subtree: true });
         clearInterval(buttonsInterval);
