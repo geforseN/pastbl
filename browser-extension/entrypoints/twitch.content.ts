746eb58432bd97480db6daeaa45745b7e3f883d5
geforseN <geforsenjs@gmail.com>
2024-10-30 20:37:06 +0700

    chore(entrypoints): [twitch] change matches

--- browser-extension/entrypoints/twitch.content.ts
+++ browser-extension/entrypoints/twitch.content.ts
@@ -3,7 +3,7 @@ import "@/assets/index.css";
 import PastblApp from "@/components/pastbl-app.vue";
 
 export default defineContentScript({
-  matches: ["*://*.twitch.tv/*"],
+  matches: ["*://twitch.tv/*"],
   main(_context) {
     consola.success("content script loaded");
     const div = document.createElement("div");
