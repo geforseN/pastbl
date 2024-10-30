f81fb907b381adea10b2eb7d53683238ebe7aabe
geforseN <geforsenjs@gmail.com>
2024-10-30 13:07:00 +0700

    chore: clear

--- browser-extension/components/HelloWorld.vue
+++ browser-extension/components/HelloWorld.vue
@@ -4,20 +4,15 @@
     <button
       type="button"
       class="btn btn-primary btn-sm w-fit"
-      @click="count++"
     >
       pastbl
     </button>
   </div>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
-
 defineProps<{
   msg: string;
 }>();
-
-const count = ref(0);
 </script>
 <style scoped>
 .read-the-docs {
--- browser-extension/entrypoints/popup/App.vue
+++ browser-extension/entrypoints/popup/App.vue
@@ -3,20 +3,6 @@ import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 <template>
   <div class="w-80 p-4">
-    <HelloWorld msg="WXT + Vue" />
+    <hello-world msg="WXT + Vue" />
   </div>
 </template>
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #54bc4ae0);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
