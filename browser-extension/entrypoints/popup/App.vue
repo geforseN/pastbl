c23f528cd8ba2aa3aa0757dddc0abb67c9d9a80a
geforseN <geforsenjs@gmail.com>
2024-10-30 13:11:02 +0700

    chore: clear

--- browser-extension/entrypoints/popup/App.vue
+++ browser-extension/entrypoints/popup/App.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import HelloWorld from "@/components/HelloWorld.vue";
+import TwitchInputContainerButton from "@/components/twitch-input-container-button.vue";
 </script>
 <template>
   <div class="w-80 p-4">
-    <hello-world />
+    <twitch-input-container-button />
   </div>
 </template>
