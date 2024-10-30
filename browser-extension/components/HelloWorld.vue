acb8311bd1851f0ca85d99c7b9bfb755d1705e91
geforseN <geforsenjs@gmail.com>
2024-10-30 13:05:36 +0700

    chore: clear

--- browser-extension/components/HelloWorld.vue
+++ browser-extension/components/HelloWorld.vue
@@ -1,30 +1,14 @@
 <template>
-  <h1>{{ msg }}</h1>
   <div class="flex flex-col items-center">
     <!-- eslint-disable tailwindcss/no-custom-classname -->
     <button
       type="button"
-      class="btn-2 btn btn-primary btn-xs w-fit bg-gradient-to-b px-4 py-2"
+      class="btn btn-primary btn-sm w-fit"
       @click="count++"
     >
-      Pastbl
+      pastbl
     </button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-  <p>
-    Install
-    <a
-      href="https://github.com/vuejs/language-tools"
-      target="_blank"
-    >Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">
-    Click on the WXT and Vue logos to learn more
-  </p>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
@@ -38,10 +22,5 @@ const count = ref(0);
 <style scoped>
 .read-the-docs {
   color: #888;
-}
-
-.btn-2 {
-  background: rgb(145,71,255);
-  background: radial-gradient(circle, rgba(249,115,22,0.7) 70%, rgba(145,71,255,1) 95%);
 }
 </style>
--- browser-extension/entrypoints/popup/App.vue
+++ browser-extension/entrypoints/popup/App.vue
@@ -2,29 +2,7 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 <template>
-  <div class="p-4">
-    <div class="flex w-60">
-      <a
-        href="https://wxt.dev"
-        target="_blank"
-      >
-        <img
-          src="/wxt.svg"
-          class="logo"
-          alt="WXT logo"
-        />
-      </a>
-      <a
-        href="https://vuejs.org/"
-        target="_blank"
-      >
-        <img
-          src="@/assets/vue.svg"
-          class="logo vue"
-          alt="Vue logo"
-        />
-      </a>
-    </div>
+  <div class="w-80 p-4">
     <HelloWorld msg="WXT + Vue" />
   </div>
 </template>
