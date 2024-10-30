ddbc0d5c4477aac207a9dee5f69d14f7ea14c9ce
geforseN <geforsenjs@gmail.com>
2024-10-30 20:27:38 +0700

    chore: [browser-extension] use error-boundary and authorize-to-twitch-button

---
+++ browser-extension/components/authorize-to-twitch-button.vue
@@ -0,0 +1,17 @@
+<template>
+  <a
+    :href="config.pastbl.auth.path"
+    target="_blank"
+    class="group btn btn-ghost w-fit flex-nowrap bg-twitch-accent text-base-300 xs:w-auto"
+  >
+    <span
+      class="text-lg/1 group-hover:underline group-hover:decoration-2 group-hover:underline-offset-2"
+    >
+      <!-- TODO: i18n browser extension api -->
+      Auth to Twitch
+    </span>
+  </a>
+</template>
+<script setup lang="ts">
+import { config } from "@/utils/config";
+</script>
--- browser-extension/entrypoints/popup/App.vue
+++ browser-extension/entrypoints/popup/App.vue
@@ -1,30 +1,38 @@
 <template>
   <div>
-    <twitch-input-container-button
-      @click="onButtonClick"
-    />
-    <teleport to="#pastbl">
-      <x-pastas-list
-        v-show="isPastasListVisible"
-        class="absolute right-0 top-0 z-50 bg-purple-800 p-2 text-white"
-        :pastas
-        @copy="copyPasta"
-        @send="sendPasta"
+    <error-boundary>
+      <twitch-input-container-button
+        @click="onButtonClick"
       />
-    </teleport>
+      <teleport to="#pastbl">
+        <x-pastas-list
+          v-show="isPastasListVisible"
+          class="absolute right-0 top-0 z-50 bg-purple-800 p-2 text-white"
+          :pastas
+          @copy="copyPasta"
+          @send="sendPasta"
+        />
+      </teleport>
+      <template #error="{ error }">
+        <authorize-to-twitch-button v-if="isNotAuthorizedError(error)" />
+      </template>
+    </error-boundary>
   </div>
 </template>
 <script setup lang="ts">
 import TwitchInputContainerButton from "@/components/twitch-input-container-button.vue";
 import XPastasList from "@/components/x-pastas-list.vue";
+import AuthorizeToTwitchButton from "@/components/authorize-to-twitch-button.vue";
+import ErrorBoundary from "@/components/error-boundary.vue";
+import { isNotAuthorizedError } from "@/utils/pastas";
 
 const isPastasListVisible = ref(false);
 const openPastasList = () => isPastasListVisible.value = true;
 
-const onButtonClick = () => {
+async function onButtonClick() {
   openPastasList();
   if (pastas.value.length === 0) {
-    fetchFirstPastas();
+    await fetchFirstPastas();
   }
 };
 
@@ -61,12 +69,8 @@ function trySaveExistingInputData(input: Element) {
 }
 
 const fetchFirstPastas = async () => {
-  try {
-    consola.log("clicked pastbl button");
-    const json = await fetchPastas();
-    pastas.value.push(...json.pastas);
-  } catch (error) {
-    consola.error(error);
-  }
+  consola.log("clicked pastbl button");
+  const json = await fetchPastas();
+  pastas.value.push(...json.pastas);
 };
 </script>
