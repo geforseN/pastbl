4a955862b9d6c686ca1f87358b7cdca118aeea3f
geforseN <geforsenjs@gmail.com>
2024-10-31 13:31:45 +0700

    refactor(config): [browser-extension] update
    add withDefaultValueIfNotString

--- browser-extension/utils/config.ts
+++ browser-extension/utils/config.ts
@@ -1,7 +1,7 @@
 let pastblBaseUrl = import.meta.env.WXT_PASTBL_GET_PASTAS_PATH;
 if (!pastblBaseUrl) {
   consola.info("WXT_PASTBL_BASE_URL is not set, using `https://localhost:3000`");
-  pastblBaseUrl = "https://localhost:3000";
+  pastblBaseUrl = "http://localhost:3000";
 } else if (typeof pastblBaseUrl !== "string") {
   throw new TypeError("provided WXT_PASTBL_BASE_URL env variable is not a string");
 } else if (pastblBaseUrl.endsWith("/")) {
@@ -10,13 +10,25 @@ if (!pastblBaseUrl) {
 }
 consola.info("final pastblBaseUrl is", pastblBaseUrl);
 
-const pastblAuthPathViaTwitch
-  = import.meta.env.WXT_PASTBL_AUTH_VIA_TWITCH_PATH
-  || pastblBaseUrl + "/auth/twitch";
+function withDefaultValueIfNotString(value: unknown, defaultValue: string) {
+  if (typeof value !== "string") {
+    if (defaultValue === undefined) {
+      throw new TypeError("defaultValue is not provided");
+    }
+    return defaultValue;
+  }
+  return value;
+};
 
-const pastblGetPastasPath
-  = import.meta.env.WXT_PASTBL_GET_PASTAS_PATH
-  || pastblBaseUrl + "/api/v1/pastas";
+const pastblAuthPathViaTwitch = withDefaultValueIfNotString(
+  import.meta.env.WXT_PASTBL_AUTH_VIA_TWITCH_PATH,
+  pastblBaseUrl + "/auth/twitch",
+);
+
+const pastblGetPastasPath = withDefaultValueIfNotString(
+  import.meta.env.WXT_PASTBL_GET_PASTAS_PATH,
+  pastblBaseUrl + "/api/v1/pastas",
+);
 
 export const config = {
   twitch: {
