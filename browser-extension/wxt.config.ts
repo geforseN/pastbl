7c3819e9d3f2b763a6da449b81c20081f684f2a6
geforseN <geforsenjs@gmail.com>
2024-10-31 17:57:20 +0700

    feat: support `srcDir`

--- browser-extension/wxt.config.ts
+++ browser-extension/wxt.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
+  srcDir: "app",
   modules: ["@wxt-dev/module-vue", "@wxt-dev/i18n/module"],
   manifest: {
     name: "pastbl",
