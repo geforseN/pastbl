b74e0f9b7a3345960edccaf2e0768f075995212f
geforseN <geforsenjs@gmail.com>
2024-10-31 17:47:31 +0700

    refactor(components): [pastbl-app]
    add `unknown-error` status
    lint
    change styles
    use i18n

--- browser-extension/components/pastbl-app.vue
+++ browser-extension/components/pastbl-app.vue
@@ -1,28 +1,25 @@
 <template>
   <div
     :class="[
-      isContainerVisible
-        // NOTE: use outline instead of border to avoid <<pastbl-rounded-button /> moving
-        ? 'outline outline-2 outline-base-content'
-        : 'pointer-events-none',
+      !isContainerVisible && 'pointer-events-none',
     ]"
     class="relative z-40 h-[400px] w-[320px] overflow-hidden rounded-lg rounded-br-3xl"
   >
     <div
       v-if="status === 'loading' && isContainerVisible"
-      class="absolute inset-0 animate-pulse bg-gray-400 "
+      class="absolute inset-0 animate-pulse bg-gray-400"
     />
     <pastbl-rounded-button
-      class="pointer-events-auto visible absolute bottom-1 right-1 z-50 flex"
+      class="pointer-events-auto visible absolute bottom-px right-px z-50 flex"
       @click="toggleVisibility"
     />
     <div
       v-show="isContainerVisible"
-      class="flex h-full flex-col-reverse items-center"
+      class="flex h-full flex-col-reverse items-center bg-purple-900 p-2"
     >
       <x-pastas-list
         v-if="status === 'ready'"
-        class="bg-purple-800 p-2 text-white"
+        class="bg-base-100 text-base-content"
         :pastas
         @copy="copyPasta"
         @send="sendPasta"
@@ -32,7 +29,7 @@
         class="flex flex-col items-center gap-2"
       >
         <span class="text-lg">
-          You are not authorized to
+          {{ i18n.t('youAreNotAuthorizedTo') }}
           <a
             class="link"
             :href="config.pastbl.baseUrl"
@@ -41,6 +38,20 @@
           </a>
         </span>
         <authorize-to-twitch-button />
+      </div>
+      <div
+        v-if="status === 'unknown-error'"
+        class="flex flex-col items-center text-lg"
+      >
+        {{ i18n.t('somethingWentWrong') }}
+        <!-- eslint-disable @stylistic/max-len  -->
+        <a
+          class="link link-accent"
+          href="https://github.com/geforseN/pastbl/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="
+        >
+          <!-- eslint-enable @stylistic/max-len  -->
+          {{ i18n.t('createIssueHere') }}
+        </a>
       </div>
     </div>
   </div>
@@ -55,14 +66,20 @@ import { fetchFirstPastas, copyPasta, sendPasta } from "@/utils/handlers";
 const isContainerVisible = ref(false);
 const toggleVisibility = () => isContainerVisible.value = !isContainerVisible.value;
 
-const status = ref<"loading" | "not-authorized" | "ready">("loading");
+const status = ref<
+  | "loading"
+  | "ready"
+  | "not-authorized"
+  | "unknown-error"
+>("loading");
+
 onMounted(async () => {
   await fetchFirstPastas()
     .then(() => status.value = "ready")
     .catch((error) => {
-      if (isNotAuthorizedError(error)) {
-        status.value = "not-authorized";
-      }
+      status.value = isNotAuthorizedError(error)
+        ? "not-authorized"
+        : "unknown-error";
     });
 });
 </script>
