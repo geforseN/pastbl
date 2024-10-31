660e4720679c5c9f783d2991411c94507977d094
geforseN <geforsenjs@gmail.com>
2024-10-31 17:05:18 +0700

    fix(config): [browser-extension] wrong env var

--- browser-extension/utils/config.ts
+++ browser-extension/utils/config.ts
@@ -1,14 +1,14 @@
-let pastblBaseUrl = import.meta.env.WXT_PASTBL_GET_PASTAS_PATH;
-if (!pastblBaseUrl) {
+let pastblBasePath = import.meta.env.WXT_PASTBL_BASE_PATH;
+if (!pastblBasePath) {
   consola.info("WXT_PASTBL_BASE_URL is not set, using `https://localhost:3000`");
-  pastblBaseUrl = "http://localhost:3000";
-} else if (typeof pastblBaseUrl !== "string") {
+  pastblBasePath = "http://localhost:3000";
+} else if (typeof pastblBasePath !== "string") {
   throw new TypeError("provided WXT_PASTBL_BASE_URL env variable is not a string");
-} else if (pastblBaseUrl.endsWith("/")) {
+} else if (pastblBasePath.endsWith("/")) {
   consola.info("WXT_PASTBL_BASE_URL ends with `/`, removing it");
-  pastblBaseUrl = pastblBaseUrl.slice(0, -1);
+  pastblBasePath = pastblBasePath.slice(0, -1);
 }
-consola.info("final pastblBaseUrl is", pastblBaseUrl);
+consola.info("final pastblBasePath is", pastblBasePath);
 
 function withDefaultValueIfNotString(value: unknown, defaultValue: string) {
   if (typeof value !== "string") {
@@ -22,12 +22,12 @@ function withDefaultValueIfNotString(value: unknown, defaultValue: string) {
 
 const pastblAuthPathViaTwitch = withDefaultValueIfNotString(
   import.meta.env.WXT_PASTBL_AUTH_VIA_TWITCH_PATH,
-  pastblBaseUrl + "/auth/twitch",
+  pastblBasePath + "/auth/twitch",
 );
 
 const pastblGetPastasPath = withDefaultValueIfNotString(
   import.meta.env.WXT_PASTBL_GET_PASTAS_PATH,
-  pastblBaseUrl + "/api/v1/pastas",
+  pastblBasePath + "/api/v1/pastas",
 );
 
 export const config = {
@@ -39,7 +39,7 @@ export const config = {
     },
   },
   pastbl: {
-    baseUrl: pastblBaseUrl,
+    baseUrl: pastblBasePath,
     auth: {
       twitch: {
         path: pastblAuthPathViaTwitch,
