f2a1b3145aad6683e0ce4124ff3eb2fb608dbc95
geforseN <geforsenjs@gmail.com>
2024-11-03 22:34:33 +0700

    fix: pastasLoad.execute mock

--- browser-extension/app/composables/usePastasLoad.ts
+++ browser-extension/app/composables/usePastasLoad.ts
@@ -19,6 +19,20 @@ export function useLazyPastasLoad(
   return {
     status,
     async execute(cursor?: Nullish<number>, __mock__?: boolean) {
+      if (__mock__) {
+        consola.debug("mocking pastas");
+        const module = await import("~/utils/pastas.mock.json");
+        if (!Array.isArray(module.default)) {
+          throw new TypeError("module.default is not an array");
+        }
+        const pastas = module.default;
+        status.value = "finished";
+        return {
+          pastas,
+          cursor: null,
+        };
+      }
+
       if (
         status.value !== "ready"
         && status.value !== "unstarted"
@@ -59,17 +73,3 @@ export function useLazyPastasLoad(
     },
   };
 }
-
-// if (__mock__) {
-//         consola.debug("mocking pastas");
-//         const module = await import("~/utils/pastas.mock.json");
-//         if (!Array.isArray(module.default)) {
-//           throw new TypeError("module.default is not an array");
-//         }
-//         const pastas = module.default;
-//         status.value = "ready";
-//         return {
-//           pastas,
-//           cursor: null,
-//         };
-//       }
