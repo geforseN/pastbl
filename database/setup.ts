cbf6405f375fc42f18945c4742eb24ba29ce345c
geforseN <geforsenjs@gmail.com>
2024-11-07 00:22:11 +0700

    fix: db column casing

--- database/setup.ts
+++ database/setup.ts
@@ -4,4 +4,4 @@ import * as schema from "~~/database/schema.ts";
 import { environment } from "~~/server/utils/environment.ts";
 
 export const client = postgres(environment.DATABASE_URL);
-export const database = drizzle(client, { schema, logger: true });
+export const database = drizzle(client, { schema, logger: true, casing: "snake_case" });
