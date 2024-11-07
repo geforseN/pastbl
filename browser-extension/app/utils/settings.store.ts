ca96d2621e2040117fe4412a3337527cd66d01b1
geforseN <geforsenjs@gmail.com>
2024-11-07 22:09:15 +0700

    refactor(entrypoints): [modal] show settings

--- browser-extension/app/utils/settings.store.ts
+++ browser-extension/app/utils/settings.store.ts
@@ -1,6 +1,8 @@
 import { watchThrottled } from "@vueuse/core";
 import { storage } from "wxt/storage";
 
+const KEY = "local:main-button-styles";
+
 // NOTE: color related properties must
 // - first character must be '#'
 // - other 6 characters must be hex value (from 0 to 9 and a to f)
@@ -11,7 +13,7 @@ export const $mainButtonStyles = reactive({
 });
 
 storage
-  .getItem<typeof $mainButtonStyles>("local:main-button-styles")
+  .getItem<typeof $mainButtonStyles>(KEY)
   .then((value) => {
     if (value) {
       Object.assign($mainButtonStyles, value);
@@ -19,7 +21,11 @@ storage
   });
 
 watchThrottled($mainButtonStyles, async (mainButtonStyles) => {
-  await storage.setItem("local:main-button-styles", mainButtonStyles);
+  await storage.setItem(KEY, mainButtonStyles);
 }, {
   throttle: 500,
+});
+
+storage.watch<typeof $mainButtonStyles>(KEY, (mainButtonStyles) => {
+  Object.assign($mainButtonStyles, mainButtonStyles);
 });
