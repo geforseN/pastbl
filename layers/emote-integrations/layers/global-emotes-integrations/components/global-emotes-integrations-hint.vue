410b1cb2010754f1f68e598c15c03bcc33201165
geforseN <geforsenjs@gmail.com>
2024-11-11 20:16:42 +0700

    feat(components): [global-emotes-integrations-hint] init

---
+++ layers/emote-integrations/layers/global-emotes-integrations/components/global-emotes-integrations-hint.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    v-if="globalEmotesIntegrationsStore.uncheckedSources.length !== allEmoteSources.count"
+    class="flex flex-wrap items-center gap-0.5 px-2 py-0.5"
+  >
+    Showing
+    <span v-if="globalEmotesIntegrationsStore.uncheckedSources.length === 0">
+      <nuxt-link-locale
+        class="link inline-flex"
+        to="/collections/global"
+      >
+        all global emotes
+      </nuxt-link-locale>
+      (<emote-integration-logos
+        :sources="globalEmotesIntegrationsStore.checkedSources.state"
+        flat
+        :size="12"
+        class="px-0.5"
+      />)
+    </span>
+    <span v-else>
+      <emote-integration-logos
+        :sources="globalEmotesIntegrationsStore.checkedSources.state"
+        flat
+        :size="12"
+        class="px-0.5"
+      />
+      <nuxt-link-locale
+        class="link inline-flex"
+        to="/collections/global"
+      >
+        global emotes
+      </nuxt-link-locale>
+    </span>
+    <span
+      v-if="globalEmotesIntegrationsStore.uncheckedSources.length > 0"
+    >
+      (
+      <emote-integration-logos
+        :sources="globalEmotesIntegrationsStore.uncheckedSources"
+        flat
+        :size="12"
+      />
+      excluded)
+    </span>
+  </div>
+</template>
+<script setup lang="ts">
+import { useGlobalEmotesIntegrationsStore } from "~/stores/useGlobalEmotesIntegrationsStore";
+
+const globalEmotesIntegrationsStore = useGlobalEmotesIntegrationsStore();
+</script>
