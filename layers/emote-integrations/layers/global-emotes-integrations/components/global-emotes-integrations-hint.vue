1dfb65b9f6a8c59681c9287f996882629a007dfc
geforseN <geforsenjs@gmail.com>
2024-11-11 23:41:43 +0700

    refactor(components): [global-emotes-integrations-hint] update

--- layers/emote-integrations/layers/global-emotes-integrations/components/global-emotes-integrations-hint.vue
+++ layers/emote-integrations/layers/global-emotes-integrations/components/global-emotes-integrations-hint.vue
@@ -1,52 +1,49 @@
 <template>
   <div
-    v-if="globalEmotesIntegrationsStore.uncheckedSources.length !== allEmoteSources.count"
+    v-if="uncheckedSources.length !== allEmoteSources.count"
     class="flex flex-wrap items-center gap-0.5 px-2 py-0.5"
   >
     Showing
-    <span v-if="globalEmotesIntegrationsStore.uncheckedSources.length === 0">
-      <nuxt-link-locale
-        class="link inline-flex"
-        to="/collections/global"
-      >
-        all global emotes
-      </nuxt-link-locale>
-      (<emote-integration-logos
-        :sources="globalEmotesIntegrationsStore.checkedSources.state"
-        flat
-        :size="12"
-        class="px-0.5"
-      />)
-    </span>
-    <span v-else>
-      <emote-integration-logos
-        :sources="globalEmotesIntegrationsStore.checkedSources.state"
-        flat
-        :size="12"
-        class="px-0.5"
-      />
-      <nuxt-link-locale
-        class="link inline-flex"
-        to="/collections/global"
-      >
-        global emotes
-      </nuxt-link-locale>
-    </span>
-    <span
-      v-if="globalEmotesIntegrationsStore.uncheckedSources.length > 0"
+    {{ allSourcesChecked && 'all' }}
+    <emote-integration-logos
+      v-if="!allSourcesChecked"
+      :sources="checkedSources"
+      flat
+      :size="12"
+      class="px-0.5"
+    />
+    <nuxt-link-locale
+      class="link inline-flex"
+      to="/collections/global"
     >
-      (
+      global emotes
+    </nuxt-link-locale>
+    <wrap-with-text
+      before="("
+      :after="allSourcesChecked ? ')' : 'excluded )'"
+    >
       <emote-integration-logos
-        :sources="globalEmotesIntegrationsStore.uncheckedSources"
+        :sources="allSourcesChecked ? checkedSources : uncheckedSources"
         flat
         :size="12"
       />
-      excluded)
-    </span>
+    </wrap-with-text>
   </div>
 </template>
 <script setup lang="ts">
 import { useGlobalEmotesIntegrationsStore } from "~/stores/useGlobalEmotesIntegrationsStore";
 
 const globalEmotesIntegrationsStore = useGlobalEmotesIntegrationsStore();
+
+const allSourcesChecked = computed(
+  () => globalEmotesIntegrationsStore.uncheckedSources.length === 0,
+);
+
+const checkedSources = computed(
+  () => globalEmotesIntegrationsStore.checkedSources.state,
+);
+
+const uncheckedSources = computed(
+  () => globalEmotesIntegrationsStore.uncheckedSources,
+);
 </script>
