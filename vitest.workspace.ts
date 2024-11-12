6be2182c7568c4fe29315f7ada1c18486f6939ef
geforseN <geforsenjs@gmail.com>
2024-11-12 12:02:50 +0700

    refactor!: comment out vitest workspace args parse

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,4 +1,4 @@
-import { parseArgs } from "node:util";
+// import { parseArgs } from "node:util";
 import { defineNuxtVitestConfig } from "./tests/client/nuxt/utils.ts";
 import { defineNitroVitestConfig } from "./tests/server/nitro/utils.ts";
 import { defineNodeVitestConfig } from "./tests/server/node/utils.ts";
@@ -7,23 +7,24 @@ import { defineVueVitestConfig } from "./tests/client/vue/utils.ts";
 const allowedProjectsArray = ["nuxt", "vue", "nitro", "node"] as const;
 type Project = (typeof allowedProjectsArray)[number];
 
-function isAllowedProject(project: unknown): project is Project {
-  return allowedProjectsArray.includes(project as Project);
-}
+// function isAllowedProject(project: unknown): project is Project {
+//   return allowedProjectsArray.includes(project as Project);
+// }
 
-const parsedArgs = parseArgs({
-  args: process.argv.slice(2),
-  options: {
-    project: {
-      type: "string",
-      default: <string[]>[...allowedProjectsArray],
-      multiple: true,
-    },
-  },
-});
+// const parsedArgs = parseArgs({
+//   args: process.argv.slice(2),
+//   options: {
+//     project: {
+//       type: "string",
+//       default: <string[]>[...allowedProjectsArray],
+//       multiple: true,
+//     },
+//   },
+// });
 
 const providedProjects = new Set<Project>(
-  parsedArgs.values.project.filter(isAllowedProject),
+  allowedProjectsArray,
+  // parsedArgs.values.project.filter(isAllowedProject),
 );
 
 export default [
