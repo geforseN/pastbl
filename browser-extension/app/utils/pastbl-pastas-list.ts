706b657b59c6bfc3fb6472614e0c66504cc1afc6
geforseN <geforsenjs@gmail.com>
2024-11-15 00:08:27 +0700

    refactor(utils): [pastbl-pastas-list] update
    stop call findPastaElement twice
    remove findPastaElement, add logic in findPastaIndex

--- browser-extension/app/utils/pastbl-pastas-list.ts
+++ browser-extension/app/utils/pastbl-pastas-list.ts
@@ -14,8 +14,7 @@ export function getCoords(event: PointerEvent) {
 }
 
 export function findPasta(target: HTMLElement) {
-  const element = findPastaElement(target);
-  const index = findPastaIndex(element);
+  const index = findPastaIndex(target);
   const pasta = $pastas.value.at(index);
   if (!pasta) {
     throw new Error(`Pasta with index=${index} not found`);
@@ -24,15 +23,6 @@ export function findPasta(target: HTMLElement) {
 }
 
 function findPastaIndex(target: HTMLElement) {
-  const pastaElement = findPastaElement(target);
-  const number = Number(pastaElement.dataset.pastaIndex);
-  if (!Number.isInteger(number)) {
-    throw new TypeError(`number=${number} is not an integer`);
-  }
-  return number;
-}
-
-function findPastaElement(target: HTMLElement) {
   const pastaElement = target.dataset.pastaIndex
     ? target
     : target.closest("[data-pasta-index]");
@@ -41,5 +31,9 @@ function findPastaElement(target: HTMLElement) {
       target,
     });
   }
-  return pastaElement;
+  const index = Number(pastaElement.dataset.pastaIndex);
+  if (!Number.isInteger(index)) {
+    throw new TypeError(`index=${index} is not an integer`);
+  }
+  return index;
 }
