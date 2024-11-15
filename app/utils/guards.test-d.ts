580f2c2e2a28aada52a39879ed9e6f22030f6dce
geforseN <geforsenjs@gmail.com>
2024-11-15 16:47:35 +0700

    chore: add FIXME comment

--- app/utils/guards.test-d.ts
+++ app/utils/guards.test-d.ts
@@ -1,9 +1,13 @@
-import { expectTypeOf } from "vitest";
+import { expectTypeOf, describe } from "vitest";
 import { isError } from "./guards.ts";
 
 class CustomError extends Error {}
 
-const error = new CustomError("test");
-assert.ok(isError(error, CustomError));
-expectTypeOf(error).toBeInstanceOf(CustomError);
-expectTypeOf(error).toBeInstanceOf(Error);
+describe("isError", () => {
+  const error = new CustomError("test");
+  // FIXME: Argument of type 'typeof CustomError' is not assignable to parameter of type 'ErrorConstructor'.
+  // Type 'typeof CustomError' provides no match for the signature '(message?: string | undefined): Error'.ts(2345)
+  assert.ok(isError(error, CustomError));
+  expectTypeOf(error).toBeInstanceOf(CustomError);
+  expectTypeOf(error).toBeInstanceOf(Error);
+});
