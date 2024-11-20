7b443813b25695b218bf9fa469bcc1f8dfa0b79a
geforseN <geforsenjs@gmail.com>
2024-11-20 13:15:12 +0700

    fix(utils): [log] stop show value when it is not provided

--- shared/utils/dev-only.ts
+++ shared/utils/dev-only.ts
@@ -4,10 +4,10 @@ export function log<
   T extends import("consola").LogType,
 >(...args: [type: T, key: string, value?: Record<string, unknown>]) {
   if (import.meta.dev) {
-    const [type, key, value] = args;
+    const [type, ...keyAndValue] = args;
     consolaPromise ??= import("consola");
     consolaPromise.then(({ consola }) => {
-      consola[type](key, value);
+      consola[type](...keyAndValue);
     });
   }
 }
