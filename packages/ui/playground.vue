2862b16c5d556fe54597f71fe464c63b2bf0e1a6
geforseN <geforsenjs@gmail.com>
2024-11-20 13:24:14 +0700

    feat(ui): add playground

---
+++ packages/ui/playground.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="flex flex-col items-center bg-base-100"
+    style="gap: 3rem;"
+  >
+    <div>
+      <h2>Compact</h2>
+      <chat-pasta
+        class="w-[420px] border"
+        text="hello world"
+        compact
+        :time="{ label: 'Created', value: Date.now() }"
+        :chatter
+      />
+    </div>
+    <div>
+      <h2>Compact with tags</h2>
+      <chat-pasta
+        class="w-[420px] border"
+        text="hello world"
+        compact
+        :time="{ label: 'Created', value: Date.now() }"
+        :tags="['tag1', 'tag2']"
+        :chatter
+      />
+    </div>
+    <div>
+      <h2>Not Compact</h2>
+      <chat-pasta
+        class="w-[420px] border"
+        text="hello world"
+        :time="{ label: 'Created', value: Date.now() }"
+        :chatter
+      />
+    </div>
+    <div>
+      <h2>Not Compact with tags</h2>
+      <chat-pasta
+        class="w-[420px] border"
+        text="hello world"
+        :time="{ label: 'Created', value: Date.now() }"
+        :tags="['tag1', 'tag2']"
+        :chatter
+      />
+    </div>
+    <div>
+      <h2>Copy Button</h2>
+      <chat-pasta-copy-button />
+      <chat-pasta-copy-button square />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import ChatPasta from "./src/chat-pasta/chat-pasta.vue";
+import ChatPastaCopyButton from "./src/chat-pasta/components/buttons/chat-pasta-copy-button.vue";
+
+const chatter = {
+  badges: 1,
+  nickname: "geforsen",
+  nicknameColor: "red",
+};
+</script>
