fc7c048113c166730b5f5342ad1238a09d491b6b
geforseN <geforsenjs@gmail.com>
2024-11-24 15:28:55 +0700

    feat(ui): [with-dropdown] init

---
+++ packages/ui/src/with-dropdown/with-dropdown.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="dropdown"
+    :class="[
+      side === 'left' && 'dropdown-left',
+      side === 'right' && 'dropdown-right',
+      placement === 'top' && 'dropdown-top',
+      placement === 'bottom' && 'dropdown-bottom',
+      align === 'end' && 'dropdown-end',
+      showOnHover && 'dropdown-hover',
+    ]"
+  >
+    <slot name="default" />
+    <slot name="content" />
+  </div>
+</template>
+<script setup lang="ts">
+const {
+  side,
+  placement,
+  align,
+  showOnHover = true,
+} = defineProps<{
+  side?: "left" | "right";
+  placement?: "top" | "bottom";
+  align?: "end";
+  showOnHover?: boolean;
+}>();
+
+defineSlots<{
+  default: () => unknown;
+  content: () => unknown;
+}>();
+</script>
