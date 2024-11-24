d9b563842afd9aa759a9b930b13959e614f37c86
geforseN <geforsenjs@gmail.com>
2024-11-24 15:27:33 +0700

    refactor(ui): [chat-pasta-tags]
    add padding
    add shouldSort prop

--- packages/ui/src/chat-pasta/components/chat-pasta-tags.vue
+++ packages/ui/src/chat-pasta/components/chat-pasta-tags.vue
@@ -1,18 +1,27 @@
 <template>
   <div
-    class="-mb-0.5 mt-0.5 inline-flex flex-wrap gap-0.5"
+    class="-mb-0.5 mt-0.5 inline-flex flex-wrap gap-0.5 px-1 py-0.5"
   >
     <clamped-pasta-tag
-      v-for="tag of tags"
+      v-for="tag of tags_"
       :key="tag"
       :tag
     />
   </div>
 </template>
 <script setup lang="ts">
+import { computed } from "vue";
 import ClampedPastaTag from "../../clamped-pasta-tag.vue";
 
-defineProps<{
+const { shouldSort = true, tags } = defineProps<{
   tags: string[];
+  shouldSort?: boolean;
 }>();
+
+const tags_ = computed(() => {
+  if (shouldSort) {
+    return [...tags].sort((a, b) => a.localeCompare(b));
+  }
+  return tags;
+});
 </script>
