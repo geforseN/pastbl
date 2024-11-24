f22fc5331db872dc886ac60110dd89762a25d120
geforseN <geforsenjs@gmail.com>
2024-11-24 15:26:01 +0700

    refactor(ui): [chat-pasta-more-actions]
    chat-pasta-more-actions:
     - use bl-button
     - try center dots in button

--- packages/ui/src/chat-pasta/components/more-actions/chat-pasta-more-actions-button.vue
+++ packages/ui/src/chat-pasta/components/more-actions/chat-pasta-more-actions-button.vue
@@ -1,10 +1,28 @@
 <template>
-  <div
-    tabindex="0"
-    role="button"
+  <bl-button
     data-testid="chat-pasta-more-actions-button"
-    class="btn btn-square btn-primary btn-md border-2 border-primary-content"
+    :class="[
+      !$slots.default && 'flex items-center',
+      twMerge('border-2 border-primary-content', $attrs.class as string | undefined),
+    ]"
+    v-bind="{
+      ...props,
+      tag: props.tag ?? 'div',
+      variant: props.variant ?? 'primary',
+    }"
   >
-    ...
-  </div>
+    <slot name="default">
+      <span class="-translate-y-1 text-lg">...</span>
+    </slot>
+  </bl-button>
 </template>
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge";
+import BlButton, { type BlButtonProps } from "../../../button/bl-button.vue";
+
+const props = defineProps<BlButtonProps>();
+
+defineSlots<{
+  default: () => unknown;
+}>();
+</script>
---
+++ packages/ui/src/chat-pasta/components/more-actions/components.ts
@@ -0,0 +1,2 @@
+export { default as ChatPastaMoreActionsButton } from "./chat-pasta-more-actions-button.vue";
+export { default as ChatPastaMoreActionsDropdownContent } from "./chat-pasta-more-actions-dropdown-content.vue";
